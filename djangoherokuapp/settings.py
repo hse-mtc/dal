c88c6ef043110b16c8a5402b51223d60fc32dd77
khoroshansky <lev.khoroshansky@yandex.ru>
2019-10-30 10:23:49 +0300

    fix static files

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 import dj_database_url 
