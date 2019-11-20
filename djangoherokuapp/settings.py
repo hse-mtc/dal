bde11c6768408351ce55cec553886491dd6d6f88
darkydash <darkydashie@gmail.com>
2019-11-20 13:45:58 +0300

    Connecting to heroku postgres

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -169,7 +170,4 @@ CORS_ALLOW_METHODS = [
     'PUT',
 ]
 
-# import dj_database_url
-#
-# prod_db = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+DATABASES['default'] = dj_database_url.config(conn_max_age=500)
