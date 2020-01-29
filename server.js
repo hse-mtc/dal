9be387ce6ad59e673a2f63459279380ad8bc5c09
TmLev <leva.bugatti@gmail.com>
2020-01-29 13:24:10 +0300

    Change frontend settings for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express   = require("express");
 const port      = process.env.PORT || 8080;
 const app       = express();
-const dist_path = __dirname + "/dist/";
+const dist_path = __dirname + "/frontend/dist/";
 
 app.use(express.static(dist_path));
 app.get(
