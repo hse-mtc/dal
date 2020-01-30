f2eec72a7a3e8251000e6360a19e87126190b432
TmLev <leva.bugatti@gmail.com>
2020-01-30 11:38:30 +0300

    Remove `proxy`

--- vue.config.js
+++ vue.config.js
@@ -36,18 +36,6 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    // proxy: {
-    //   // change xxx-api/login => mock/login
-    //   // detail: https://cli.vuejs.org/config/#devserver-proxy
-    //   [process.env.VUE_APP_BASE_API]: {
-    //     target: `https://military-dms.herokuapp.com/api`,
-    //     changeOrigin: true,
-    //     pathRewrite: {
-    //       ['^' + process.env.VUE_APP_BASE_API]: ''
-    //     }
-    //   }
-    // }
-    // after: require('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
