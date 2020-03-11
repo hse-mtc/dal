8d87cd473a050cfa8991098038d8382dc1d24d94
TmLev <leva.bugatti@gmail.com>
2020-03-11 13:51:28 +0300

    Refactor admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -4,11 +4,12 @@ from django.contrib.auth.models import User
 
 from backend.models import (
     Author,
+    Category,
     Document,
     Profile,
     Publisher,
     Subject,
-    Category)
+)
 
 
 class ProfileInline(admin.StackedInline):
@@ -25,7 +26,7 @@ admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
 admin.site.register(Author)
+admin.site.register(Category)
 admin.site.register(Document)
 admin.site.register(Publisher)
 admin.site.register(Subject)
-admin.site.register(Category)
