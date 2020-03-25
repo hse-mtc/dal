30b5b1bd87ab25416b01a988bb08381e2100984a
Grihalkh <38518245+Grihalkh@users.noreply.github.com>
2020-03-25 11:28:21 +0300

    lms_populate bug fix

--- dms/settings.py
+++ dms/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "NAME": "db_name",
         "USER": "db_user",
         "PASSWORD": "db_user_password",
-        "HOST": "db",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
