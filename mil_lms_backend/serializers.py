d9988fa8c91543983de30d4dfff26acf3a4f58bf
Grihalkh <38518245+Grihalkh@users.noreply.github.com>
2020-04-04 16:57:11 +0300

    Fixed bug with date format when modifying student

--- mil_lms_backend/serializers.py
+++ mil_lms_backend/serializers.py
@@ -1,3 +1,4 @@
+import datetime
 from rest_framework import serializers
 
 from .models import (
@@ -28,7 +29,7 @@ class ProgramSerializer(serializers.ModelSerializer):
 class StudentSerializer(serializers.ModelSerializer):
     milgroup = MilgroupSerializer(many=False)
     program = ProgramSerializer(many=False)
-    birthdate = serializers.DateField(format='%d.%m.%Y', input_formats=['%d.%m.%Y'])
+    birthdate = serializers.DateField(format='%d.%m.%Y', input_formats=['%d.%m.%Y', 'iso-8601'])
 
     fullname = serializers.SerializerMethodField()
 
@@ -63,6 +64,12 @@ class StudentSerializer(serializers.ModelSerializer):
         program_data = validated_data.pop('program')
         program = Program.objects.get(**program_data)
 
+        # Convert from '%d.%m.%Y' format to '%Y-%m-%d' format
+        # because Django doesn't accept russian formats.
+        # Though, it accepts russian format on creating -_-
+        validated_data['birthdate'] = datetime.datetime.strptime(
+            validated_data['birthdate'], '%d.%m.%Y').strftime('%Y-%m-%d')
+        # serializer converts
         student_modified = instance.update(
             milgroup=milgroup, program=program,
             **validated_data)
