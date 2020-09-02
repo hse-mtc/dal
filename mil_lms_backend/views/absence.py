92fa54b6935adbf4730978bc52c18697765c1b20
gakhromov <gakhromov@yandex.ru>
2020-09-02 16:27:13 +0300

    Absence PUT finished

--- mil_lms_backend/views/absence.py
+++ mil_lms_backend/views/absence.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+from django.db import IntegrityError
 from django.db.models import Value
 from django.db.models.functions import (
     Lower,
@@ -36,6 +37,8 @@ class AbsenceView(APIView):
         .../absence/
         .../absence/?id=2
         .../absence/?studentid=3&date=...
+        :param request:
+        :return:
         """
         # check query params
         query_params = AbsenceGetQuerySerializer(data=request.query_params)
@@ -70,10 +73,46 @@ class AbsenceView(APIView):
             absences = absences.filter(date=request.query_params['date'])
         
         # filter by type
-        if 'type' in request.query_params:
-            absences = absences.filter(type=request.query_params['type'])
+        if 'absenceType' in request.query_params:
+            absences = absences.filter(absenceType=request.query_params['absenceType'])
         
         absences = AbsenceSerializer(absences, many=True)
         return Response({'code': HTTP_200_OK * 100,
                          'absences': absences.data},
                         status=HTTP_200_OK)
+
+    def put(self, request: Request) -> Response:
+        """
+        Create new absence record.
+        PUT function - data is given via 'data' from PUT request (not query!)
+        
+        Payload example:
+        {
+            "date": "21.01.2020",
+            "absenceType": "Уважительная",
+            "studentid": {
+                "name": "Артем",
+                "surname": "Кацевалов"
+            },
+            "reason": "Заболел",
+            "status": "1",
+            "comment": "Болеть будет недолго"
+        }
+        :param request:
+        :return:
+        """
+        absence = AbsenceSerializer(data=request.data)
+        try:
+            if absence.is_valid():
+                absence = absence.save()
+                return Response({'code': HTTP_200_OK * 100,
+                                'message': f'Absence record with id {absence.id} successfuly created'},
+                                status=HTTP_200_OK)
+            else:
+                return Response({'code': HTTP_400_BAD_REQUEST * 100,
+                                'message': absence.errors},
+                                status=HTTP_400_BAD_REQUEST)
+        except IntegrityError as e:
+            return Response({'code': HTTP_400_BAD_REQUEST * 100,
+                                'message': 'A record with this student and this date already exists. Please, modify existing record.'},
+                                status=HTTP_400_BAD_REQUEST)
