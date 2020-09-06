224c4e39536816f0d819095832fa8279589e168d
TmLev <leva.bugatti@gmail.com>
2020-09-06 15:20:41 +0300

    fix: move dms urls to backend directory

---
+++ backend/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+
+from backend.views import (
+    authors,
+    delete_document,
+    documents,
+    get_file,
+    get_tags,
+    info,
+    login,
+    logout,
+    published_places,
+    subjects,
+    CategoryView,
+    SubjectSectionView,
+    UploadNirView,
+)
+
+urlpatterns = [
+    path("authors", authors),
+    path("category", CategoryView.as_view()),
+    path("delete_document", delete_document),
+    path("documents", documents),
+    path("get_file", get_file),
+    path("get_tags", get_tags),
+    path("published_places", published_places),
+    path("subject", SubjectSectionView.as_view()),
+    path("subjects", subjects),
+    path("upload", UploadNirView.as_view()),
+    path("user/info", info),
+    path("user/login", login),
+    path("user/logout", logout),
+]
--- dms/urls.py
+++ dms/urls.py
@@ -6,22 +6,7 @@ from rest_framework import permissions
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
-from backend.views import (
-    authors,
-    delete_document,
-    documents,
-    get_file,
-    get_tags,
-    info,
-    login,
-    logout,
-    populate,
-    published_places,
-    subjects,
-    CategoryView,
-    SubjectSectionView,
-    UploadNirView,
-)
+from backend.views import populate
 
 from mil_lms_backend.views.populate import lms_populate
 
@@ -35,23 +20,16 @@ SchemaView = get_schema_view(
 )
 
 urlpatterns = [
+    # Internal
     path("admin/", admin.site.urls),
     path("populate/", populate),
-    path("api/authors", authors),
-    path("api/category", CategoryView.as_view()),
-    path("api/delete_document", delete_document),
-    path("api/documents", documents),
-    path("api/get_file", get_file),
-    path("api/get_tags", get_tags),
-    path("api/published_places", published_places),
-    path("api/subject", SubjectSectionView.as_view()),
-    path("api/subjects", subjects),
-    path("api/upload", UploadNirView.as_view()),
-    path("api/user/info", info),
-    path("api/user/login", login),
-    path("api/user/logout", logout),
     path("lms_populate/", lms_populate),
+
+    # Public API
+    path("api/", include("backend.urls")),
     path("api/lms/", include("mil_lms_backend.urls")),
+
+    # Swagger
     path("swagger/",
          SchemaView.with_ui("swagger", cache_timeout=0),
          name="schema-swagger-ui"),
