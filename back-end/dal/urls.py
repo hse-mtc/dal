1c60cf6a31ca8f59c2f7f8874f85bf3cfb86ecb9
TmLev <leva.bugatti@gmail.com>
2020-09-09 14:36:27 +0300

    fix: remove front-end serving

--- back-end/dal/urls.py
+++ back-end/dal/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-from django.views.decorators.cache import never_cache
 
 from rest_framework import permissions
 
@@ -12,7 +10,6 @@ from dal.settings import DEBUG
 from dms.views import populate
 from lms.views.populate import lms_populate
 
-index_view = never_cache(TemplateView.as_view(template_name="index.html"))
 
 SchemaView = get_schema_view(
     openapi.Info(
@@ -24,17 +21,14 @@ SchemaView = get_schema_view(
 )
 
 urlpatterns = [
-    # Frontend
-    path("", index_view, name="index"),
+    # Swagger
+    path("",
+         SchemaView.with_ui("swagger", cache_timeout=0),
+         name="schema-swagger-ui"),
 
     # Public API
     path("api/", include("dms.urls")),
     path("api/lms/", include("lms.urls")),
-
-    # Swagger
-    path("swagger/",
-         SchemaView.with_ui("swagger", cache_timeout=0),
-         name="schema-swagger-ui"),
 
     # Internal
     path("admin/", admin.site.urls),
