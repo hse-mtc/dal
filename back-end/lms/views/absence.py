c9db52b614116c9f85c05fde3654918d02bfa050
gakhromov <gakhromov@yandex.ru>
2020-09-11 11:14:52 +0300

    removed 'code' from data

--- back-end/lms/views/absence.py
+++ back-end/lms/views/absence.py
@@ -44,12 +44,8 @@ class AbsenceView(APIView):
         # check query params
         query_params = AbsenceGetQuerySerializer(data=request.query_params)
         if not query_params.is_valid():
-            return Response(
-                {
-                    'code': HTTP_400_BAD_REQUEST * 100,
-                    'message': query_params.errors
-                },
-                status=HTTP_400_BAD_REQUEST)
+            return Response({'message': query_params.errors},
+                            status=HTTP_400_BAD_REQUEST)
 
         absences = Absence.objects.all()
 
@@ -57,12 +53,7 @@ class AbsenceView(APIView):
         if 'id' in request.query_params:
             absence = absences.get(id=request.query_params['id'])
             absence = AbsenceSerializer(absence)
-            return Response(
-                {
-                    'code': HTTP_200_OK * 100,
-                    'students': absence.data
-                },
-                status=HTTP_200_OK)
+            return Response({'students': absence.data}, status=HTTP_200_OK)
 
         # filter by studentid
         if 'studentid' in request.query_params:
@@ -87,11 +78,7 @@ class AbsenceView(APIView):
                 absenceType=request.query_params['absenceType'])
 
         absences = AbsenceSerializer(absences, many=True)
-        return Response({
-            'code': HTTP_200_OK * 100,
-            'absences': absences.data
-        },
-                        status=HTTP_200_OK)
+        return Response({'absences': absences.data}, status=HTTP_200_OK)
 
     # pylint: disable=no-self-use
     @csrf_exempt
@@ -120,26 +107,16 @@ class AbsenceView(APIView):
                 absence = absence.save()
                 return Response(
                     {
-                        'code':
-                            HTTP_200_OK * 100,
-                        'message':
-                            f'Absence record with id {absence.id} '
-                            f'successfuly created'
+                        'message': f'Absence record with id {absence.id} '
+                                   f'successfuly created'
                     },
                     status=HTTP_200_OK)
-            return Response(
-                {
-                    'code': HTTP_400_BAD_REQUEST * 100,
-                    'message': absence.errors
-                },
-                status=HTTP_400_BAD_REQUEST)
+            return Response({'message': absence.errors},
+                            status=HTTP_400_BAD_REQUEST)
         except IntegrityError:
             return Response(
                 {
-                    'code':
-                        HTTP_400_BAD_REQUEST * 100,
-                    'message':
-                        'A record with this student and this date '
-                        'already exists. Please, modify existing record.'
+                    'message': 'A record with this student and this date '
+                               'already exists. Please, modify existing record.'
                 },
                 status=HTTP_400_BAD_REQUEST)
