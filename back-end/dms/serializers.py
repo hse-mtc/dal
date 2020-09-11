48213f74538c157aa33cf66705f60965c10e9f29
TmLev <leva.bugatti@gmail.com>
2020-09-11 10:40:12 +0300

    feat: add publisher, subject serializers

--- back-end/dms/serializers.py
+++ back-end/dms/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
-from dms.models import (Author, Category)
+from dms.models import (
+    Author,
+    Category,
+    Publisher,
+    Subject,
+)
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -20,4 +25,24 @@ class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
+        fields = "__all__"
+
+
+class PublisherSerializer(serializers.ModelSerializer):
+    """
+    Serializes Publisher model.
+    """
+
+    class Meta:
+        model = Publisher
+        fields = "__all__"
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+    """
+    Serializes Subject model.
+    """
+
+    class Meta:
+        model = Subject
         fields = "__all__"
