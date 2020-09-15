3d3bbb7742a8099d5b43eea9a4371cf11c67ca3e
gakhromov <gakhromov@yandex.ru>
2020-09-15 17:54:18 +0300

    Started to migrate Views to ViewSets

--- back-end/lms/urls.py
+++ back-end/lms/urls.py
@@ -1,10 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
 from lms.views.student import StudentView
 from lms.views.teacher import TeacherView
 from lms.views.absence import AbsenceView
+
+from lms.views.student2 import StudentViewSet
+
+
+router = DefaultRouter()
+router.register('student2', StudentViewSet)
 
 urlpatterns = [
     path('student/', StudentView.as_view()),
     path('teacher/', TeacherView.as_view()),
     path('absence/', AbsenceView.as_view()),
 ]
+
+urlpatterns += router.urls
---
+++ back-end/lms/views/viewsets.py
@@ -0,0 +1,65 @@
+from django.db.models.query import QuerySet
+from django.db.models import Value
+from django.db.models.functions import (
+    Lower,
+    Concat,
+)
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
+
+
+class GetPutPostDeleteModelViewSet(ModelViewSet):
+    query_params_serializer_class = None
+
+    get_by_id = True
+    get_filters = []
+    special_get_filters = []
+
+    def list(self, request: Request) -> Response:
+        items = self.queryset
+
+        # check query params
+        query_params = self.query_params_serializer_class(data=request.query_params)
+        if not query_params.is_valid():
+            return Response(query_params.errors,
+                            status=HTTP_400_BAD_REQUEST)
+
+        # get by id
+        if self.get_by_id and 'id' in request.query_params:
+            item = items.get(id=request.query_params['id'])
+            return Response(self.serializer_class(item).data)
+
+        # filter by filters
+        for filt in self.get_filters:
+            if filt in request.query_params:
+                items = items.filter(**{filt: request.query_params[filt]})
+        
+        # filter by special filters
+        for filt in self.special_get_filters:
+            if filt in request.query_params:
+                items = self.special_get_filters[filt](items, request)
+        """
+        # filter name
+        if 'name' in request.query_params:
+            students = students.annotate(search_name=Lower(
+                Concat('surname', Value(' '), 'name', Value(' '),
+                       'patronymic')))
+            students = students.filter(
+                search_name__contains=request.query_params['name'].lower())
+        """
+
+        return Response(self.serializer_class(items, many=True).data)
+
+
+
+def filter_names(items: QuerySet, request: Request) -> QuerySet:
+    items = items.annotate(search_name=Lower(
+        Concat('surname', Value(' '), 'name', Value(' '),
+               'patronymic')))
+    items = items.filter(
+        search_name__contains=request.query_params['name'].lower())
+    return items
