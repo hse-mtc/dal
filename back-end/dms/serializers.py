52c0d5716fc72682fff74288e696dbbbaf753652
TmLev <leva.bugatti@gmail.com>
2020-09-16 21:18:11 +0300

    feat: add tags, document serializers

--- back-end/dms/serializers.py
+++ back-end/dms/serializers.py
@@ -41,9 +41,32 @@ class SubjectSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class TagListField(serializers.ListField):
+    child = serializers.CharField()
+
+    def to_representation(self, data):
+        return data.values_list("name", flat=True)
+
+
 class DocumentSerializer(serializers.ModelSerializer):
     """Serializes Document model."""
+
+    tags = TagListField(required=False)
 
     class Meta:
         model = Document
         fields = "__all__"
+
+    def create(self, validated_data):
+        tags = validated_data.pop("tags", None)
+        instance = super().create(validated_data)
+        if tags:
+            instance.tags.set(*tags)
+        return instance
+
+    def update(self, instance, validated_data):
+        tags = validated_data.pop("tags", None)
+        instance = super().update(instance, validated_data)
+        if tags:
+            instance.tags.set(*tags)
+        return instance
