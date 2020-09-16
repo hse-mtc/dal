dfd2b8f7141267c1d69329c9e8e71015f7f35761
TmLev <leva.bugatti@gmail.com>
2020-09-16 21:23:40 +0300

    fix: update urls

--- back-end/dms/urls.py
+++ back-end/dms/urls.py
@@ -2,36 +2,35 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from dms import views
 from dms.views import (
-    delete_document,
-    documents,
+    AuthorViewSet,
+    CategoryViewSet,
+    DocumentViewSet,
+    PublisherViewSet,
+    SubjectSectionView,
+    SubjectViewSet,
     get_file,
-    tags,
     info,
     login,
     logout,
-    SubjectSectionView,
-    UploadNirView,
+    tags,
 )
 
 router = routers.DefaultRouter()
-router.register(r"authors", views.AuthorViewSet)
-router.register(r"categories", views.CategoryViewSet)
-router.register(r"publishers", views.PublisherViewSet)
-router.register(r"subjects", views.SubjectViewSet)
+router.register(r"authors", AuthorViewSet)
+router.register(r"categories", CategoryViewSet)
+router.register(r"publishers", PublisherViewSet)
+router.register(r"subjects", SubjectViewSet)
+router.register(r"documents", DocumentViewSet)
 
 urlpatterns = [
     # REST router
     path("", include(router.urls)),
 
     # Manual urls
-    path("delete_document/", delete_document),
-    path("documents/", documents),
     path("get_file/", get_file),
     path("subject/", SubjectSectionView.as_view()),
     path("tags/", tags),
-    path("upload/", UploadNirView.as_view()),
     path("user/info/", info),
     path("user/login/", login),
     path("user/logout/", logout),
