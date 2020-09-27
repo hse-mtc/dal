c4322a65e95d9883166548f910a7ff0db89ad5a4
TmLev <leva.bugatti@gmail.com>
2020-09-27 12:12:39 +0300

    refactor: import views

--- back-end/auth/urls.py
+++ back-end/auth/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+    token_obtain_pair,
+    token_refresh,
 )
+
 from auth.views import info
 
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/", token_obtain_pair),
+    path("token/refresh/", token_refresh),
     path("info/", info),
 ]
