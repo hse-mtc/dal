1132956b1271b36cebc39b7463643c3087af7a41
TmLev <leva.bugatti@gmail.com>
2020-09-27 20:34:34 +0300

    swagger: extract parameter. auth: move login, logout logic

--- back-end/auth/views.py
+++ back-end/auth/views.py
@@ -1,32 +1,28 @@
+from django.contrib.auth import authenticate
+from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+)
 
 from drf_yasg.utils import swagger_auto_schema
-from drf_yasg.openapi import (
-    IN_HEADER,
-    TYPE_STRING,
-    Parameter,
-)
+
+from auth.swagger import token_auth_header
 
 
-@swagger_auto_schema(
-    method='get',
-    manual_parameters=[
-        Parameter(
-            "Authorization",
-            IN_HEADER,
-            type=TYPE_STRING,
-            required=True,
-            default="Token ",
-        ),
-    ],
-)
+@swagger_auto_schema(method="get", manual_parameters=[token_auth_header])
 @csrf_exempt
 @api_view(["GET"])
+@permission_classes([AllowAny])
 def info(request: Request) -> Response:
     data = {
         "roles": ["admin"],
@@ -35,3 +31,39 @@ def info(request: Request) -> Response:
     }
 
     return Response(data, status=HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def login(request: Request) -> Response:
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if not username or not password:
+        return Response({"message": "Username and password are required."},
+                        status=HTTP_400_BAD_REQUEST)
+
+    user = authenticate(
+        username=username,
+        password=password,
+    )
+    if not user:
+        return Response({"message": "Invalid credentials."},
+                        status=HTTP_404_NOT_FOUND)
+
+    token, _ = Token.objects.get_or_create(user=user)
+
+    return Response({"token": token.key}, status=HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def logout(request: Request) -> Response:
+    try:
+        request.user.auth_token.delete()
+    except (AttributeError, ObjectDoesNotExist):
+        pass
+
+    return Response({"message": "User logged out successfully."},
+                    status=HTTP_200_OK)
