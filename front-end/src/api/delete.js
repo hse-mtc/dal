e64054ec6129cff3e7f75aba168b9ee580f20749
TmLev <leva.bugatti@gmail.com>
2020-09-29 10:28:06 +0300

    fix: append trailing slash

--- front-end/src/api/delete.js
+++ front-end/src/api/delete.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function deleteDocument(params) {
   return request({
-    url: `/dms/documents/${params.id}`,
+    url: `/dms/documents/${params.id}/`,
     method: 'delete',
   })
 }
