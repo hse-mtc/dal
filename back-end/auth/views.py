f42292ec00936e1dfed08c318a60c41b2535397b
TmLev <leva.bugatti@gmail.com>
2020-10-04 08:57:50 +0300

    fix: remove login view

--- back-end/auth/views.py
+++ back-end/auth/views.py
@@ -1,25 +1,19 @@
-from django.contrib.auth import authenticate
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework.decorators import api_view
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
-from rest_framework.authtoken.models import Token
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.status import (
-    HTTP_200_OK,
-    HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
-)
+from rest_framework.status import HTTP_200_OK
 
 from drf_yasg.utils import swagger_auto_schema
 
 from auth.swagger import token_auth_header
 
 
-@swagger_auto_schema(method="get", manual_parameters=[token_auth_header])
+@swagger_auto_schema(method="GET", manual_parameters=[token_auth_header])
 @csrf_exempt
 @api_view(["GET"])
 @permission_classes([AllowAny])
@@ -31,29 +25,6 @@ def info(request: Request) -> Response:
     }
 
     return Response(data, status=HTTP_200_OK)
-
-
-@csrf_exempt
-@api_view(["POST"])
-@permission_classes([AllowAny])
-def login(request: Request) -> Response:
-    username = request.data.get("username")
-    password = request.data.get("password")
-    if not username or not password:
-        return Response({"message": "Username and password are required."},
-                        status=HTTP_400_BAD_REQUEST)
-
-    user = authenticate(
-        username=username,
-        password=password,
-    )
-    if not user:
-        return Response({"message": "Invalid credentials."},
-                        status=HTTP_404_NOT_FOUND)
-
-    token, _ = Token.objects.get_or_create(user=user)
-
-    return Response({"token": token.key}, status=HTTP_200_OK)
 
 
 @csrf_exempt
