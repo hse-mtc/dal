0511c76dbaf1e6b3ec052fd0ca3428de99bfb799
TmLev <leva.bugatti@gmail.com>
2020-10-04 09:01:08 +0300

    style: proper imports

--- back-end/auth/views.py
+++ back-end/auth/views.py
@@ -1,12 +1,14 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.csrf import csrf_exempt
 
-from rest_framework.decorators import api_view
-from rest_framework.decorators import permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework import permissions
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+)
 
 from drf_yasg.utils import swagger_auto_schema
 
@@ -16,7 +18,7 @@ from auth.swagger import token_auth_header
 @swagger_auto_schema(method="GET", manual_parameters=[token_auth_header])
 @csrf_exempt
 @api_view(["GET"])
-@permission_classes([AllowAny])
+@permission_classes([permissions.AllowAny])
 def info(request: Request) -> Response:
     data = {
         "roles": ["admin"],
@@ -29,7 +31,7 @@ def info(request: Request) -> Response:
 
 @csrf_exempt
 @api_view(["POST"])
-@permission_classes([AllowAny])
+@permission_classes([permissions.AllowAny])
 def logout(request: Request) -> Response:
     try:
         request.user.auth_token.delete()
