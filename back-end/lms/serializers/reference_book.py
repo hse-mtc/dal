b8c0c6e6d6c9d023c501857758154107de16ab21
Vladis <veisakov@miem.hse.ru>
2020-10-09 13:11:31 +0300

    Fix: linting; Added: comment to GET method

--- back-end/lms/serializers/reference_book.py
+++ back-end/lms/serializers/reference_book.py
@@ -15,7 +15,6 @@ from lms.serializers.serializers import (MilgroupSerializer, ProgramSerializer,
                                          StudentPostSerializer)
 
 
-# pylint: disable=abstract-method
 class ReferenceBookSerializer(Serializer):
     milgroups = SerializerMethodField(read_only=True)
     programs = SerializerMethodField(read_only=True)
@@ -24,38 +23,44 @@ class ReferenceBookSerializer(Serializer):
     teacher_posts = SerializerMethodField(read_only=True)
     student_posts = SerializerMethodField(read_only=True)
 
-    # pylint: disable=unused-argument
     def get_milgroups(self, obj):
+        # pylint: disable=unused-argument
         # pylint: disable=no-self-use
         queryset = Milgroup.objects.all()
         return MilgroupSerializer(queryset, many=True).data
 
-    # pylint: disable=unused-argument
     def get_programs(self, obj):
+        # pylint: disable=unused-argument
         # pylint: disable=no-self-use
         queryset = Program.objects.all()
         return ProgramSerializer(queryset, many=True).data
 
-    # pylint: disable=unused-argument
     def get_ranks(self, obj):
+        # pylint: disable=unused-argument
         # pylint: disable=no-self-use
         queryset = Rank.objects.all()
         return RankSerializer(queryset, many=True).data
 
-    # pylint: disable=unused-argument
     def get_skills(self, obj):
+        # pylint: disable=unused-argument
         # pylint: disable=no-self-use
         queryset = Skill.objects.all()
         return SkillSerializer(queryset, many=True).data
 
-    # pylint: disable=unused-argument
     def get_teacher_posts(self, obj):
+        # pylint: disable=unused-argument
         # pylint: disable=no-self-use
         queryset = TeacherPost.objects.all()
         return TeacherPostSerializer(queryset, many=True).data
 
-    # pylint: disable=unused-argument
     def get_student_posts(self, obj):
+        # pylint: disable=unused-argument
         # pylint: disable=no-self-use
         queryset = StudentPost.objects.all()
         return StudentPostSerializer(queryset, many=True).data
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
