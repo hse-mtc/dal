f00d04312e7b49612e5351c4f955bcff86e81611
gakhromov <gakhromov@yandex.ru>
2020-10-09 11:31:34 +0300

    Changed custom NestedSerializers to WritableNestedSerializers from drf-writable-nested

--- back-end/lms/serializers/teacher.py
+++ back-end/lms/serializers/teacher.py
@@ -1,31 +1,20 @@
-from rest_framework.serializers import SerializerMethodField, CharField
+from rest_framework.serializers import SerializerMethodField
+from drf_writable_nested.serializers import WritableNestedModelSerializer
 
 from lms.models import (
-    Milfaculty,
     Milgroup,
-    Rank,
-    TeacherPost,
     Teacher,
 )
 
 from lms.validators import PresentInDatabaseValidator
-from lms.serializers.serializers import (MilgroupSerializer,
-                                         NestedModelSerializer)
+from lms.serializers.serializers import MilgroupSerializer
 
 
-class TeacherSerializer(NestedModelSerializer):
+class TeacherSerializer(WritableNestedModelSerializer):
     milgroup = MilgroupSerializer(
         required=False,
         many=False,
         validators=[PresentInDatabaseValidator(Milgroup)])
-    milfaculty = CharField(
-        required=False,
-        validators=[PresentInDatabaseValidator(Milfaculty, 'milfaculty')])
-    rank = CharField(required=False,
-                     validators=[PresentInDatabaseValidator(Rank, 'rank')])
-    teacher_post = CharField(
-        required=False,
-        validators=[PresentInDatabaseValidator(TeacherPost, 'teacher_post')])
 
     fullname = SerializerMethodField(required=False)
 
@@ -36,10 +25,3 @@ class TeacherSerializer(NestedModelSerializer):
     def get_fullname(self, obj):
         # pylint: disable=(no-self-use)
         return f'{obj.surname} {obj.name} {obj.patronymic}'
-
-    nested_fields = [
-        ['milgroup', Milgroup],
-        ['milfaculty', Milfaculty, 'milfaculty'],
-        ['rank', Rank, 'rank'],
-        ['teacher_post', TeacherPost, 'teacher_post'],
-    ]
