f5741966b6bb98fd25e6bcdf9949712675cd2efc
TmLev <leva.bugatti@gmail.com>
2020-10-12 12:35:26 +0300

    refactor: bump atomic changes for papers

--- front-end/src/api/documents.js
+++ front-end/src/api/documents.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function getDocuments(category, authors, published_places, start_date, end_date, text) {
   return request({
-    url: '/dms/documents/',
+    url: '/dms/papers/',
     method: 'get',
     params : {category: category, authors: authors, publishers: published_places, start_date: start_date, end_date: end_date, search: text}
   })
