7b7724eec5346be2d5b161872b7266834f13f76c
TmLev <leva.bugatti@gmail.com>
2020-10-15 21:36:29 +0300

    feat: add simple test

--- back-end/dms/tests.py
+++ back-end/dms/tests.py
@@ -1,2 +1,11 @@
-def test_placeholder():
-    pass
+import pytest
+
+
+@pytest.mark.django_db
+def test_trailing_slash_redirect(client):
+    response = client.get("/api/dms/authors")
+    assert response.status_code == 301
+
+    response = client.get("/api/dms/authors/")
+    assert response.status_code == 200
+    assert not response.data
