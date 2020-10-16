457ef57fd437149360a628a4bcab759c2eefa083
Vladis <veisakov@miem.hse.ru>
2020-10-16 10:57:43 +0300

    Move serializer logic to list() method in ReferenceBookView

--- back-end/lms/urls.py
+++ back-end/lms/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from lms.views.viewsets import StudentViewSet, TeacherViewSet
-from lms.views.views import ReferenceBookView
+from lms.views.reference_book import ReferenceBookView
 from lms.views.absence import AbsenceViewSet, AbsenceJournalView
 
 routers = DefaultRouter()
