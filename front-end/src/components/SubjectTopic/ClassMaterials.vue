5395bfff139c48dfd4e9fe02955f8c043f04b4b2
TmLev <leva.bugatti@gmail.com>
2020-10-19 21:44:09 +0300

    refactor: extract class materials component

---
+++ front-end/src/components/SubjectTopic/ClassMaterials.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="class-materials file-group" v-show="hasMaterials">
+    <div class="class-materials-title file-section-title" @click="toggleFiles">
+      <div>{{ title }}</div>
+      <img src="../../assets/scienceWorks/dropdown.svg" alt="" class="files-dropdown">
+    </div>
+
+    <div class="files" style="display: none">
+      <div class="file" v-for="(material, index) in materials" :key="index">
+        <div class="file-icon"><img src="../../assets/subject/iconPowerPoint.svg" alt="" class="kebab"></div>
+        <div class="file-title">{{ material.title }}</div>
+
+        <div class="file-kebab">
+          <el-popover placement="bottom" trigger="click">
+            <div style="text-align: center; margin: 0; padding: 0; font-size: 15px;">
+              <div style="cursor:pointer;">Действие 1</div>
+              <div style="cursor:pointer;">Действие 2</div>
+            </div>
+
+            <div slot="reference" class="d-flex justify-content-center" style="width: 10px; cursor: pointer">
+              <img src="../../assets/subject/greyKebab.svg" alt="" class="kebab">
+            </div>
+          </el-popover>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    materials: {
+      type: Array,
+      required: true,
+    }
+  },
+
+  methods: {
+    toggleFiles(event) {
+      if (event.target.closest('.file-group').children[1].style.display === 'none') {
+        event.target.closest('.file-group').children[1].style.display = 'flex'
+        event.target.closest('.file-section-title').children[1].style.transform = 'rotate(180deg)'
+      } else {
+        event.target.closest('.file-group').children[1].style.display = 'none'
+        event.target.closest('.file-section-title').children[1].style.transform = 'rotate(0deg)'
+      }
+    }
+  },
+
+  computed: {
+    hasMaterials() {
+      return this.materials && this.materials.length > 0
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "style";
+</style>
--- front-end/src/components/SubjectTopic/SubjectTopic.vue
+++ front-end/src/components/SubjectTopic/SubjectTopic.vue
@@ -8,146 +8,32 @@
                 <div>Тема №{{index + 1}}</div>
                 <div class="topic-download-all">Скачать все материалы архивом</div>
             </div>
+
             <div class="topic-content">
                 {{title}}
             </div>
+
             <div class="topic-files">
-                <div class="lectures file-group" v-if="lectures.length !== 0">
-                    <div class="lectures-title file-section-title" @click="toggleFiles">
-                        <div>Лекции</div>
-                        <img src="../../assets/scienceWorks/dropdown.svg" alt="" class="files-dropdown">
-                    </div>
-                    <div class="files" style="display: none">
-                        <div class="file" v-for="(lecture, index) in lectures" :key="index">
-                            <a :href="`https://military-dms-back.herokuapp.com${lecture.file}`" download><div class="file-icon"><img src="../../assets/subject/iconPowerPoint.svg" alt="" class="kebab"></div></a>
-                            <div class="file-title">{{lecture.title}}</div>
-                            <div class="file-kebab">
-                                <el-popover
-                                        placement="bottom"
-                                        trigger="click"
-                                >
-                                    <div style="text-align: center; margin: 0; padding: 0; font-size: 15px;">
-                                        <div style="cursor:pointer;">Действие 1</div>
-                                        <div style="cursor:pointer;">Действие 2</div>
-                                    </div>
-                                    <div slot="reference" class="d-flex justify-content-center" style="width: 10px; cursor: pointer">
-                                        <img src="../../assets/subject/greyKebab.svg" alt="" class="kebab">
-                                    </div>
-                                </el-popover>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="seminars file-group" v-if="seminars.length !== 0">
-                    <div class="seminars-title file-section-title" @click="toggleFiles">
-                        <div>Семинары</div>
-                        <img src="../../assets/scienceWorks/dropdown.svg" alt="" class="files-dropdown">
-                    </div>
-                    <div class="files" style="display: none">
-                        <div class="file" v-for="(seminar, index) in seminars" :key="index">
-                            <div class="file-icon"><img src="../../assets/subject/iconPowerPoint.svg" alt="" class="kebab"></div>
-                            <div class="file-title">{{seminar.title}}</div>
-                            <div class="file-kebab">
-                                <el-popover
-                                        placement="bottom"
-                                        trigger="click"
-                                >
-                                    <div style="text-align: center; margin: 0; padding: 0; font-size: 15px;">
-                                        <div style="cursor:pointer;">Действие 1</div>
-                                        <div style="cursor:pointer;">Действие 2</div>
-                                    </div>
-                                    <div slot="reference" class="d-flex justify-content-center" style="width: 10px; cursor: pointer">
-                                        <img src="../../assets/subject/greyKebab.svg" alt="" class="kebab">
-                                    </div>
-                                </el-popover>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="group-classes file-group" v-if="groups.length !== 0">
-                    <div class="group-classes-title file-section-title" @click="toggleFiles">
-                        <div>Групповые занятия</div>
-                        <img src="../../assets/scienceWorks/dropdown.svg" alt="" class="files-dropdown">
-                    </div>
-                    <div class="files" style="display: none">
-                        <div class="file" v-for="(group_class, index) in groups" :key="index">
-                            <div class="file-icon"><img src="../../assets/subject/iconPowerPoint.svg" alt="" class="kebab"></div>
-                            <div class="file-title">{{group_class.title}}</div>
-                            <div class="file-kebab">
-                                <el-popover
-                                        placement="bottom"
-                                        trigger="click"
-                                >
-                                    <div style="text-align: center; margin: 0; padding: 0; font-size: 15px;">
-                                        <div style="cursor:pointer;">Действие 1</div>
-                                        <div style="cursor:pointer;">Действие 2</div>
-                                    </div>
-                                    <div slot="reference" class="d-flex justify-content-center" style="width: 10px; cursor: pointer">
-                                        <img src="../../assets/subject/greyKebab.svg" alt="" class="kebab">
-                                    </div>
-                                </el-popover>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="practice-classes file-group" v-if="practices.length !== 0">
-                    <div class="practice-classes-title file-section-title" @click="toggleFiles">
-                        <div>Практические занятия</div>
-                        <img src="../../assets/scienceWorks/dropdown.svg" alt="" class="files-dropdown">
-                    </div>
-                    <div class="files" style="display: none">
-                        <div class="file" v-for="(lecture, index) in practices" :key="index">
-                            <div class="file-icon"><img src="../../assets/subject/iconPowerPoint.svg" alt="" class="kebab"></div>
-                            <div class="file-title">{{lecture.title}}</div>
-                            <div class="file-kebab">
-                                <el-popover
-                                        placement="bottom"
-                                        trigger="click"
-                                >
-                                    <div style="text-align: center; margin: 0; padding: 0; font-size: 15px;">
-                                        <div style="cursor:pointer;">Действие 1</div>
-                                        <div style="cursor:pointer;">Действие 2</div>
-                                    </div>
-                                    <div slot="reference" class="d-flex justify-content-center" style="width: 10px; cursor: pointer">
-                                        <img src="../../assets/subject/greyKebab.svg" alt="" class="kebab">
-                                    </div>
-                                </el-popover>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <ClassMaterials title="Лекции" :materials="lectures" />
+              <ClassMaterials title="Семинары" :materials="seminars" />
+              <ClassMaterials title="Групповые занятия" :materials="groups" />
+              <ClassMaterials title="Практические занятия" :materials="practices" />
             </div>
         </div>
     </div>
 </template>
 
 <script>
-  import moment from 'moment'
+import ClassMaterials from "./ClassMaterials";
 
-  export default {
-    name: '',
-    components: {},
-    props: ['data'],
-    data() {
-      return {
-
-      }
-    },
-    methods: {
-      toggleFiles(event) {
-        if (event.target.closest('.file-group').children[1].style.display === 'none') {
-          event.target.closest('.file-group').children[1].style.display = 'flex'
-          event.target.closest('.file-section-title').children[1].style.transform = 'rotate(180deg)'
-        } else {
-          event.target.closest('.file-group').children[1].style.display = 'none'
-          event.target.closest('.file-section-title').children[1].style.transform = 'rotate(0deg)'
-        }
-
-      }
-    },
-    mounted() {
-    }
-  }
+export default {
+  name: '',
+  components: {ClassMaterials},
+  props: ['data'],
+  data() {
+    return {}
+  },
+}
 </script>
 
 <style scoped lang="scss">
