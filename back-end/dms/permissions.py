2004281a78b71efba7938c6d6539e89eda2db7fa
TmLev <leva.bugatti@gmail.com>
2020-10-21 12:59:39 +0300

    refactor: split permissions

--- back-end/dms/permissions.py
+++ back-end/dms/permissions.py
@@ -1,10 +1,13 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class ReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
+        return request.method in permissions.SAFE_METHODS
 
+
+class IsOwner(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
         return obj.user == request.user
