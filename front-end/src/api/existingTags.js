6549c8954aa0aa881cc6630454279726817d300c
TmLev <leva.bugatti@gmail.com>
2020-10-29 18:24:54 +0300

    fix: remove mapping from tags request

--- front-end/src/api/existingTags.js
+++ front-end/src/api/existingTags.js
@@ -5,10 +5,5 @@ export function getExistingTags(params) {
     url: '/dms/tags/',
     method: 'get',
     params
-  }).then(response => {
-      return response.data.map(tag => ({
-          key: tag,
-          value: tag,
-      }))
   })
 }
