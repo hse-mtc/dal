870ab902998f76cab5bac51cc0fd0a42ed719cf6
darkydash <darkydashie@gmail.com>
2020-10-30 10:14:35 +0300

    Fix auth.serializers codestyle

--- back-end/auth/serializers.py
+++ back-end/auth/serializers.py
@@ -4,12 +4,14 @@ from django.contrib.auth import get_user_model
 
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = get_user_model()
         fields = ["id", "username"]
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Profile
         fields = "__all__"
