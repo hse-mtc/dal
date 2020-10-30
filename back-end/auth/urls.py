5bf372d81bfaffbe697d6f9a53608c1d8efb86e5
darkydash <darkydashie@gmail.com>
2020-10-30 12:24:10 +0300

    Add /profiles api endpoint

--- back-end/auth/urls.py
+++ back-end/auth/urls.py
@@ -5,13 +5,11 @@ from rest_framework_simplejwt.views import (
     token_refresh,
 )
 
-from auth.views import (
-    info,
-    logout,
-)
+from auth.views import (info, logout, ProfileViewSet)
 
 urlpatterns = [
     path("users/info/", info),
+    path("profiles/", ProfileViewSet.as_view()),
     path("users/logout/", logout),
     path("tokens/obtain/", token_obtain_pair),
     path("tokens/refresh/", token_refresh),
--- back-end/auth/views.py
+++ back-end/auth/views.py
@@ -1,7 +1,9 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework import permissions
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
@@ -13,6 +15,9 @@ from rest_framework.decorators import (
 from drf_yasg.utils import swagger_auto_schema
 
 from auth.swagger import TOKEN_AUTH_HEADER
+from auth.models import Profile
+from auth.serializers import ProfileSerializer
+from django.shortcuts import get_object_or_404
 
 
 @swagger_auto_schema(method="GET", manual_parameters=[TOKEN_AUTH_HEADER])
@@ -27,6 +32,20 @@ def info(request: Request) -> Response:
     }
 
     return Response(data, status=HTTP_200_OK)
+
+
+@method_decorator(
+    name="get",
+    decorator=swagger_auto_schema(manual_parameters=[TOKEN_AUTH_HEADER]))
+class ProfileViewSet(RetrieveAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = get_object_or_404(queryset, user=self.request.user)
+        self.check_object_permissions(self.request, obj)
+        return obj
 
 
 @csrf_exempt
