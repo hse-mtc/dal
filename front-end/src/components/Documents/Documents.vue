9227e3adde7e547d3e3c5d596ea3dd9ebcc20921
TmLev <leva.bugatti@gmail.com>
2020-11-02 23:22:59 +0300

    fix: adjust documents, scienceworks components to use new modal

--- front-end/src/components/Documents/Documents.vue
+++ front-end/src/components/Documents/Documents.vue
@@ -1,14 +1,8 @@
 <template>
   <div>
     <div v-if="documents.length !== 0" class="my-document">
-      <div v-if="count > 5" class="count">
-        {{ count }} документов
-      </div>
-      <div v-else-if="count > 1">
-        {{ count }} документа
-      </div>
-      <div v-else-if="count > 0">
-        {{ count }} документ
+      <div class="count">
+        {{ count }} {{ "документ" + (count > 5 ? "ов" : count > 1 ? "а" : "") }}
       </div>
 
       <div v-for="group in documents" :key="group.year">
@@ -17,25 +11,32 @@
           {{ group.year }}
           <hr class="ml-3">
         </div>
+
         <el-row v-for="(document, index) in group.documents" :key="document.id" class="document-card mt-3 mb-4">
           <el-col :span="2" style="font-size: 22px" class="mt-4">
             № {{ index + 1 }}
           </el-col>
+
           <el-col :span="21">
             <div class="d-flex">
               <div>
                 {{ document.publication_date | moment }}
               </div>
+
               <div class="ml-5" style="color: #76767A">
                 <span v-for="(publisher, index) in document.publishers" :key="index">{{ publisher.name }} </span>
               </div>
             </div>
+
             <div class="document-card-title">{{ document.title }}</div>
+
             <div v-for="(author, index) in document.authors" :key="index" class="document-card-authors">
               {{ `${author.last_name} ${author.first_name[0]}. ${author.patronymic[0]}.` }}
             </div>
+
             <div class="document-card-annotation">{{ document.annotation }}</div>
           </el-col>
+
           <el-col :span="1" class="d-flex justify-content-end mt-4">
             <el-popover
                 placement="bottom"
@@ -43,9 +44,10 @@
             >
               <div style="text-align: center; margin: 0; padding: 0; font-size: 15px;">
                 <div style="cursor:pointer;" @click.prevent="downloadFile(document.file)">Скачать</div>
-                <div style="cursor:pointer;" @click="editDocument(document.id)">Редактировать</div>
-                <div style="cursor:pointer;" @click="deleteArticle(document.id)">Удалить</div>
+                <div style="cursor:pointer;" @click="editPaper(document.id)">Редактировать</div>
+                <div style="cursor:pointer;" @click="deletePaper(document.id)">Удалить</div>
               </div>
+
               <div slot="reference" class="d-flex justify-content-center" style="width: 10px; cursor: pointer">
                 <img src="../../assets/scienceWorks/popover.svg" alt="">
               </div>
@@ -54,150 +56,132 @@
         </el-row>
       </div>
     </div>
+
     <div v-else class="my-document">
       Документы не найдены
     </div>
-    <EditDocumentModalWindow v-if="editModal" v-on:closeModal="closeModal" :document="currentEditDocument"/>
   </div>
 </template>
 
 <script>
+import axios from "axios"
+import moment from 'moment'
+import groupBy from 'lodash/groupBy'
+import keys from 'lodash/keys'
+
 import {getDocuments} from '@/api/documents'
 import {deleteDocument} from '@/api/delete'
-import EventBus from '../EventBus';
-import {baseURL} from '@/utils/request';
-import moment from 'moment'
-import groupBy from 'lodash/groupBy';
-import keys from 'lodash/keys';
-import EditDocumentModalWindow from "@/components/EditDocumentModalWindow/EditDocumentModalWindow";
-import axios from "axios";
+
+import EventBus from '../EventBus'
 
 export default {
   name: '',
-  components: {EditDocumentModalWindow},
+
   filters: {
     moment: function (date) {
       return moment(date).format('DD MMMM YYYY')
     }
   },
+  watch: {
+    '$route': 'fetchData',
+  },
+
   data() {
     return {
       documents: [],
       count: undefined,
-      currentEditDocument: {},
+      paperToEdit: {},
     }
   },
-  watch: {
-    '$route'(to, from) {
-      this.fetchData()
-    }
-  },
-  props: {
-    editModal: {
-      type: Boolean,
-      required: true
-    }
-  },
-  created() {
-  },
-  mounted() {
-    this.fetchData()
+
+  async mounted() {
+    await this.fetchData()
     const self = this
-    EventBus.$on('UPDATE_EVENT', function () {
+    EventBus.$on('UPDATE_EVENT', () => {
       self.fetchData()
     })
   },
-  computed: {
-    download() {
-      return `${baseURL}/dms/get_file`
-    }
-  },
+
   methods: {
-    downloadFile(file) {
-      axios.get(
-        file.content,
-        {responseType: 'blob'},
-      ).then(response => {
-        let create = document.createElement.bind(document)
-        const link = create('a')
+    moment,
 
-        const blob = new Blob([response.data])
-        link.href = URL.createObjectURL(blob)
-        link.download = file.name
-
-        link.click()
-        URL.revokeObjectURL(link.href)
-      }).catch(console.error)
-    },
-    openModal() {
-      this.$emit('openEditModal');
-    },
-    closeModal() {
-      this.$emit('closeEditModal');
-    },
-    editDocument(id) {
-      let allDocs = []
-      this.documents.forEach(item => allDocs = [...allDocs, ...item.documents]);
-      this.currentEditDocument = allDocs.find(item => item.id === id)
-      this.openModal()
-    },
-    deleteArticle(id) {
-      const deletedId = {
-        'id': id
+    async downloadFile(file) {
+      let data
+      try {
+        data = (await axios.get(file.content, {responseType: 'blob'})).data
+      } catch (error) {
+        console.log("Failed to download Paper.file: ", error)
+        return
       }
-      this.$confirm('Вы уверены?', 'Подтвердите действие', {
+
+      const create = document.createElement.bind(document)
+      const link = create('a')
+
+      const blob = new Blob([data])
+      link.href = URL.createObjectURL(blob)
+      link.download = file.name
+
+      link.click()
+      URL.revokeObjectURL(link.href)
+    },
+
+    editPaper(id) {
+      const allDocs = this.documents.map(group => group.documents).flat(1)
+      const paperToEdit = allDocs.find(paper => paper.id === id)
+      this.$emit("openPaperModal", "edit", paperToEdit)
+    },
+
+    async deletePaper(id) {
+      await this.$confirm('Вы уверены?', 'Подтвердите действие', {
         confirmButtonText: 'Удалить',
         cancelButtonText: 'Отменить',
         type: 'warning'
       })
-          .then(() => {
-            deleteDocument(deletedId).then(response => {
-              this.documents.forEach(group => {
-                group.documents = group.documents.filter(i => {
-                  return i.id !== id
-                })
-              })
-              this.documents = this.documents.filter(group => {
-                return group.documents.length !== 0
-              })
-              console.log('файл удален')
-              this.count = this.count - 1
-            }).catch(() => {
-              console.log('Ошибка удаления файла')
-            })
-            this.$message({
-              type: 'success',
-              message: 'Удаление завершено'
-            })
-          })
-          .catch(() => {
-          })
 
+      try {
+        await deleteDocument(id)
+      } catch (error) {
+        console.log("Failed to delete Paper: ", error)
+        return
+      }
 
+      this.documents.forEach(group => {
+        group.documents = group.documents.filter(paper => paper.id !== id)
+      })
+      this.documents = this.documents.filter(group => group.documents.length)
+      this.count = this.count - 1
+
+      this.$message({
+        type: 'success',
+        message: 'Удаление завершено'
+      })
     },
-    fetchData() {
+
+    async fetchData() {
       let author = this.$route.query.author ? this.$route.query.author : null
       let place = this.$route.query.place ? this.$route.query.place : null
       let start_date = this.$route.query.start_date ? this.$route.query.start_date : null
       let end_date = this.$route.query.end_date ? this.$route.query.end_date : null
       let text = this.$route.query.text ? this.$route.query.text : null
       let category = this.$route.query.category ? this.$route.query.category : null
-      getDocuments(category, author, place, start_date, end_date, text).then(response => {
-        let groupsByYear = groupBy(response.data, function (document) {
-          return moment(document.publication_date).year()
-        })
-        this.documents = keys(groupsByYear).sort().reverse().map(year => ({
-          year: year,
-          documents: groupsByYear[year],
-        }))
-        this.count = response.data.length
-      }).catch(() => {
-        console.log('Данные по документам не указаны')
+
+      let papers
+      try {
+        papers = (await getDocuments(category, author, place, start_date, end_date, text)).data
+      } catch (error) {
+        console.log("Failed to fetch Papers: ", error)
+        return
+      }
+
+      const groupsByYear = groupBy(papers, (paper) => {
+        return moment(paper.publication_date).year()
       })
+      this.documents = keys(groupsByYear).sort().reverse().map((year) => ({
+        year: year,
+        documents: groupsByYear[year],
+      }))
+      this.count = papers.length
     },
-    moment: function () {
-      return moment()
-    }
   },
 }
 
--- front-end/src/components/ScienceWorks/ScienceWorks.vue
+++ front-end/src/components/ScienceWorks/ScienceWorks.vue
@@ -2,122 +2,134 @@
   <div>
     <el-col :span="24" class="scienceWork">
       <el-row class="pageTitle">
-        <el-col  :offset="2">
+        <el-col :offset="2">
           Военно-научная работа
         </el-col>
       </el-row>
+
       <div class="categories-block" v-if="categories.length">
         <el-row class="select-work">
           <el-col :span="10" :offset="2">
-            <span class="category-selected" @click="openAllCategories">{{this.selectedCategory.title}}</span>
-            <img id="dark-arrow" src="../../assets/scienceWorks/dropdownBlack.svg" alt="" class="ml-2" style="cursor:pointer;" @click="openAllCategories">
+            <span class="category-selected" @click="openAllCategories">{{ category.title }}</span>
+            <img id="dark-arrow" src="../../assets/scienceWorks/dropdownBlack.svg" alt="" class="ml-2"
+                 style="cursor:pointer;" @click="openAllCategories">
           </el-col>
+
           <el-col :span="7">
-            <div class="add-document-top" @click="openModal"><span>Добавить новый документ</span></div>
+            <div class="add-document-top" @click="openPaperModal('add')">
+              Добавить новый документ
+            </div>
           </el-col>
         </el-row>
+
         <el-row v-if="modalCategories" class="categories-selector">
           <el-col :offset="2" :span="20" class="categories-title">
             <el-row style="width: 100%">
-              <el-col :span="12" v-for="(category, index) in categories" :key="category.id" class="category-title" >
-                <div><span style="cursor:pointer;" :id="category.id" @click="selectCategory($event)">{{category.title}}</span> <img @click="deleteCategory(category.id)" class="category-delete ml-2" height="10px" src="../../assets/scienceWorks/close.svg" alt=""></div>
+              <el-col :span="12" v-for="category in categories" :key="category.id" class="category-title">
+                <div>
+                  <span style="cursor:pointer;" :id="category.id"
+                        @click="selectCategory(category.id)">
+                    {{ category.title }}
+                  </span>
+                  <img @click="deleteCategory(category.id)"
+                       class="category-delete ml-2" height="10px"
+                       src="../../assets/scienceWorks/close.svg" alt=""
+                  />
+                </div>
               </el-col>
+
               <el-col :span="12" class="category-title" style="color: #0050B2">
                 <span @click="addNewPaperCategory" style="cursor: pointer">Добавить новую категорию</span>
               </el-col>
             </el-row>
           </el-col>
+
           <el-col :span="1" class="cross-col">
             <img src="../../assets/scienceWorks/cross.svg" alt="" @click="closeSelectCategory">
           </el-col>
         </el-row>
       </div>
-<!--      <el-row class="selectWork">-->
-<!--        <el-col :span="4">-->
-<!--          <div id="scienceArticles" class="selectWork-item selected" @click="selectClick($event)">-->
-<!--            Научные статьи-->
-<!--          </div>-->
-<!--        </el-col>-->
-<!--        <el-col :span="8" :offset="1">-->
-<!--          <div id="scienceWorks" class="selectWork-item" @click="selectClick($event)">-->
-<!--            Научно-исследовательские работы-->
-<!--          </div>-->
-<!--        </el-col>-->
-<!--      </el-row>-->
+
       <el-row class="search ">
         <el-col :span="12" :offset="2">
-          <Search placeholder="Введите ключевые слова" />
-<!--          <div class="add-document" @click="openModal">+ <span>Добавить публикацию</span></div>-->
-          <AdvancedSearch class="advanced-search" />
-          <Documents class="documents" :editModal="editModal" v-on:openEditModal="openEditModal" v-on:closeEditModal="closeEditModal"/>
+          <Search placeholder="Введите ключевые слова"/>
+          <AdvancedSearch class="advanced-search"/>
+          <Documents
+              class="documents"
+              v-on:openPaperModal="openPaperModal"
+          />
         </el-col>
+
         <el-col :offset="1" :span="8">
           <FunctionalCalendar
-                  v-model="calendarData"
-                  :is-date-range="true"
-                  :change-month-function="true"
-                  :change-year-function="true"
+              v-model="calendarData"
+              :is-date-range="true"
+              :change-month-function="true"
+              :change-year-function="true"
           />
         </el-col>
       </el-row>
     </el-col>
-    <AddCategoryModalWindow v-if="addNewCategory"/>
-    <AddModalWindow v-if="addModal" v-on:closeModal="closeModal" />
-    <div v-if="addModal || addNewCategory || editModal" class="background" @click="closeModal"></div>
+
+    <AddCategoryModalWindow
+        v-if="addNewCategory"
+        v-on:closeModal="closeModal"
+    />
+    <UpsertPaperModal
+        v-if="paperAction"
+        :action="paperAction"
+        :paper="paperToEdit"
+        v-on:closeModal="closeModal"
+    />
+
+    <div v-if="paperAction || addNewCategory" class="background" @click="closeModal"></div>
   </div>
 </template>
 
 <script>
-  import { mapState, mapActions } from 'vuex'
-  import Search from '../Search/Search'
-  import AdvancedSearch from '../AdvancedSearch/AdvancedSearch'
-  import Documents from '../Documents/Documents'
-  import { FunctionalCalendar } from 'vue-functional-calendar'
-  import AddModalWindow from "../AddModalWindow/AddModalWindow";
-  import {getPaperCategories, deletePaperCategories} from "../../api/paper_categories";
-  import AddCategoryModalWindow from "../AddCategoryModalWindow/AddCategoryModalWindow";
-  import EventBus from "../EventBus";
+import {mapState, mapActions} from 'vuex'
+import {FunctionalCalendar} from 'vue-functional-calendar'
+
+import Documents from '@/components/Documents/Documents'
+import UpsertPaperModal from "@/components/Documents/UpsertPaperModal"
+
+import Search from '@/components/Search/Search'
+import AdvancedSearch from '@/components/AdvancedSearch/AdvancedSearch'
+import AddCategoryModalWindow from "@/components/AddCategoryModalWindow/AddCategoryModalWindow"
+
+import {getPaperCategories, deletePaperCategory} from "@/api/paper_categories"
+import EventBus from "@/components/EventBus"
 
 export default {
   name: '',
   components: {
     AddCategoryModalWindow,
-    AddModalWindow,
-    Search,
     AdvancedSearch,
     Documents,
-    FunctionalCalendar
+    FunctionalCalendar,
+    Search,
+    UpsertPaperModal,
   },
+
   data() {
     return {
       calendarData: undefined,
+
       documents: [],
       count: undefined,
-      addModal: false,
-      editModal: false,
-      selectedCategory: {},
+
+      paperAction: "",
+      paperToEdit: {},
+
+      category: {},
       modalCategories: false,
       addNewCategory: false,
-      // categories: [
-      //   {
-      //     title: 'Научно-исследовательские работы',
-      //     id: 1
-      //   },
-      //   {
-      //     title: 'Научные статьи',
-      //     id: 2
-      //   },
-      //   {
-      //     title: 'Хер',
-      //     id: 3
-      //   }
-      // ]
     }
   },
-  created() {
-    this.$router.replace({ name: 'Science Articles', query: { category: "1" }})
-    this.fetchData()
 
+  created() {
+    this.$router.replace({name: 'Science Articles', query: {category: "1"}})
+    this.fetchData()
   },
   mounted() {
     const self = this
@@ -125,23 +137,41 @@ export default {
       self.fetchData()
     })
   },
+
   computed: {
     ...mapState({
       categories: state => state.documents.categories,
     })
   },
+
   methods: {
     ...mapActions({
       setCategories: 'documents/setCategories',
     }),
-    openEditModal() {
-      this.editModal = true
-      document.getElementById('main-container').classList.add('stop-scrolling')
-    },
-    closeEditModal() {
-      this.editModal = false
+
+    startScrolling() {
       document.getElementById('main-container').classList.remove('stop-scrolling')
     },
+    stopScrolling() {
+      document.getElementById('main-container').classList.add('stop-scrolling')
+    },
+
+    openPaperModal(action, paperToEdit = {}) {
+      console.log("Open Paper Modal: action = ", action, " to edit = ", paperToEdit)
+      this.paperAction = action
+      this.paperToEdit = paperToEdit
+      this.stopScrolling()
+    },
+
+    // Any modal (paper or category)
+    closeModal() {
+      console.log("Close modal")
+      this.paperAction = ""
+      this.paperToEdit = {}
+      this.addNewCategory = false
+      this.startScrolling()
+    },
+
     closeSelectCategory() {
       this.modalCategories = false
       this.modalCategories ? document.getElementById('dark-arrow').style.transform = 'rotate(180deg)' : document.getElementById('dark-arrow').style.transform = 'rotate(0deg)'
@@ -150,69 +180,50 @@ export default {
       this.modalCategories = !this.modalCategories
       this.modalCategories ? document.getElementById('dark-arrow').style.transform = 'rotate(180deg)' : document.getElementById('dark-arrow').style.transform = 'rotate(0deg)'
     },
-    deleteCategory(id) {
-      this.$confirm('Вы уверены?', 'Подтвердите действие', {
+    async deleteCategory(id) {
+      await this.$confirm('Вы уверены?', 'Подтвердите действие', {
         confirmButtonText: 'Удалить',
         cancelButtonText: 'Отменить',
         type: 'warning'
       })
-              .then(() => {
-                deletePaperCategories({id: id}).then(response => {
-                  this.categories.filter(item => {
-                    item.id !== id
-                  })
-                  console.log('Категория удалена')
-                  this.fetchData()
-                  this.$message({
-                    type: 'success',
-                    message: 'Удаление завершено'
-                  })
-                }).catch(() => {
-                  console.log('Ошибка удаления категории')
-                })
-              })
-              .catch(() => { })
+
+      try {
+        await deletePaperCategory(id)
+      } catch (error) {
+        console.log("Failed to delete Category: ", error)
+        return
+      }
+
+      await this.fetchData()
+      this.$message({
+        type: 'success',
+        message: 'Удаление завершено'
+      })
     },
     addNewPaperCategory() {
       this.addNewCategory = true
     },
-    fetchData() {
-      getPaperCategories()
-              .then(response => {
-                this.setCategories(response.data)
-                this.selectedCategory = response.data[0]
-              }).catch(() => {
-                 console.log('Данных по категориям нет')
-              })
-    },
-    closeModal() {
-      this.addModal = false
-      this.addNewCategory = false
-      this.editModal = false
-      document.getElementById('main-container').classList.remove('stop-scrolling')
-    },
-    openModal() {
-      this.addModal = true
-      document.getElementById('main-container').classList.add('stop-scrolling')
-    },
-    selectCategory(event) {
-      this.selectedCategory =  {
-        title: this.categories.filter(item => item.id == event.target.id)[0].title,
-        id: event.target.id
+
+    async fetchData() {
+      try {
+        const categories = (await getPaperCategories()).data
+        this.setCategories(categories)
+        this.category = categories[0]
+      } catch (error) {
+        console.log("Failed to fetch Categories: ", error)
       }
-      this.$router.replace({ name: 'Science Articles', query: { category: event.target.id.toString() }})
+    },
+
+    selectCategory(id) {
+      this.category = this.categories.find(category => category.id === id)
+      this.$router.replace({name: 'Science Articles', query: {category: id.toString()}})
       this.modalCategories = false
       this.modalCategories ? document.getElementById('dark-arrow').style.transform = 'rotate(180deg)' : document.getElementById('dark-arrow').style.transform = 'rotate(0deg)'
     },
-    selectClick(event) {
-      Array.from(document.getElementsByClassName(event.target.className)).forEach(item => item.classList.remove('selected'))
-      event.target.classList.add('selected')
-      this.$router.replace({ name: 'Science Articles', query: { category: event.target.id.toString() }})
-    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-  @import "style";
+@import "style";
 </style>
