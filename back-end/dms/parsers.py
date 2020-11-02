99b756293c23769a5c017eb7e22355f81add9107
TmLev <leva.bugatti@gmail.com>
2020-11-02 23:21:08 +0300

    fix: process list in parser separately

--- back-end/dms/parsers.py
+++ back-end/dms/parsers.py
@@ -8,5 +8,10 @@ class MultiPartWithJSONParser(MultiPartParser):
     def parse(self, stream, media_type=None, parser_context=None):
         daf = super().parse(stream, media_type, parser_context)
         daf.data = daf.data.copy()
-        daf.data.update(json.loads(daf.data.pop("data")[0]))
+        json_data = json.loads(daf.data.pop("data")[0])
+        for key, value in json_data.items():
+            if isinstance(value, list):
+                daf.data.setlist(key, value)
+            else:
+                daf.data[key] = value
         return daf
