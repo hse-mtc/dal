774a2f953eba3821ab2b5b9803648fa1c894bf82
TmLev <leva.bugatti@gmail.com>
2020-11-02 23:29:01 +0300

    style: apply pylint

--- back-end/dms/parsers.py
+++ back-end/dms/parsers.py
@@ -6,12 +6,16 @@ from rest_framework.parsers import MultiPartParser
 class MultiPartWithJSONParser(MultiPartParser):
 
     def parse(self, stream, media_type=None, parser_context=None):
+        # pylint: disable=too-many-locals
+
         daf = super().parse(stream, media_type, parser_context)
         daf.data = daf.data.copy()
         json_data = json.loads(daf.data.pop("data")[0])
+
         for key, value in json_data.items():
             if isinstance(value, list):
                 daf.data.setlist(key, value)
             else:
                 daf.data[key] = value
+
         return daf
