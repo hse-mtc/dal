6580450f91d13c7207ccdfb5e8819dc3b27ed9bc
TmLev <leva.bugatti@gmail.com>
2020-11-02 23:20:32 +0300

    refactor: simpler category modal

--- front-end/src/components/AddCategoryModalWindow/AddCategoryModalWindow.vue
+++ front-end/src/components/AddCategoryModalWindow/AddCategoryModalWindow.vue
@@ -1,52 +1,57 @@
 <template>
-    <div class="addModal">
-        <el-form ref="form" :model="form" label-width="175px">
-            <el-form-item label="Название категории">
-                <el-input v-model="form.title" placeholder="Введите название"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="onSubmit">Добавить</el-button>
-                <el-button @click="closeModal">Отменить</el-button>
-            </el-form-item>
-        </el-form>
-    </div>
+  <div class="addModal">
+    <el-form ref="form" :model="form" label-width="175px">
+      <el-form-item label="Название категории">
+        <el-input placeholder="Введите название" v-model="form.title"/>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Добавить</el-button>
+        <el-button @click="closeModal">Отменить</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-    import EventBus from '../EventBus';
-    import {addPaperCategories} from "../../api/paper_categories";
+import {addPaperCategories} from "@/api/paper_categories"
 
-    export default {
-        name: "AddCategoryModalWindow",
-        data() {
-            return {
-                form: {
-                    title: ''
-                }
-            }
-        },
-        created() {
-        },
-        updated() {
-        },
-        methods: {
-            onSubmit() {
-                const self = this
-                if (this.form.title.trim() !== '') {
-                    addPaperCategories(this.form.title)
-                        .then(function(){
-                            EventBus.$emit('UPDATE_CATEGORY');
-                            self.$emit('closeModal');
-                        })
-                }
-            },
-            closeModal() {
-                this.$emit('closeModal');
-            }
-        }
+import EventBus from '../EventBus'
+
+export default {
+  name: "AddCategoryModalWindow",
+
+  data() {
+    return {
+      form: {
+        title: ''
+      }
     }
+  },
+
+  methods: {
+    async onSubmit() {
+      const title = this.form.title.trim()
+      if (title === "") {
+        return
+      }
+
+      try {
+        await addPaperCategories({title})
+        EventBus.$emit('UPDATE_CATEGORY')
+        this.closeModal()
+      } catch (error) {
+        console.log("Failed to add Category: ", error)
+      }
+    },
+
+    closeModal() {
+      this.$emit("closeModal")
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss">
-    @import "style.scss";
+@import "style.scss";
 </style>
