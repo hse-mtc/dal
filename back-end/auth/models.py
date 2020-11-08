05f2f215bc9267c54e03042d96a67e084796312c
TmLev <leva.bugatti@gmail.com>
2020-11-08 18:42:00 +0300

    refactor: inherit profile from person

--- back-end/auth/models.py
+++ back-end/auth/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from common.models import Person
 
-class Profile(models.Model):
-    name = models.CharField(max_length=255)
-    user = models.OneToOneField(
-        to=get_user_model(),
-        on_delete=models.CASCADE,
-    )
+
+class Profile(Person):
+    user = models.OneToOneField(to=get_user_model(), on_delete=models.CASCADE)
     photo = models.URLField(blank=True)
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
