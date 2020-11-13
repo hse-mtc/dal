d92659d651c59fbd6bb065021f0ac782f39ad1dc
TmLev <leva.bugatti@gmail.com>
2020-11-13 20:30:46 +0300

    fix: add trailing slash

--- front-end/src/api/material.js
+++ front-end/src/api/material.js
@@ -2,7 +2,7 @@ import request from "@/utils/request";
 
 export function deleteMaterial(id) {
   return request({
-    url: `/dms/class-materials/${id}`,
+    url: `/dms/class-materials/${id}/`,
     method: "delete",
   });
 }
