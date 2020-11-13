207e2acfe00a5a63e297f34a65d9bc0be22926d0
darkydash <darkydashie@gmail.com>
2020-11-13 15:57:55 +0300

    Return permissions to Volodya

--- back-end/auth/serializers.py
+++ back-end/auth/serializers.py
@@ -1,13 +1,30 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 from auth.models import Profile
 
 
+class PermissionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Permission
+        fields = ["name", "codename"]
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    permissions = PermissionSerializer(many=True)
+
+    class Meta:
+        model = Group
+        fields = ["name", "permissions"]
+
+
 class UserSerializer(serializers.ModelSerializer):
+    groups = GroupSerializer(many=True)
 
     class Meta:
         model = get_user_model()
-        fields = ["id", "username"]
+        fields = ["id", "username", "groups"]
 
 
 class ProfileSerializer(serializers.ModelSerializer):
