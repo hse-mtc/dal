8d0a6265e0521e0d56a8bfc37361a0cd9d33d827
MaratSaidov <msaidov1@yandex.ru>
2020-11-13 09:22:21 +0300

    added fixtures to settings.py

--- back-end/conf/settings.py
+++ back-end/conf/settings.py
@@ -179,6 +179,12 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+# Fixtures to store backups
+
+FIXTURE_DIRS = [
+   BASE_DIR / "fixtures",
+]
+
 # JWT authentication settings
 
 BEARER_AUTH_HEADER_TYPE = "Bearer"
