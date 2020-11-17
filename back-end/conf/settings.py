d4d7cff49acfe01400c62f8a06ea504b040c7b22
TmLev <leva.bugatti@gmail.com>
2020-11-17 20:22:39 +0300

    style: remove newline

--- back-end/conf/settings.py
+++ back-end/conf/settings.py
@@ -214,7 +214,6 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 1024 * 1024 * 8  # 80MiB
 
 SPECTACULAR_SETTINGS = {
     "COMPONENT_SPLIT_REQUEST": True,
-
     "SWAGGER_UI_SETTINGS": {
         "docExpansion": "none",
         "filter": True,
