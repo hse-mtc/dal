58b7c0cc1bd7217e6141918c44ebf382320ccdd3
Vladis <veisakov@miem.hse.ru>
2020-11-20 10:55:42 +0300

    fix: typo (encouragenement -> encouragement)

--- back-end/lms/urls.py
+++ back-end/lms/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from lms.views.viewsets import (StudentViewSet, TeacherViewSet,
-                                PunishmentViewSet, EncouragenementViewSet)
+                                PunishmentViewSet, EncouragementViewSet)
 from lms.views.reference_book import ReferenceBookView
 from lms.views.absence import AbsenceViewSet, AbsenceJournalView
 
@@ -12,7 +12,7 @@ routers.register('student', StudentViewSet)
 routers.register('teacher', TeacherViewSet)
 routers.register('absence', AbsenceViewSet)
 routers.register('punishment', PunishmentViewSet)
-routers.register('encouragement', EncouragenementViewSet)
+routers.register('encouragement', EncouragementViewSet)
 
 urlpatterns = [
     path('', include(routers.urls)),
