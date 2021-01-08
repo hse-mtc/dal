1a33bacac13477c3dec55d7d94ea34d0e87dcaf1
TmLev <sololev27@gmail.com>
2021-01-09 01:58:51 +0300

    refactor: remove old files

--- tgbot/src/bot/__init__.py
+++
@@ -1,4 +0,0 @@
-from .bot import bot
-
-from . import middleware_handlers
-from . import command_handlers
--- tgbot/src/bot/exception_handler.py
+++
@@ -1,9 +0,0 @@
-import telebot
-from libs.exceptions import NotAuthorised
-
-
-class ExceptionHandler(telebot.ExceptionHandler):
-    def handle(self, exception):
-        if isinstance(exception, NotAuthorised):
-            return True
-        return False
