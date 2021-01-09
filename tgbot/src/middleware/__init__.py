15622349272e63ca23533fc2bffef7f1a309799d
TmLev <sololev27@gmail.com>
2021-01-09 11:52:40 +0300

    feat: add auth middleware

--- tgbot/src/middleware/__init__.py
+++ tgbot/src/middleware/__init__.py
@@ -1,7 +1,11 @@
 from aiogram import Dispatcher
 
+from .auth import AuthMiddleware
+
 
 def setup(dp: Dispatcher) -> None:
+    # Register `logging` middleware
+    # TODO
+
     # Register `auth` middleware
-    # TODO: once it is done
-    pass
+    dp.middleware.setup(AuthMiddleware())
