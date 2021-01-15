78477396fca1ffd6e3f99f6876779976a6f6d0c7
askatsevalov <a79154721620@gmail.com>
2021-01-15 13:34:42 +0300

    feat: schedule table template

---
+++ front-end/src/views/Schedule/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-container">
+    <Schedule />
+  </div>
+</template>
+
+<script>
+import Schedule from "../../components/Schedule/Schedule";
+
+export default {
+  components: {
+    Schedule,
+  },
+  data() {
+    return {};
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {},
+  },
+};
+</script>
