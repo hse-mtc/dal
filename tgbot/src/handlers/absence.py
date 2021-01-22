56c0e137ab1833dcb042940bb818d5101bde2497
Vladis <veisakov@miem.hse.ru>
2021-01-22 09:59:39 +0300

    feat: send absence data, interactive buttons

--- tgbot/src/handlers/absence.py
+++ tgbot/src/handlers/absence.py
@@ -7,6 +7,7 @@ from asyncio import gather
 from aiogram.types import Message
 from aiogram.types import ParseMode
 from aiogram.types import CallbackQuery
+from aiogram.dispatcher.storage import FSMContext
 
 from api.auth import (
     session_exists,
@@ -14,6 +15,7 @@ from api.auth import (
 )
 
 from api.student import fetch_students
+from api.absence import post_all_absence
 
 from keyboards.inline import student_absence_keyboard
 from keyboards.reply import absence_keyboard
@@ -21,32 +23,55 @@ from keyboards.reply import absence_keyboard
 MD2 = ParseMode.MARKDOWN_V2
 
 
-async def get_student(message: Message) -> None:
+async def get_student(message: Message, state: FSMContext) -> None:
     chat_id = message.chat.id
+
     user = await fetch_user(chat_id)
-    kek = []
-    global students
+    message_tasks = []
+
     students = await fetch_students(user.milgroup)
-    # TODO: combine all answers and wait for them
+    await state.set_data(students)
+
     for student in students:
-        kek.append(
+        message_tasks.append(
             message.answer(
             student.full_name,
-            reply_markup=student_absence_keyboard(student.id, )
+            reply_markup=student_absence_keyboard(student.id)
         ))
-    await gather(*kek)
+    await gather(*message_tasks)
+
     await message.answer('После того, как отметите всех студентов, нажмите\n'
-                         'кнопку "Отправить"',
+                         'кнопку "Отправить данные"',
                          reply_markup=absence_keyboard())
     return
 
 
-async def callback_query_process(callback_query: CallbackQuery) -> None:
+async def callback_query_process(
+    callback_query: CallbackQuery,
+    state: FSMContext
+) -> None:
     data = callback_query.data
-    state, student_id = data.split()
-    for student in students:
-        if student.id == student_id:
-            student.state = state
+    message = callback_query.message
+    
+    selected_button, student_id = data.split()
 
-async def absence_sending(message: Message) -> None:
-    print(students)
+    students = await state.get_data()
+    for student in students:
+        if int(student_id) == student.id:
+            student.state = int(selected_button)
+
+    await state.set_data(students)
+
+    await message.edit_reply_markup(
+        reply_markup=student_absence_keyboard(
+            student_id,
+            selected_button
+        )
+    )
+
+
+async def send_abcense(message: Message, state: FSMContext) -> None:
+    students = await state.get_data()
+    message_text = await post_all_absence(students)
+    await message.answer(message_text)
+    return
