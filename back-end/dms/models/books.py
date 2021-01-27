67b319f64f537a9084ea42e7d9a5a35a49035793
TmLev <sololev27@gmail.com>
2021-01-27 22:05:48 +0300

    feat: add book cover model

--- back-end/dms/models/books.py
+++ back-end/dms/models/books.py
@@ -1,4 +1,5 @@
 import datetime
+import uuid
 
 from django.db import models
 
@@ -11,6 +12,23 @@ from dms.models.common import (
 from common.models.subjects import Subject
 
 
+def upload_to(instance, filename):
+    # pylint: disable=unused-argument
+    return f"covers/{instance.id}/"
+
+
+class Cover(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = models.ImageField(upload_to=upload_to, blank=True)
+
+    class Meta:
+        verbose_name = "Cover"
+        verbose_name_plural = "Covers"
+
+    def __str__(self) -> str:
+        return self.image.name
+
+
 def current_year():
     return datetime.date.today().year
 
@@ -20,6 +38,7 @@ class Book(Document):
     publication_year = models.PositiveSmallIntegerField(default=current_year)
     publishers = models.ManyToManyField(to=Publisher, blank=True)
     subjects = models.ManyToManyField(to=Subject, blank=True)
+    cover = models.OneToOneField(to=Cover, on_delete=models.CASCADE, null=True)
 
     class Meta:
         verbose_name = "Book"
