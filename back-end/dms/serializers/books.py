38b31c8270df2b5e831e7a6c8df82090642e0ec9
TmLev <sololev27@gmail.com>
2021-01-27 22:06:00 +0300

    feat: add cover serializer

--- back-end/dms/serializers/books.py
+++ back-end/dms/serializers/books.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 
 from drf_spectacular.utils import inline_serializer
 
-from dms.models.books import Book
+from dms.models.books import (
+    Book,
+    Cover,
+)
 from dms.serializers.documents import (
     DocumentMutateSerializer,
     DocumentSerializer,
@@ -14,10 +17,24 @@ from dms.serializers.common import (
 )
 
 
+class CoverSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(
+        use_url=True,
+        allow_null=True,
+        required=False,
+        read_only=True,
+    )
+
+    class Meta:
+        model = Cover
+        exclude = ["id"]
+
+
 class BookSerializer(DocumentSerializer):
     authors = AuthorSerializer(many=True, read_only=True)
     publishers = PublisherSerializer(many=True, read_only=True)
     subjects = SubjectSerializer(many=True, read_only=True)
+    cover = CoverSerializer(read_only=True)
 
     class Meta:
         model = Book
@@ -25,16 +42,33 @@ class BookSerializer(DocumentSerializer):
 
 
 class BookMutateSerializer(DocumentMutateSerializer):
+    image = serializers.ImageField(write_only=True)
 
     class Meta:
         model = Book
         fields = "__all__"
+
+    def create(self, validated_data):
+        if image := validated_data.pop("image", None):
+            cover = Cover.objects.create(image=image)
+            validated_data["cover"] = cover
+        return super().create(validated_data)
+
+    def update(self, instance: Book, validated_data):
+        if image := validated_data.pop("image", None):
+            if instance.cover:
+                instance.cover.image = image
+                instance.cover.save()
+            else:
+                instance.cover = Cover.objects.create(image=image)
+        return super().update(instance, validated_data)
 
 
 BookMutateSerializerForSwagger = inline_serializer(
     name="BookMutateInline",
     fields={
         "content": serializers.FileField(),
+        "image": serializers.ImageField(),
         "data": BookMutateSerializer(),
     },
 )
