57de2ee8a7c3287bb97c1e4f78aab62559c7f528
darkydash <darkydashie@gmail.com>
2021-01-29 12:56:14 +0300

    Add filter by upload_date in statistics

--- back-end/dms/views/common.py
+++ back-end/dms/views/common.py
@@ -7,6 +7,8 @@ from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 
 from drf_spectacular.views import extend_schema
+from drf_spectacular.utils import OpenApiParameter
+from drf_spectacular.types import OpenApiTypes
 
 from django_filters.rest_framework import DjangoFilterBackend
 from dms.filters import SubjectFilter
@@ -67,19 +69,36 @@ class OrderUpdateAPIView(generics.GenericAPIView, mixins.UpdateModelMixin):
         return self.update(request, *args, **kwargs)
 
 
-@extend_schema(tags=["statistics"])
+@extend_schema(tags=["statistics"],
+               parameters=[
+                   OpenApiParameter("start_date", OpenApiTypes.DATE,
+                                    OpenApiParameter.QUERY),
+                   OpenApiParameter("end_date", OpenApiTypes.DATE,
+                                    OpenApiParameter.QUERY)
+               ])
 class StatisticsAPIView(generics.GenericAPIView):
+    # pylint: disable-msg=too-many-locals
     permission_classes = [permissions.AllowAny]
 
     def get(self, request, uid):
-        paper_count = Paper.objects.filter(user__id=uid).count()
-        book_count = Book.objects.filter(user__id=uid).count()
-        subject_count = Subject.objects.filter(user__id=uid).count()
+        start_date = request.GET.get("start_date", "")
+        end_date = request.GET.get("end_date", "")
+
+        paper_filter = Paper.objects.filter(user__id=uid)
+        book_filter = Book.objects.filter(user__id=uid)
+        subject_filter = Subject.objects.filter(user__id=uid)
+
+        if start_date:
+            paper_filter = paper_filter.filter(upload_date__gte=start_date)
+            book_filter = book_filter.filter(upload_date__gte=start_date)
+        if end_date:
+            paper_filter = paper_filter.filter(upload_date__lte=end_date)
+            book_filter = book_filter.filter(upload_date__lte=end_date)
 
         data = {
-            "paper_count": paper_count,
-            "book_count": book_count,
-            "subject_count": subject_count
+            "paper_count": paper_filter.count(),
+            "book_count": book_filter.count(),
+            "subject_count": subject_filter.count()
         }
 
         return Response(data, status=HTTP_200_OK)
