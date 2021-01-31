3eb08141c2d1fac41cf3b0e3450a5ffd970c3d2c
Александр Сивков <aasivkov@miem.hse.ru>
2021-01-31 22:16:10 +0300

    db_model

--- back-end/common/models/persons.py
+++ back-end/common/models/persons.py
@@ -5,6 +5,7 @@ class Person(models.Model):
     surname = models.CharField(max_length=32)
     name = models.CharField(max_length=32)
     patronymic = models.CharField(max_length=32, blank=True)
+    
 
     @property
     def full_name(self):
@@ -14,6 +15,55 @@ class Person(models.Model):
         abstract = True
         verbose_name = "Person"
         verbose_name_plural = "Persons"
+
+    def __str__(self):
+        return self.full_name
+
+    
+class Relative(Person):
+    relative_list = [('father', 'отец'),
+                    ('mother', 'мать'),
+                    ('sister', 'сестра'),
+                    ('brother', 'брат')]
+    rel_status = models.CharField(max_length=32, choices=relative_list)
+    birthdate = models.DateField()
+    citizenship = models.CharField(max_length=32)
+    pers_mobile = models.CharField(max_length=15, blank=True)
+    permanent_address = models.CharField(max_length=64)
+    place_birth = models.CharField(max_length=64)
+
+    @property
+    def full_name(self):
+        return " ".join([self.surname, self.name, self.patronymic])
+
+    class Meta:
+        abstract = False
+        verbose_name = "Person"
+        verbose_name_plural = "Persons"
+
+    def __str__(self):
+        return self.full_name
+
+    
+class Personel(Person):
+    birthdate = models.DateField()
+    photo = models.ImageField(upload_to = '', max_length=128)
+    place_birth = models.CharField(max_length=32)
+    region_birth = models.CharField(max_length=64)
+    citizenship = models.CharField(max_length=32)
+    pers_mobile = models.CharField(max_length=15)
+    corp_email = models.EmailField()
+    pers_email = models.EmailField(blank=True)
+    permanent_address = models.CharField(max_length=64)
+
+    @property
+    def full_name(self):
+        return " ".join([self.surname, self.name, self.patronymic])
+
+    class Meta:
+        abstract = True
+        verbose_name = "Personel"
+        verbose_name_plural = "Personels"
 
     def __str__(self):
         return self.full_name
--- back-end/lms/models/student.py
+++ back-end/lms/models/student.py
@@ -1,11 +1,43 @@
 from django.db import models
 
-from common.models.persons import Person
+from common.models.persons import Person, Personel, Relative
 from lms.models.common import Milgroup
+
+class Faculty(models.Model):
+    faculty = models.CharField(primary_key=True, max_length=128, blank=True)
+
+    def __str__(self):
+        return str(self.faculty)
+
+    class Meta:
+        verbose_name = 'Faculty'
+        verbose_name_plural = 'Faculties'
+
+
+class Mil_specialty(models.Model):
+    specialty_list = [('094001', 'Применение наземных подразделений войсковой разведки'),
+                ('411300', 'Эксплуатация и ремонт автоматизированных систем комплексов баллистических стратегических ракет наземного базирования'),
+                ('453000', 'Организация эксплуатации и ремонта автоматизированных систем управления и вычислительных комплексов ракетно-космической обороны'),
+               ('453100', 'Математическое и программное обеспечение функционирования вычислительных комплексов ракетно-космической обороны'),
+               ('751100','Защита информационных технологий'),
+               ('100182', 'Стрелковые, командир стрелкового отделения'),
+               ('106646-543', 'Разведывательные, разведчик-оператор СБР, ПСНР')]
+    mil_specialty = models.CharField(primary_key=True, max_length=20, choices=specialty_list)
+
+    def __str__(self):
+        return str(self.mil_specialty)
+
+    class Meta:
+        verbose_name = 'Military specialty'
+        verbose_name_plural = 'Military specialties'
 
 
 class Status(models.Model):
-    status = models.CharField(primary_key=True, max_length=20)
+    status_list = [('student', 'обучающийся'),
+                   ('graduated', 'выпускник'),
+                   ('deducted', 'отчислен'),
+                   ('enrollee','абитуриент')]
+    status = models.CharField(primary_key=True, max_length=20, choices=status_list)
 
     def __str__(self):
         return str(self.status)
@@ -17,7 +49,8 @@ class Status(models.Model):
 
 class Program(models.Model):
     code = models.CharField(primary_key=True, max_length=8)
-    program = models.CharField(max_length=500, blank=True, null=True)
+    program = models.CharField(max_length=128, blank=True, null=True)
+    faculty = models.ForeignKey(Faculty, models.DO_NOTHING, blank=True)
 
     def __str__(self):
         return f'({str(self.code)}) {str(self.program)}'
@@ -25,14 +58,38 @@ class Program(models.Model):
     class Meta:
         verbose_name = 'Educational Program'
         verbose_name_plural = 'Educational Programs'
+        
 
-
-class Student(Person):
+class Student(Personel):
+    #militrary
     milgroup = models.ForeignKey(Milgroup, models.DO_NOTHING)
-    birthdate = models.DateField()
+    commissariat_city = models.CharField(max_length=32)
+    commissariat_district = models.CharField(max_length=32)
+    mil_specialty = models.ForeignKey(Mil_specialty, models.DO_NOTHING)
+    
+    #student characteristics
     program = models.ForeignKey(Program, models.DO_NOTHING)
     status = models.ForeignKey(Status, models.DO_NOTHING)
-    photo = models.CharField(max_length=128, blank=True, null=True)
+    hse_id = models.CharField(max_length=32)
+    hse_group = models.CharField(max_length=32)
+    
+    #passport information
+    passport_series = models.CharField(max_length=4)
+    passport_code = models.CharField(max_length=6)
+    pasport_ufms_name = models.CharField(max_length=64)
+    pasport_ufms_code = models.CharField(max_length=7)
+    pasport_date = models.DateField()
+    
+    #family
+    mother = models.ForeignKey(Relative, models.DO_NOTHING, related_name='mother')
+    father = models.ForeignKey(Relative, models.DO_NOTHING, related_name='father')
+    brother = models.ForeignKey(Relative, models.DO_NOTHING, related_name='brother')
+    sister = models.ForeignKey(Relative, models.DO_NOTHING, related_name='sister')
+    
+    #name in genitive
+    surname_genitive = models.CharField(max_length=32)
+    name_genitive = models.CharField(max_length=32)
+    patronymic_genetive = models.CharField(max_length=32, blank=True)
 
     def __str__(self):
         return f'ID = {str(self.id)}\n' \
