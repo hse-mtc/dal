616b66c9bf72dfece914b278758eb44a1a28228c
TmLev <sololev27@gmail.com>
2021-02-12 11:02:55 +0300

    fix: proper model registration in admin

--- back-end/common/admin.py
+++ back-end/common/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from common.models.subjects import Subject
+from common.models.persons import Relative
 
 admin.site.register(Subject)
+admin.site.register(Relative)
