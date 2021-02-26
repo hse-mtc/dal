b2bdaafeb0e6702207ab427280f2c9b890b4d4a0
TmLev <sololev27@gmail.com>
2021-02-26 22:36:14 +0300

    refactor: serve swagger from its path

--- back-end/conf/urls.py
+++ back-end/conf/urls.py
@@ -17,8 +17,8 @@ from tgbot.views import populate as tgbot_populate
 
 urlpatterns = [
     # Swagger
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("",
+    path("swagger/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger/",
          SpectacularSwaggerView.as_view(url_name="schema"),
          name="swagger-ui"),
 
