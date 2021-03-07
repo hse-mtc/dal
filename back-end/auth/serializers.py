0ac7130285c5a9a06449dfe34621939e8f8dcbb0
darkydash <darkydashie@gmail.com>
2021-03-08 00:54:26 +0300

    Add Email token link

--- back-end/auth/serializers.py
+++ back-end/auth/serializers.py
@@ -1,6 +1,8 @@
+from datetime import timedelta
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group, Permission
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from auth.models import Profile
 
 
@@ -43,6 +45,22 @@ class ProfileSerializer(serializers.ModelSerializer):
 class TokenPairSerializer(serializers.Serializer):
     access = serializers.CharField(read_only=True)
     refresh = serializers.CharField(read_only=True)
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        token = super(MyTokenObtainPairSerializer, cls).get_token(user)
+        token = token.access_token
+        token.set_exp(lifetime=timedelta(minutes=5))
+        return token
 
     def create(self, validated_data):
         pass
