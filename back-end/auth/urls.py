cd06a2bdba85777510515fe3b3969a0645915d39
TmLev <sololev27@gmail.com>
2021-03-10 14:51:04 +0300

    chore: bump urls for new view

--- back-end/auth/urls.py
+++ back-end/auth/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
-from auth.views import (info, ProfileRetrieveAPIView,
-                        TokenObtainPairExtendedView, TokenRefreshExtendedView,
-                        AuthLink)
+from auth.views import (
+    info,
+    ProfileRetrieveAPIView,
+    TokenObtainPairExtendedView,
+    TokenRefreshExtendedView,
+    CreatePasswordAPIView,
+)
 
 urlpatterns = [
     path("users/info/", info),
     path("profile/", ProfileRetrieveAPIView.as_view()),
     path("tokens/obtain/", TokenObtainPairExtendedView.as_view()),
     path("tokens/refresh/", TokenRefreshExtendedView.as_view()),
-    path("send_access_token/<str:email>/", AuthLink.as_view()),
+    path("password/create/", CreatePasswordAPIView.as_view()),
 ]
