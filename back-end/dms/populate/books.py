252e1a073f9ed8b33d61d4ee70fdd5f70a5c7a78
TmLev <sololev27@gmail.com>
2021-03-10 14:04:53 +0300

    refactor: extract books populate

---
+++ back-end/dms/populate/books.py
@@ -0,0 +1,69 @@
+import random
+
+from dms.models.common import Author
+from dms.models.documents import File
+from dms.models.papers import Publisher
+from dms.models.class_materials import Subject
+from dms.models.books import (
+    Book,
+    FavoriteBook,
+)
+
+
+def create_books(
+    authors: list[Author],
+    files: list[File],
+    publishers: list[Publisher],
+    subjects: list[Subject],
+) -> list[Book]:
+    # pylint: disable=too-many-arguments,too-many-locals
+
+    random.seed(11)
+
+    books = []
+
+    titles = [
+        "Учебник по ТСП",
+        "Физика для ракетчиков",
+        "Сферическая геометрия",
+        "Пособие по танкам",
+        "Тактическая подготовка",
+        "Рукопашный бой под водой",
+    ]
+
+    for file in files:
+        book, is_created = Book.objects.get_or_create(
+            title=random.choice(titles),
+            file=file,
+            page_count=random.randint(100, 300),
+        )
+
+        if not is_created:
+            continue
+
+        for _ in range(random.randint(1, 2)):
+            book.authors.add(random.choice(authors))
+
+        for _ in range(random.randint(1, 2)):
+            book.publishers.add(random.choice(publishers))
+
+        for _ in range(random.randint(1, 2)):
+            book.subjects.add(random.choice(subjects))
+
+        book.save()
+        books.append(book)
+
+    return books
+
+
+def create_favorite_books(
+    books: list[Book],
+    user,
+) -> list[FavoriteBook]:
+    favorites = []
+
+    for book in books:
+        favorite, _ = FavoriteBook.objects.get_or_create(book=book, user=user)
+        favorites.append(favorite)
+
+    return favorites
