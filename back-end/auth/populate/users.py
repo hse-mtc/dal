2508a251f726cc6933d93963f0f29601ab6df47d
TmLev <sololev27@gmail.com>
2021-03-10 14:05:04 +0300

    refactor: extract users populate

---
+++ back-end/auth/populate/users.py
@@ -0,0 +1,95 @@
+from collections import namedtuple
+
+from django.contrib.auth import get_user_model
+
+from auth.models import Profile
+
+User = get_user_model()
+
+Data = namedtuple(
+    "Data",
+    [
+        "username",
+        "password",
+        "is_staff",
+        "is_superuser",
+        "surname",
+        "name",
+        "patronymic",
+    ],
+)
+
+
+def create_user(data: Data) -> (User, Profile):
+    query = User.objects.filter(username=data.username)
+
+    if not query.exists():
+        User.objects.create_user(
+            username=data.username,
+            password=data.password,
+            is_staff=data.is_staff,
+            is_superuser=data.is_superuser,
+        )
+
+    user = query.first()
+
+    profile, _ = Profile.objects.get_or_create(
+        surname=data.surname,
+        name=data.name,
+        patronymic=data.patronymic,
+        user=user,
+    )
+
+    return user, profile
+
+
+def create_users() -> list[(User, Profile)]:
+    users = [
+        Data(
+            username="vspelyak",
+            password="qwerty",
+            is_staff=True,
+            is_superuser=True,
+            surname="Пеляк",
+            name="Виктор",
+            patronymic="Степанович",
+        ),
+        Data(
+            username="test",
+            password="qwerty",
+            is_staff=True,
+            is_superuser=False,
+            surname="Фамилия",
+            name="Имя",
+            patronymic="Отчество",
+        ),
+        Data(
+            username="student",
+            password="qwerty",
+            is_staff=True,
+            is_superuser=False,
+            surname="Студентов",
+            name="Студент",
+            patronymic="Студентов",
+        ),
+        Data(
+            username="teacher",
+            password="qwerty",
+            is_staff=True,
+            is_superuser=False,
+            surname="Преподов",
+            name="Препод",
+            patronymic="Преподович",
+        ),
+        Data(
+            username="milfaculty_head",
+            password="qwerty",
+            is_staff=True,
+            is_superuser=False,
+            surname="Начальников",
+            name="Начальник",
+            patronymic="Начальникович",
+        ),
+    ]
+
+    return [create_user(data) for data in users]
