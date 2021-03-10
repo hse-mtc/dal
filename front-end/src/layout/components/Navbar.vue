a4b6bef5233e5f99ef0e9c1cee6a413b43a6f704
TmLev <sololev27@gmail.com>
2021-03-10 15:20:56 +0300

    refactor: bump navbar for full name

--- front-end/src/layout/components/Navbar.vue
+++ front-end/src/layout/components/Navbar.vue
@@ -13,7 +13,7 @@
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper m-0" style="font-size: 19px">
           <!--          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">-->
-          {{ name }}
+          {{ fullName }}
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
@@ -41,10 +41,13 @@ export default {
     Hamburger,
   },
   computed: {
-    ...mapGetters(["sidebar", "name"]),
+    ...mapGetters(["sidebar", "surname", "name", "patronymic"]),
     isCollapse() {
       return !this.sidebar.opened;
     },
+    fullName() {
+      return `${this.surname} ${this.name[0]}. ${this.patronymic[0]}.`
+    }
   },
   methods: {
     logout() {
