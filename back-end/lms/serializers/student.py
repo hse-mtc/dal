16fbe5489ebe01ab76582725b1d6a40b27e93eb5
darkydash <darkydashie@gmail.com>
2021-03-12 23:49:42 +0300

    Change lms.Student serializer, add more fields

--- back-end/lms/serializers/student.py
+++ back-end/lms/serializers/student.py
@@ -1,9 +1,20 @@
-from rest_framework.serializers import (IntegerField, SerializerMethodField,
-                                        ModelSerializer)
+from rest_framework.serializers import (
+    IntegerField,
+    SerializerMethodField,
+    ModelSerializer,
+)
 from drf_writable_nested.serializers import WritableNestedModelSerializer
+from common.models.persons import Relative
 
 from lms.models.common import Milgroup
-from lms.models.student import Program, Student
+from lms.models.student import (
+    Program,
+    Student,
+    Passport,
+    Family,
+    RecruitmentOffice,
+    UniversityInfo,
+)
 
 from lms.validators import PresentInDatabaseValidator
 from lms.serializers.common import MilgroupSerializer
@@ -17,11 +28,62 @@ class ProgramSerializer(ModelSerializer):
         extra_kwargs = {'code': {'validators': []}}
 
 
+class PassportSerializer(ModelSerializer):
+
+    class Meta:
+        model = Passport
+        exclude = ['id']
+
+
+class RelativeSerializer(ModelSerializer):
+
+    class Meta:
+        model = Relative
+        exclude = ['id']
+
+
+class FamilySerializer(ModelSerializer):
+    mother = RelativeSerializer()
+    father = RelativeSerializer()
+    brothers = RelativeSerializer(many=True)
+    sisters = RelativeSerializer(many=True)
+
+    class Meta:
+        model = Family
+        exclude = ['id']
+
+
+class RecruitmentOfficeSerializer(ModelSerializer):
+
+    class Meta:
+        model = RecruitmentOffice
+        exclude = ['id']
+
+
+class UniversityInfoSerializer(ModelSerializer):
+
+    class Meta:
+        model = UniversityInfo
+        exclude = ['id']
+
+
 class StudentSerializer(WritableNestedModelSerializer):
     milgroup = MilgroupSerializer(
         required=False, validators=[PresentInDatabaseValidator(Milgroup)])
+
     program = ProgramSerializer(
         required=False, validators=[PresentInDatabaseValidator(Program)])
+
+    recruitment_office = RecruitmentOfficeSerializer(
+        required=False,
+        validators=[PresentInDatabaseValidator(RecruitmentOffice)])
+
+    university_info = UniversityInfoSerializer(
+        required=False, validators=[PresentInDatabaseValidator(Family)])
+
+    passport = PassportSerializer(required=False)
+
+    family = FamilySerializer(required=False)
 
     fullname = SerializerMethodField(required=False)
 
