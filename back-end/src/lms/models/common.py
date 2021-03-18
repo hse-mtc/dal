9547c1d1d9291bcef2fc38e5d170aee6b57298de
TmLev <sololev27@gmail.com>
2021-03-18 17:56:51 +0300

    layout: move milspecialty to common

--- back-end/src/lms/models/common.py
+++ back-end/src/lms/models/common.py
@@ -12,6 +12,18 @@ class Milfaculty(models.Model):
         verbose_name_plural = 'Military Faculties'
 
 
+class Milspecialty(models.Model):
+    code = models.CharField(primary_key=True, max_length=20)
+    mil_specialty = models.CharField(max_length=150)
+
+    def __str__(self):
+        return str(self.mil_specialty)
+
+    class Meta:
+        verbose_name = 'Military specialty'
+        verbose_name_plural = 'Military specialties'
+
+
 class Milgroup(models.Model):
     milgroup = models.DecimalField(primary_key=True,
                                    max_digits=4,
