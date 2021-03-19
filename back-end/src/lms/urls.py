9d2934f6c3cfd57dc53ea038fc6b0b7854840561
TmLev <sololev27@gmail.com>
2021-03-19 17:01:20 +0300

    feat: add url for milspecialties

--- back-end/src/lms/urls.py
+++ back-end/src/lms/urls.py
@@ -9,10 +9,17 @@ from lms.views.absence_time import AbsenceTimeView
 from lms.views.punishments import PunishmentViewSet
 from lms.views.encouragements import EncouragementViewSet
 from lms.views.achievements import AchievementViewSet
-from lms.views.reference_book import (ReferenceBookView, MilfacultyViewSet,
-                                      MilgroupViewSet, ProgramViewSet,
-                                      RankViewSet, TeacherPostViewSet,
-                                      RoomViewSet, AchievementTypeViewSet)
+from lms.views.reference_book import (
+    ReferenceBookView,
+    MilfacultyViewSet,
+    MilspecialtyViewSet,
+    MilgroupViewSet,
+    ProgramViewSet,
+    RankViewSet,
+    TeacherPostViewSet,
+    RoomViewSet,
+    AchievementTypeViewSet,
+)
 from lms.views.subjects import LessonSubjectViewSet
 from lms.views.lessons import LessonViewSet, LessonJournalView
 from lms.views.marks import MarkViewSet, MarkJournalView
@@ -28,6 +35,7 @@ routers.register('achievements', AchievementViewSet)
 routers.register('lessons', LessonViewSet)
 routers.register('marks', MarkViewSet)
 routers.register('milfaculties', MilfacultyViewSet)
+routers.register('milspecialties', MilspecialtyViewSet)
 routers.register('milgroups', MilgroupViewSet)
 routers.register('programs', ProgramViewSet)
 routers.register('ranks', RankViewSet)
