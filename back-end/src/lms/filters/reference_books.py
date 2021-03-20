94740c853e531ce390600b01c66c55aad168ae24
TmLev <sololev27@gmail.com>
2021-03-20 17:21:59 +0300

    feat: add campus filter

--- back-end/src/lms/filters/reference_books.py
+++ back-end/src/lms/filters/reference_books.py
@@ -1,7 +1,30 @@
-from django_filters.rest_framework import FilterSet
+from django_filters.rest_framework import (
+    FilterSet,
+    ChoiceFilter,
+)
 
-from lms.models.common import Milgroup
-from lms.models.universities import Program
+from lms.models.universities import (
+    UniversityInfo,
+    Program,
+)
+from lms.models.common import (
+    Milgroup,
+    Milspecialty,
+)
+
+
+class MilspecialtyFilter(FilterSet):
+    campus = ChoiceFilter(
+        choices=UniversityInfo.Campus.choices,
+        method="filter_by_campus",
+    )
+
+    def filter_by_campus(self, queryset, name, value):
+        return queryset.filter(available_for__contains=[value])
+
+    class Meta:
+        model = Milspecialty
+        fields = ["campus"]
 
 
 class MilgroupFilter(FilterSet):
