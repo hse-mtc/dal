2032adea8d5d38c5435f207c0fbeb782ad946975
TmLev <sololev27@gmail.com>
2021-03-22 12:21:55 +0300

    chore: adjust populate

--- back-end/src/common/views/populate.py
+++ back-end/src/common/views/populate.py
@@ -98,10 +98,10 @@ class PopulateAPIView(GenericAPIView):
         teachers.permissions.set(get_teacher_permissions())
         milfaculty_heads.permissions.set(get_milfaculty_head_permissions())
 
-        students.user_set.add(User.objects.get(username="student"))
-        teachers.user_set.add(User.objects.get(username="teacher"))
+        students.user_set.add(User.objects.get(email="student@mail.com"))
+        teachers.user_set.add(User.objects.get(email="teacher@mail.com"))
         milfaculty_heads.user_set.add(
-            User.objects.get(username="milfaculty_head"))
+            User.objects.get(email="milfaculty_head@mail.com"))
 
         # ----------------------------------------------------------------------
         # common
@@ -137,7 +137,8 @@ class PopulateAPIView(GenericAPIView):
             publishers=publishers,
             subjects=subjects,
         )
-        create_favorite_books(books[:11], User.objects.get(username="vspelyak"))
+        create_favorite_books(books[:11],
+                              User.objects.get(email="vspelyak@mail.com"))
 
         # ----------------------------------------------------------------------
         # lms
