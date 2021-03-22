e0b4c4b8a1dc7a3e53beeb1211a8e45fdad31725
TmLev <sololev27@gmail.com>
2021-03-22 12:22:09 +0300

    cfg: specify user model in settings

--- back-end/src/conf/settings.py
+++ back-end/src/conf/settings.py
@@ -232,6 +232,10 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 1024 * 1024 * 8  # 80MiB
 
 CREATE_PASSWORD_TOKEN_LIFETIME = timedelta(days=5)
 
+# Custom user model
+
+AUTH_USER_MODEL = "dal_auth.User"
+
 # Swagger settings for drf-spectacular
 
 SPECTACULAR_SETTINGS = {
