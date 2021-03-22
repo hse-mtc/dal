4f967f9100ca8055919106dc88f9740ab756e7b3
TmLev <sololev27@gmail.com>
2021-03-22 12:25:46 +0300

    fix: register profile in admin

--- back-end/src/auth/admin.py
+++ back-end/src/auth/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from auth.models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
+
 
 admin.site.register(get_user_model())
