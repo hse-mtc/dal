980ef3d861215bd9aabd16d3b7fba5e6dfd2024d
TmLev <sololev27@gmail.com>
2021-03-22 12:23:00 +0300

    fix: register custom user model in admin

--- back-end/src/auth/admin.py
+++ back-end/src/auth/admin.py
@@ -1,16 +1,4 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from auth.models import Profile
 
-
-class ProfileInline(admin.StackedInline):
-    model = Profile
-
-
-class UserAdmin(BaseUserAdmin):
-    inlines = [ProfileInline]
-
-
-admin.site.unregister(get_user_model())
-admin.site.register(get_user_model(), UserAdmin)
+admin.site.register(get_user_model())
