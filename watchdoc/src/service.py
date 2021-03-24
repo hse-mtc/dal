5f67ad167a656f9551c60df741bb2b5d863272a6
TmLev <sololev27@gmail.com>
2021-03-24 23:33:32 +0300

    feat: upload documents

--- watchdoc/src/service.py
+++ watchdoc/src/service.py
@@ -21,16 +21,34 @@ WATCHDOC_FOLDER: str = "watchdoc"
 APPLICANTS_FOLDER: str = "Абитуриенты"
 CAMPUSES_FOLDER: str = "Кампусы"
 
+DOCUMENTS = [
+    ("mec-application.docx", "Заявление на поступление.docx"),
+    ("ro-reference.docx", "Направление ВК.docx"),
+]
+
 
 class WatchDocService:
     def __init__(self) -> None:
+        # Templates
+
+        jinja_env = jinja2.Environment()
+        jinja_env.filters["date_to_russian_format"] = date_to_russian_format
+        jinja_env.filters["month_to_russian_title"] = month_to_russian_title
+
+        self.jinja_env = jinja_env
+
+        # Drive
+
         self.ds = DriveService()
 
         # watchdoc folder
 
-        self._watchdoc_folder = self.ds.obtain_folder(name=WATCHDOC_FOLDER, )
+        self._watchdoc_folder = self.ds.obtain_folder(
+            name=WATCHDOC_FOLDER,
+        )
         self.ds.write_share_folder_with_admin(
-            folder_id=self._watchdoc_folder["id"], )
+            folder_id=self._watchdoc_folder["id"],
+        )
         log.info(f"`{WATCHDOC_FOLDER}` web view link: "
                  f"{self._watchdoc_folder['webViewLink']}")
 
@@ -60,20 +78,39 @@ class WatchDocService:
             )
 
     def generate_documents(self, applicant: Applicant):
-        template_path = TEMPLATES_DIR / "mec-application.docx"
-        doc = DocxTemplate(template_path)
-
-        jinja_env = jinja2.Environment()
-        jinja_env.filters["date_to_russian_format"] = date_to_russian_format
-        jinja_env.filters["month_to_russian_title"] = month_to_russian_title
+        applicant_dir = GENERATED_DIR / applicant.contact_info.corporate_email
+        applicant_dir.mkdir(exist_ok=True)
 
         context = {
             "date": today(),
             **applicant.dict(),
         }
-        doc.render(context, jinja_env)
 
-        applicant_dir = GENERATED_DIR / applicant.contact_info.corporate_email
-        applicant_dir.mkdir(exist_ok=True)
-        generated_path = applicant_dir / "mec-application.docx"
-        doc.save(generated_path)
+        for (en, rus) in DOCUMENTS:
+            doc = DocxTemplate(TEMPLATES_DIR / en)
+            doc.render(context, self.jinja_env)
+            doc.save(applicant_dir / rus)
+
+    def upload_documents(self, applicant: Applicant):
+        email = applicant.contact_info.corporate_email
+        campus = applicant.university_info.campus.value
+        folder_name = f"{applicant.full_name} {email}"
+
+        applicant_folder = self.ds.obtain_folder(
+            name=folder_name,
+            parents=[self._campus_folders[campus]["id"]],
+        )
+        documents_folder = self.ds.obtain_folder(
+            name="Документы",
+            parents=[applicant_folder["id"]],
+        )
+
+        for (_, rus) in DOCUMENTS:
+            body = {
+                "name": rus,
+                "parents": [documents_folder["id"]],
+            }
+            self.ds.upload_docx(
+                body=body,
+                path=GENERATED_DIR / email / rus,
+            )
