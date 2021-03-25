fa95d86439f50912acb2bc7dd6c43d3220c9cf3d
TmLev <sololev27@gmail.com>
2021-03-25 15:46:54 +0300

    feat: use gmail service for notifications

--- watchdoc/src/service.py
+++ watchdoc/src/service.py
@@ -4,6 +4,8 @@ import jinja2
 
 from docxtpl import DocxTemplate
 
+from auth import obtain_credentials
+from gmail import GmailService
 from drive import DriveService
 from campuses import Campus
 from proto import Applicant
@@ -11,6 +13,8 @@ from config import (
     TEMPLATES_DIR,
     GENERATED_DIR,
 )
+
+from email_utils import create_message
 from date_utils import (
     date_to_russian_format,
     month_to_russian_title,
@@ -28,55 +32,14 @@ DOCUMENTS = [
 
 
 class WatchDocService:
-    def __init__(self) -> None:
-        # Templates
+    """WatchDoc manages applicants' documents.
 
-        jinja_env = jinja2.Environment()
-        jinja_env.filters["date_to_russian_format"] = date_to_russian_format
-        jinja_env.filters["month_to_russian_title"] = month_to_russian_title
-
-        self.jinja_env = jinja_env
-
-        # Drive
-
-        self.ds = DriveService()
-
-        # watchdoc folder
-
-        self._watchdoc_folder = self.ds.obtain_folder(
-            name=WATCHDOC_FOLDER,
-        )
-        self.ds.write_share_folder_with_admin(
-            folder_id=self._watchdoc_folder["id"],
-        )
-        log.info(f"`{WATCHDOC_FOLDER}` web view link: "
-                 f"{self._watchdoc_folder['webViewLink']}")
-
-        # applicants folder
-
-        self._applicants_folder = self.ds.obtain_folder(
-            name=APPLICANTS_FOLDER,
-            parents=[self._watchdoc_folder["id"]],
-        )
-        log.info(f"`{APPLICANTS_FOLDER}` web view link: "
-                 f"{self._applicants_folder['webViewLink']}")
-
-        # campus folders
-
-        self._campuses_folder = self.ds.obtain_folder(
-            name=CAMPUSES_FOLDER,
-            parents=[self._applicants_folder["id"]],
-        )
-        log.info(f"`{CAMPUSES_FOLDER}` web view link: "
-                 f"{self._campuses_folder['webViewLink']}")
-
-        self._campus_folders = {}
-        for abbr, campus in Campus.choices():
-            self._campus_folders[abbr] = self.ds.obtain_folder(
-                name=campus,
-                parents=[self._campuses_folder["id"]],
-            )
-
+    Usage:
+        wds = WatchDocService()
+        wds.generate_documents(applicant)
+        folder_link = wds.upload_documents(applicant)
+        wds.notify(applicant, folder_link)
+    """
     def generate_documents(self, applicant: Applicant):
         applicant_dir = GENERATED_DIR / applicant.contact_info.corporate_email
         applicant_dir.mkdir(exist_ok=True)
@@ -114,3 +77,73 @@ class WatchDocService:
                 body=body,
                 path=GENERATED_DIR / email / rus,
             )
+
+        self.ds.read_share_folder_with_anyone(
+            folder_id=applicant_folder["id"],
+        )
+
+        return applicant_folder["webViewLink"]
+
+    def notify(self, applicant: Applicant, folder_link: str):
+        email = applicant.contact_info.corporate_email
+        msg = create_message(to=email, link=folder_link)
+        self.gs.send_message(body=msg)
+
+    # --------------------------------------------------------------------------
+    # Internal methods
+
+    def __init__(self) -> None:
+        # Templates
+        self.jinja_env = self._init_jinja_env()
+
+        # Credentials
+        credentials = obtain_credentials()
+
+        # Gmail
+        self.gs = GmailService(credentials)
+
+        # Drive
+        self.ds = DriveService(credentials)
+        self._watchdoc_folder = self._init_watchdoc_folder()
+        self._applicants_folder = self._init_applicants_folder()
+        self._campuses_folder = self._init_campuses_folder()
+        self._campus_folders = self._init_campus_folders()
+
+    def _init_jinja_env(self):
+        env = jinja2.Environment()
+        env.filters["date_to_russian_format"] = date_to_russian_format
+        env.filters["month_to_russian_title"] = month_to_russian_title
+        return env
+
+    def _init_watchdoc_folder(self):
+        folder = self.ds.obtain_folder(name=WATCHDOC_FOLDER)
+        log.info(f"`{WATCHDOC_FOLDER}` web view link: "
+                 f"{folder['webViewLink']}")
+        return folder
+
+    def _init_applicants_folder(self):
+        folder = self.ds.obtain_folder(
+            name=APPLICANTS_FOLDER,
+            parents=[self._watchdoc_folder["id"]],
+        )
+        log.info(f"`{APPLICANTS_FOLDER}` web view link: "
+                 f"{folder['webViewLink']}")
+        return folder
+
+    def _init_campuses_folder(self):
+        folder = self.ds.obtain_folder(
+            name=CAMPUSES_FOLDER,
+            parents=[self._applicants_folder["id"]],
+        )
+        log.info(f"`{CAMPUSES_FOLDER}` web view link: "
+                 f"{folder['webViewLink']}")
+        return folder
+
+    def _init_campus_folders(self):
+        folders = {}
+        for abbr, campus in Campus.choices():
+            folders[abbr] = self.ds.obtain_folder(
+                name=campus,
+                parents=[self._campuses_folder["id"]],
+            )
+        return folders
