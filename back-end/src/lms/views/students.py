8bbe604105dcc24c1b84e836e162cd60d66da672
TmLev <sololev27@gmail.com>
2021-03-25 17:35:24 +0300

    feat: send applicant data to watchdoc

--- back-end/src/lms/views/students.py
+++ back-end/src/lms/views/students.py
@@ -1,10 +1,21 @@
+import requests
+
+from rest_framework import status
+
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import BasePermission
+from rest_framework.renderers import JSONRenderer
 
 from django_filters.rest_framework import DjangoFilterBackend
 
 from drf_spectacular.views import extend_schema
+
+from conf.settings import (
+    WATCHDOC_HOST,
+    WATCHDOC_PORT,
+)
 
 from common.constants import MUTATE_ACTIONS
 
@@ -13,6 +24,7 @@ from lms.filters.students import StudentFilter
 from lms.serializers.students import (
     StudentSerializer,
     StudentMutateSerializer,
+    ApplicantSerializer,
 )
 
 from auth.permissions import BasicPermission
@@ -42,3 +54,24 @@ class StudentViewSet(ModelViewSet):
         if self.action in MUTATE_ACTIONS:
             return StudentMutateSerializer
         return StudentSerializer
+
+    def create(self, request, *args, **kwargs):
+        # pylint: disable=too-many-locals
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance = self.perform_create(serializer)
+
+        applicant = ApplicantSerializer(instance=instance)
+        requests.post(
+            f'http://{WATCHDOC_HOST}:{WATCHDOC_PORT}/applicants/',
+            data=JSONRenderer().render(applicant.data),
+        )
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data,
+                        status=status.HTTP_201_CREATED,
+                        headers=headers)
+
+    def perform_create(self, serializer):
+        return serializer.save()
