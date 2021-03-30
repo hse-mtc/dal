e3622330b066d5a6cc6236e8b4faaf15452d0d08
TmLev <sololev27@gmail.com>
2021-03-30 23:28:27 +0300

    refactor: better wrapper

--- tgbot/src/utils/auth.py
+++ tgbot/src/utils/auth.py
@@ -1,32 +1,42 @@
 import os
 import typing as tp
 
+from functools import wraps
+
 from api.client import client
 
 
-def check_token(request_method: tp.Awaitable):
+RequestMethod = tp.Callable[..., tp.Awaitable]
+
+
+def auth_required(request_method: RequestMethod):
+
+    @wraps(request_method)
     async def wrapper(*args: tp.Any, **kwargs: tp.Any):
         try:
             response = await request_method(*args, **kwargs)
         except:
             token = await fetch_token()
-            headers = fetch_headers(token)
+            headers = make_headers(token)
             response = await request_method(headers=headers, *args, **kwargs)
         return response
+
     return wrapper
 
 
 async def fetch_token() -> str:
     body = {
         "username": os.environ["TGBOT_USERNAME"],
-        "password": os.environ["TGBOT_PASSWORD"]
+        "password": os.environ["TGBOT_PASSWORD"],
     }
+
     async with client.post("auth/tokens/obtain/", json=body) as resp:
         response = await resp.json()
-    return response.get("access", None)
+
+    return response["access"]
 
 
-def fetch_headers(token: str) -> dict:
+def make_headers(token: str) -> dict:
     return {
-        "Authorization": f"Bearer {token}"
+        "Authorization": f"Bearer {token}",
     }
