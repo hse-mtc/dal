a5320e44f3805d6cec83b76d4b28aa2f4caede47
TmLev <sololev27@gmail.com>
2021-04-07 13:22:29 +0300

    feat: log email too

--- watchdoc/src/main.py
+++ watchdoc/src/main.py
@@ -24,13 +24,15 @@ wds = WatchDocService()
 @watchdoc.post("/applicants/")
 def post_applicant(applicant: Applicant, background_tasks: BackgroundTasks):
     def closure():
-        log.info("Generating documents...")
+        email = applicant.contact_info.corporate_email
+
+        log.info(f"{email}: generating documents...")
         wds.generate_documents(applicant)
 
-        log.info("Uploading documents...")
+        log.info(f"{email}: uploading documents...")
         folder_link = wds.upload_documents(applicant)
 
-        log.info("Notifying applicant...")
+        log.info(f"{email}: notifying applicant...")
         wds.notify(applicant, folder_link)
 
     background_tasks.add_task(closure)
