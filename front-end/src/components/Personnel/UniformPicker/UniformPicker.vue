5073b039539fae0657a0fbcac2bd78ec29fb6a9a
Grigory Khromov <gakhromov@yandex.ru>
2021-04-12 12:29:14 +0000

    feat: front-end is mostly done, have to wait when graphics are done

--- front-end/src/components/Personnel/UniformPicker/UniformPicker.vue
+++ front-end/src/components/Personnel/UniformPicker/UniformPicker.vue
@@ -1,43 +1,44 @@
 <template>
   <div class="text-center">
-    <el-tabs type="border-card">
+    <el-tabs type="border-card" @tab-click="handleClick">
       <el-tab-pane
         v-for="milfaculty in milfaculties"
-        :key="milfaculty"
-        :label="milfaculty"
+        :key="milfaculty.milfaculty"
+        :label="milfaculty.milfaculty"
+        v-loading="milfaculty.milfaculty !== uniform.milfaculty"
       >
         <div>
           <el-button
             icon="el-icon-caret-left"
             circle
-            @click="cycleThroughHeaddresses()"
+            @click="cycleThroughHeaddresses"
           ></el-button>
           <el-button
             icon="el-icon-caret-left"
             circle
-            @click="cycleThroughOuterwears()"
+            @click="cycleThroughOuterwears"
           ></el-button>
           <!-- SVG human image here -->
           <p>
-            {{ headdresses[headdress_index] }} +
-            {{ outerwears[outerwear_index] }}
+            {{ uniform.headdress }} +
+            {{ uniform.outerwear }}
           </p>
           <el-button
             icon="el-icon-caret-right"
             circle
-            @click="cycleThroughHeaddresses()"
+            @click="cycleThroughHeaddresses"
           ></el-button>
           <el-button
             icon="el-icon-caret-right"
             circle
-            @click="cycleThroughOuterwears()"
+            @click="cycleThroughOuterwears"
           ></el-button>
         </div>
         <el-button
           icon="el-icon-check"
           type="success"
           style="margin-top: 20px"
-          @click="confirmUniform(milfaculty)"
+          @click="confirmUniform"
           >Утвердить</el-button
         >
       </el-tab-pane>
@@ -54,74 +55,78 @@ export default {
   name: "",
   data() {
     return {
-      uniforms: {},
+      uniform: {},
       milfaculties: [],
       headdresses: ["CA", "HA"],
       outerwears: ["JA", "PC"],
-      headdress_index: 0,
-      outerwear_index: 0,
     };
   },
-  created() {
-    var tmp_milfaculties, tmp_uniforms;
-
-    Promise.all([getMilFaculties(), getUniforms()])
-      .then((responses) => {
-        tmp_milfaculties = responses[0].data;
-        tmp_uniforms = responses[1].data;
-        // some processing
-        for (var i = 0; i < tmp_uniforms.length; ++i) {
-          this.milfaculties.push(tmp_uniforms[i].milfaculty);
-          this.uniforms[tmp_uniforms[i].milfaculty] = tmp_uniforms[i];
-        }
-        this.milfaculties.sort();
-        // if there are milfaculties without uniforms, create them
-        for (var i = 0; i < tmp_milfaculties.length; ++i) {
-          if (!this.milfaculties.includes(tmp_milfaculties[i].milfaculty))
-            createUniform({
-              headdress: "CA",
-              outerwear: "JA",
-              milfaculty: tmp_milfaculties[i].milfaculty,
-            })
-              .then((response) => {
-                console.log(
-                  `Created uniform for ${milfaculty} with code ${response.status}`
-                );
-              })
-              .catch((err) => {
-                postError("формы одежды", err.response.status);
-              });
-        }
-      })
-      .catch((errors) => {
-        for (var i = 0; i < errors.length; ++i)
-          getError("данных", errors[i].response.status);
-      });
-  },
   methods: {
+    fetchUniform(milfaculty) {
+      getUniforms({
+        milfaculty: milfaculty
+      })
+      .then((response) => {
+        if (response.data.length === 0) {
+          // Create 
+          createUniform({
+            headdress: "CA",
+            outerwear: "JA",
+            milfaculty: milfaculty,
+          })
+          .then((resp) => {
+            this.uniform = resp.data;
+          })
+          .catch((err) => {
+            postError("формы одежды", err.response.status);
+          });
+        }
+        else
+          this.uniform = response.data[0];
+      })
+      .catch((err) => {
+        getError("информации о форме одежды", err.response.status);
+      });
+    },
+    handleClick(obj) {
+      this.fetchUniform(obj.label);
+    },
     cycleThroughHeaddresses() {
-      this.headdress_index =
-        (this.headdress_index + 1) % this.headdresses.length;
+      var headdress_index = this.headdresses.indexOf(this.uniform.headdress);
+      headdress_index = (headdress_index + 1) % this.headdresses.length;
+      this.uniform.headdress = this.headdresses[headdress_index];
     },
     cycleThroughOuterwears() {
-      this.outerwear_index =
-        (this.outerwear_index + 1) % this.outerwears.length;
+      var outerwear_index = this.outerwears.indexOf(this.uniform.outerwear);
+      outerwear_index = (outerwear_index + 1) % this.outerwears.length;
+      this.uniform.outerwear = this.outerwears[outerwear_index];
     },
-    confirmUniform(milfaculty) {
+    confirmUniform() {
       changeUniform(
         {
-          headdress: this.headdresses[this.headdress_index],
-          outerwear: this.outerwears[this.outerwear_index],
+          headdress: this.uniform.headdress,
+          outerwear: this.uniform.outerwear,
         },
-        this.uniforms[milfaculty].id
+        this.uniform.id
       )
-        .then((response) => {
+        .then(() => {
           patchSuccess("формы одежды");
         })
         .catch((err) => {
           patchError("формы одежды", err.response.status);
         });
     },
+  },
+  created() {
+    getMilFaculties()
+    .then((response) => {
+      this.milfaculties = response.data;
+      this.fetchUniform(this.milfaculties[0].milfaculty);
+    })
+    .catch((err) => {
+      console.log(err);
+      getError("информации о циклах", err.response.status);
+    });
   },
 };
 </script>
