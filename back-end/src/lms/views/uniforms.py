51fed814fdfb59e5030d827bfb0343c0bb968554
Grigory Khromov <gakhromov@yandex.ru>
2021-04-12 11:23:08 +0000

    feat: added filters to uniforms

--- back-end/src/lms/views/uniforms.py
+++ back-end/src/lms/views/uniforms.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from drf_spectacular.views import extend_schema
+from django_filters.rest_framework import DjangoFilterBackend
 
 from lms.models.uniforms import Uniform
 from lms.serializers.uniforms import UniformSerializer, UniformMutateSerializer
+from lms.filters.uniforms import UniformFilter
 
 from common.constants import MUTATE_ACTIONS
 from auth.permissions import BasicPermission
@@ -17,6 +19,9 @@ class UniformViewSet(ModelViewSet):
     queryset = Uniform.objects.all()
 
     permission_classes = [UniformPermission]
+    filter_backends = [DjangoFilterBackend]
+
+    filterset_class = UniformFilter
 
     def get_serializer_class(self):
         if self.action in MUTATE_ACTIONS:
