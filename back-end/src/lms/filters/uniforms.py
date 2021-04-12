c3074b1076229049696d1010f4a2338f237cc8fa
TmLev <sololev27@gmail.com>
2021-04-12 23:55:26 +0300

    style: apply yapf

--- back-end/src/lms/filters/uniforms.py
+++ back-end/src/lms/filters/uniforms.py
@@ -2,8 +2,9 @@ from django_filters.rest_framework import FilterSet
 
 from lms.models.uniforms import Uniform
 
+
 class UniformFilter(FilterSet):
-    
+
     class Meta:
         model = Uniform
         fields = ['milfaculty']
