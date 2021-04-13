9e0453c1c0f2414a1300ffa50c308068cf0acbe5
TmLev <sololev27@gmail.com>
2021-04-13 17:57:01 +0300

    refactor: extract timezone to config

--- tgbot/src/utils/time.py
+++ tgbot/src/utils/time.py
@@ -1,17 +1,21 @@
-import pytz
+import typing as tp
+
 from datetime import datetime
 
+import pytz
+
 from api.client import client
+from config import TIMEZONE
 
 
 async def fetch_restriction_time() -> str:
     async with client.get('lms/absence-time/') as response:
-        data: list[dict[str, tp.Any]] = await response.json()
+        data: dict[str, tp.Any] = await response.json()
     return data['absence_restriction_time']
 
 
 def utc_to_local(utc_dt: datetime) -> datetime:
-    local_tz = pytz.timezone('Europe/Moscow')
+    local_tz = pytz.timezone(TIMEZONE)
     local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
     return local_tz.normalize(local_dt)
 
