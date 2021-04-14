678a521fb1dd5a523f217093eddd7ba37a638acd
TmLev <sololev27@gmail.com>
2021-04-14 14:40:16 +0300

    feat: register cover

--- back-end/src/dms/admin.py
+++ back-end/src/dms/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from dms.models.books import (
+    Cover,
     Book,
     FavoriteBook,
 )
@@ -19,6 +20,7 @@ from dms.models.common import (
 )
 
 admin.site.register(Author)
+admin.site.register(Cover)
 admin.site.register(Book)
 admin.site.register(FavoriteBook)
 admin.site.register(Category)
