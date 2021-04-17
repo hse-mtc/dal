af59c6a32eb0517138df05415595f4e2f12161bc
TmLev <sololev27@gmail.com>
2021-04-17 17:00:54 +0300

    feat: filter students by campus

--- back-end/src/lms/filters/students.py
+++ back-end/src/lms/filters/students.py
@@ -1,10 +1,22 @@
-from django_filters.rest_framework import FilterSet
+from django_filters.rest_framework import (
+    FilterSet,
+    ChoiceFilter,
+)
 
 from lms.models.students import Student
+from lms.models.universities import UniversityInfo
 
 
 class StudentFilter(FilterSet):
+    campus = ChoiceFilter(
+        choices=UniversityInfo.Campus.choices,
+        method="filter_by_campus",
+    )
+
+    def filter_by_campus(self, queryset, name, value):
+        # pylint: disable=unused-argument
+        return queryset.filter(university_info__campus=value)
 
     class Meta:
         model = Student
-        fields = ['status', 'milgroup']
+        fields = ['status', 'milgroup', 'campus']
