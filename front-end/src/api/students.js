eb04cc9bd3501fa036e21b66e35188d09716f5d0
TmLev <sololev27@gmail.com>
2021-05-31 15:10:28 +0300

    fix: redundant slash

--- front-end/src/api/students.js
+++ front-end/src/api/students.js
@@ -59,5 +59,5 @@ export const updateStudentApplicationInfo = (id, data) => request({
 });
 
 export function getApplicationsExcelDownloadLink(campus) {
-  return `${BASE_API_URL}${applicationsExport}/?campus=${campus}`;
+  return `${BASE_API_URL}${applicationsExport}?campus=${campus}`;
 }
