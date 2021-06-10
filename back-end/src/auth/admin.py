4bc21c62e2ce3bac4454e165fce0131df2e49db9
Grigory Khromov <gakhromov@yandex.ru>
2021-06-10 13:12:07 +0000

    feat: add Permission and Group to admin

--- back-end/src/auth/admin.py
+++ back-end/src/auth/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from auth.models import Permission, Group
+
 admin.site.register(get_user_model())
+
+admin.site.register(Permission)
+admin.site.register(Group)
