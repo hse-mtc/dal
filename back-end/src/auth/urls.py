6883b7f762326931da6300f5fe1c2f4e4e50cb33
Grigory Khromov <gakhromov@yandex.ru>
2021-06-10 13:54:17 +0000

    feat: add post and delete for permissions

--- back-end/src/auth/urls.py
+++ back-end/src/auth/urls.py
@@ -1,13 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from auth.views import (
     UserRetrieveAPIView,
+    UserControlViewSet,
     TokenObtainPairExtendedView,
     TokenRefreshExtendedView,
     ChangePasswordAPIView,
 )
 
+routers = DefaultRouter()
+routers.register("user/control", UserControlViewSet)
+
 urlpatterns = [
+    path("", include(routers.urls)),
     path("user/", UserRetrieveAPIView.as_view()),
     path("tokens/obtain/", TokenObtainPairExtendedView.as_view()),
     path("tokens/refresh/", TokenRefreshExtendedView.as_view()),
--- back-end/src/auth/views.py
+++ back-end/src/auth/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework import permissions
+from rest_framework import permissions, viewsets
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import action
 
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.request import Request
@@ -13,7 +15,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from drf_spectacular.utils import OpenApiParameter
 from drf_spectacular.views import extend_schema
+
+from auth.models import Permission
 
 from auth.serializers import (
     UserSerializer,
@@ -31,6 +36,57 @@ class UserRetrieveAPIView(RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+@extend_schema(tags=["auth"])
+class UserControlViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+    permission_classes = []
+
+    @extend_schema(parameters=[
+        OpenApiParameter(name="permission",
+                         description="Permission code",
+                         required=True,
+                         type=str),
+    ])
+    @action(detail=True, methods=["post", "delete"])
+    def permissions(self, request: Request) -> Response:
+        """
+        Modify user permissions
+        """
+        if "permission" not in request.query_params:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        viewset, method, scope = request.query_params["permission"].split(".")
+        scope = int(getattr(Permission.Scopes, scope.upper()))
+        user = self.get_object()
+
+        if request.method == "POST":
+            permission = Permission.objects.filter(viewset=viewset,
+                                                   method=method,
+                                                   scope=scope)
+            if permission.count() == 0:
+                return Response(status=status.HTTP_400_BAD_REQUEST,
+                                data="There is no such permission")
+            permission = permission[0]
+            # adding the same permission does nothing,
+            # so no need to check for existing permissions
+            user.permissions.add(permission)
+            return Response(status=status.HTTP_200_OK, data="Ok")
+
+        # request.method == "DELETE":
+        permission = user.permissions.filter(viewset=viewset,
+                                             method=method,
+                                             scope=scope)
+        if permission.count() == 0:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data="There is no such permission in user permissions")
+        permission = permission[0]
+        user.permissions.remove(permission)
+        return Response(status=status.HTTP_200_OK, data="Ok")
 
 
 @extend_schema(tags=["auth"])
