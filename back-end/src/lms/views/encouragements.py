9e14941719fca147aab19e1a406fd0841ab800a5
Grigory Khromov <gakhromov@yandex.ru>
2021-06-30 13:32:27 +0000

    feat: add scopes to encouragement

--- back-end/src/lms/views/encouragements.py
+++ back-end/src/lms/views/encouragements.py
@@ -6,16 +6,26 @@ from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.views import extend_schema
 from common.constants import MUTATE_ACTIONS
 
+from lms.models.students import Student
+from lms.models.teachers import Teacher
 from lms.models.encouragements import Encouragement
 from lms.serializers.encouragements import (EncouragementSerializer,
                                             EncouragementMutateSerializer)
 from lms.filters.encouragements import EncouragementFilter
 
+from auth.models import Permission
 from auth.permissions import BasePermission
 
 
 class EncouragementPermission(BasePermission):
     permission_class = 'encouragement'
+    view_name_rus = 'Поощрения'
+    scopes = [
+        Permission.Scopes.ALL,
+        Permission.Scopes.MILFACULTY,
+        Permission.Scopes.MILGROUP,
+        Permission.Scopes.SELF,
+    ]
 
 
 @extend_schema(tags=['encouragements'])
@@ -32,3 +42,50 @@ class EncouragementViewSet(ModelViewSet):
         if self.action in MUTATE_ACTIONS:
             return EncouragementMutateSerializer
         return EncouragementSerializer
+
+    # pylint: disable=too-many-return-statements
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return self.queryset
+
+        scope = self.request.user.get_perm_scope(
+            EncouragementPermission.permission_class, self.request.method)
+
+        if scope == Permission.Scopes.ALL:
+            return self.queryset
+
+        # check if user is a teacher ot a student
+        user = Teacher.objects.filter(user=self.request.user)
+        user_type = 'teacher'
+        if user.count() == 0:
+            # check if user is a student
+            user = Student.objects.filter(user=self.request.user)
+            user_type = 'student'
+            if user.count() == 0:
+                # return nothing is user is not a student or a teacher
+                return self.queryset.none()
+
+        if scope == Permission.Scopes.SELF:
+            res = self.queryset.filter(**{user_type: user[0]})
+            if res.count() == 0:
+                return self.queryset.none()
+            return res
+
+        if scope == Permission.Scopes.MILGROUP:
+            # we are only interested in encouragements that
+            # are given to students of milgroup == teacher/student milgroup
+            res = self.queryset.filter(student__milgroup=user[0].milgroup)
+            if res.count() == 0:
+                return self.queryset.none()
+            return res
+
+        if scope == Permission.Scopes.MILFACULTY:
+            # we are only interested in encouragements that
+            # are given to students of milfaculty == teacher/student milfaculty
+            res = self.queryset.filter(
+                student__milgroup__milfaculty=user[0].milfaculty)
+            if res.count() == 0:
+                return self.queryset.none()
+            return res
+
+        return self.queryset.none()
