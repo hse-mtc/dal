b399c14a40242b981be97510c0309b41165bf5c6
Taranenko Andrey <a.taranenko@rambler-co.ru>
2021-07-01 16:05:17 +0300

    feat: vue-property-decorator added

--- front-end/babel.config.js
+++ front-end/babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ["@vue/app"],
+  plugins: [
+    ["@babel/proposal-decorators", { legacy: true }],
+    ["@babel/proposal-class-properties", { loose: true }],
+  ],
 };
