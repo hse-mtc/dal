589297db308bc91561bbb1adf6e072ebda07d866
Taranenko Andrey <a.taranenko@rambler-co.ru>
2021-07-01 17:24:01 +0300

    fix: styles import

--- front-end/src/primeApply.js
+++ front-end/src/primeApply.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 
 import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
 import PrimeTable from "primevue/datatable";
 import PrimeColumn from "primevue/column";
