34b9db9d5ef860edc7a35c1fb0b3e607ba74d2a0
TmLev <sololev27@gmail.com>
2021-07-02 18:35:43 +0300

    refactor: adjust to client & button text

--- tgbot/src/handlers/menu.py
+++ tgbot/src/handlers/menu.py
@@ -1,9 +1,14 @@
 from aiogram.types import Message
 
-from keyboards.reply import start_keyboard
+from keyboards.reply import list_milgroup_keyboard
+from keyboards.button_texts import ButtonText
 
 
 async def menu_handler(message: Message) -> None:
-    await message.answer(
-        'Возврат в главное меню. Выберите нужное действие',
-        reply_markup=start_keyboard())
+    await message.answer('Возврат в главное меню. Выберите нужное действие',
+                         reply_markup=list_milgroup_keyboard())
+
+
+menu_handler.handler_filters = [
+    lambda message: message.text == ButtonText.MAIN_MENU.value,
+]
