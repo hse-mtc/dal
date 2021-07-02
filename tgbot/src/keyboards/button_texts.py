17bd7558b95ba8d3de5259e2a6a77a56ee88c55e
TmLev <sololev27@gmail.com>
2021-07-02 18:27:38 +0300

    feat: extract button texts

---
+++ tgbot/src/keyboards/button_texts.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class ButtonText(Enum):
+    MAIN_MENU = "Главное меню"
+    LIST_MILGROUP = "Личный состав"
+    REPORT_ABSENCE = "Доложить расход"
+    SHARE_CONTACT = "Поделиться контактом"
