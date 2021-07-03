c84b13292f45ee28fda0c185788854ee04577bf0
 vladisa88 <vladisa88@gmail.com>
2021-07-03 14:54:54 +0300

    fix: filter on last semester

--- back-end/src/lms/views/dashboard.py
+++ back-end/src/lms/views/dashboard.py
@@ -1,6 +1,5 @@
-from statistics import mean
-
 from rest_framework.views import APIView
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -9,6 +8,33 @@ from drf_spectacular.views import extend_schema
 from lms.models.marks import Mark
 from lms.models.absences import Absence
 from lms.models.lessons import Lesson
+from lms.models.students import Student
+
+from lms.serializers.students import (
+    StudentBasicInfoSerializer,
+    StudentExtraInfoSerializer,
+    StudentSkillsSerializer,
+)
+
+from lms.functions import get_current_semester_range
+
+
+@extend_schema(tags=["students"])
+class StudentBasicInfoViewSet(ReadOnlyModelViewSet):
+    queryset = Student.objects.order_by("surname", "name", "patronymic", "id")
+    serializer_class = StudentBasicInfoSerializer
+
+
+@extend_schema(tags=["students"])
+class StudentExtraInfoViewSet(ReadOnlyModelViewSet):
+    queryset = Student.objects.order_by("surname", "name", "patronymic", "id")
+    serializer_class = StudentExtraInfoSerializer
+
+
+@extend_schema(tags=["students"])
+class StudentSkillsView(ReadOnlyModelViewSet):
+    queryset = Student.objects.order_by("surname", "name", "patronymic", "id")
+    serializer_class = StudentSkillsSerializer
 
 
 @extend_schema(tags=["students"])
@@ -16,10 +42,13 @@ class StudentPerformanceView(APIView):
 
     # pylint:disable=("invalid-name")
     def get(self, request: Request, pk: int) -> Response:
+        date_range = get_current_semester_range()
         marks = Mark.objects.filter(student=pk).select_related(
             "lesson", "lesson__subject")
-        lessons = Lesson.objects.all().select_related("subject")
-        absences_dates = Absence.objects.filter(student=pk).values_list("date")
+        lessons = Lesson.objects.filter(
+            date__range=date_range).select_related("subject")
+        absences_dates = Absence.objects.filter(
+            student=pk, date__range=date_range).values_list("date")
         absences_dates = [date[0] for date in absences_dates]
         student_subject_marks = {}
         subject_dates = {}
@@ -30,11 +59,12 @@ class StudentPerformanceView(APIView):
                                              []).append(*mark.mark)
         for lesson in lessons:
             subject_dates.setdefault(lesson.subject.title,
-                                        []).append(lesson.date)
+                                     []).append(lesson.date)
 
         for subject in student_subject_marks:
             current_marks = student_subject_marks[subject]
-            student_subject_marks[subject] = float(sum(current_marks) / len(current_marks))
+            student_subject_marks[subject] = float(
+                sum(current_marks) / len(current_marks))
 
         for subject in subject_dates:
             subject_dates[subject] = len(
