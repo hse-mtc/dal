f6c8a25f9d5dc58deeb1215bc16aae20574e9fa7
TmLev <sololev27@gmail.com>
2021-07-03 16:15:06 +0300

    feat: add personnel users search view

---
+++ back-end/src/lms/views/personnel.py
@@ -0,0 +1,51 @@
+from django.db.models import Q
+from django.contrib.auth import get_user_model
+
+from rest_framework.mixins import ListModelMixin
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from drf_spectacular.views import (
+    extend_schema,
+    OpenApiParameter,
+)
+
+from common.models.persons import Personnel
+
+from lms.serializers.personnel import (
+    SearchPersonnelUsersSerializer,
+    PersonnelUsersSerializer,
+)
+
+
+@extend_schema(
+    tags=["personnel"],
+    parameters=[
+        OpenApiParameter(
+            name="name",
+            description="Filter by full name",
+            required=True,
+        ),
+    ],
+)
+class SearchPersonnelUsersViewSet(ListModelMixin, GenericViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = PersonnelUsersSerializer
+
+    def list(self, request: Request, *args, **kwargs) -> Response:
+        serializer = SearchPersonnelUsersSerializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+
+        name = serializer.validated_data["name"]
+        query = (Q(surname__icontains=name) | Q(name__icontains=name) |
+                 Q(patronymic__icontains=name))
+
+        results = []
+
+        for model in Personnel.__subclasses__():
+            queryset = model.objects.filter(user__isnull=False).filter(query)
+            serializer = PersonnelUsersSerializer(queryset, many=True)
+            results.extend(serializer.data)
+
+        return Response(data=results)
