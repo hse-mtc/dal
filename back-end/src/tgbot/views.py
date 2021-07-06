bd17f965fb86ccac1e93b8e9095c13bd2acd0b88
TmLev <sololev27@gmail.com>
2021-07-06 12:18:14 +0300

    refactor: remove tgbot populate

--- back-end/src/tgbot/views.py
+++ back-end/src/tgbot/views.py
@@ -1,10 +1,5 @@
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
-from rest_framework.decorators import permission_classes
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
-from rest_framework.decorators import api_view
 
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -21,20 +16,3 @@ class SessionViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
 
     filterset_class = SessionFilter
-
-
-def create_codes():
-    values = ['ckld']
-
-    for value in values:
-        session, _ = Session.objects.get_or_create(code=value)
-        session.save()
-
-
-@api_view(['POST'])
-@permission_classes((AllowAny,))
-def populate(request: Request) -> Response:
-    create_codes()
-
-    return Response({'message': 'Population successful'},
-                    status=HTTP_201_CREATED)
