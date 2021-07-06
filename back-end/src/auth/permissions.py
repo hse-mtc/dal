b29e2774439190959e09130e36ff4d6de2615ff3
TmLev <sololev27@gmail.com>
2021-07-06 08:58:33 +0300

    fix: allow superuser anything

--- back-end/src/auth/permissions.py
+++ back-end/src/auth/permissions.py
@@ -14,6 +14,10 @@ class BasePermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
+
+        if request.user.is_superuser:
+            return True
+
         # check for method permission
         return request.user.has_general_perm(self.permission_class,
                                              request.method)
