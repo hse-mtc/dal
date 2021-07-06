854ef818f758d2594622e6088badd1499b6203ac
TmLev <sololev27@gmail.com>
2021-07-06 16:13:32 +0300

    feat: notify milgroup leaders on uniform update

--- tgbot/src/routes/uniforms.py
+++ tgbot/src/routes/uniforms.py
@@ -1,4 +1,13 @@
+import asyncio
+
 from aiohttp import web
+
+from aiogram.bot import Bot
+
+import config
+
+from api.auth import fetch_session
+from api.student import fetch_milgroup_leader_phones
 
 from proto.uniforms import Uniform
 
@@ -9,7 +18,18 @@ routes = web.RouteTableDef()
 async def post_uniforms(request: web.Request) -> web.Response:
     body = await request.json()
     uniform = Uniform.from_raw(body)
-    # TODO(TmLev): fetch milgroup commanders from `uniform.milfaculty`
-    #              and send them uniform update. Remove debug print.
-    print(uniform)
-    return web.Response(text="Hello!")
+
+    bot: Bot = request.app[config.BOT]
+    phones = await fetch_milgroup_leader_phones(milfaculty=uniform.milfaculty)
+
+    sessions = await asyncio.gather(*[
+        fetch_session(params={"phone": phone})
+        for phone in phones
+    ])
+
+    await asyncio.gather(*[
+        bot.send_message(chat_id=session.chat_id, text=f"{uniform}")
+        for session in sessions
+    ])
+
+    return web.Response(body={"detail": "Successful uniform notification."})
