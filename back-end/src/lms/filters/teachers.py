38c84881f760a495865c0fd14562361e90a06024
Grigory Khromov <gakhromov@yandex.ru>
2021-07-12 10:47:44 +0000

    fix: teacher milgroupp filter

--- back-end/src/lms/filters/teachers.py
+++ back-end/src/lms/filters/teachers.py
@@ -1,10 +1,15 @@
 from django_filters.rest_framework import FilterSet
+from django_filters import NumberFilter
 
 from lms.models.teachers import Teacher
 
 
 class TeacherFilter(FilterSet):
+    milgroup = NumberFilter(
+        field_name="milgroups__id",
+        lookup_expr="contains",
+    )
 
     class Meta:
         model = Teacher
-        fields = ["milgroups", "milfaculty", "rank", "post"]
+        fields = ["milgroup", "milfaculty", "rank", "post"]
