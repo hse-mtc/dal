c2d3113a7a3c922ce0df5c2f4453b65893650329
Taranenko Andrey <a.taranenko@rambler-co.ru>
2021-07-19 16:35:15 +0300

    feat: generic form added

---
+++ front-end/src/common/Form/index.vue
@@ -0,0 +1,123 @@
+<template>
+  <el-form
+    ref="form"
+    :class="$style.root"
+    :model="formData"
+    :rules="rules"
+    action="#"
+    @submit.native.prevent="validate"
+    @keypress.enter.native.prevent="validate"
+  >
+    <el-form-item
+      v-for="({ component, title, props = {} }, key) in fields"
+      :key="key"
+      :prop="key"
+    >
+      <component
+        :is="components[component]"
+        :model-value="formData[key]"
+        :title="title"
+        v-bind="props"
+        @change="onChange(key, $event)"
+      />
+    </el-form-item>
+
+    <el-form-item>
+      <slot name="buttons" :validate="validate">
+        <el-button
+          type="primary"
+          @click="validate"
+        >
+          Сохранить
+        </el-button>
+      </slot>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+import {
+  Component,
+  Emit,
+  Model,
+  Prop,
+  Ref,
+} from "vue-property-decorator";
+
+import {
+  DateInput,
+  FileInput,
+  NumberInput,
+  SelectInput,
+  TagsInput,
+  TextInput,
+  SingleCheckbox,
+} from "@/common/inputs";
+
+@Component({
+  name: "GenericForm",
+  components: {
+    DateInput,
+    FileInput,
+    NumberInput,
+    SelectInput,
+    TagsInput,
+    TextInput,
+    SingleCheckbox,
+  },
+})
+class GenericForm {
+  @Model("change", { type: Object, required: true }) formData
+  @Prop({ type: Object, required: true, default: () => ({}) }) fields
+  @Prop({ type: Object, default: () => ({}) }) rules
+  @Prop({ type: Function, default: () => true }) onSubmit
+
+  @Ref() form
+
+  components = {
+    date: DateInput,
+    file: FileInput,
+    number: NumberInput,
+    select: SelectInput,
+    tags: TagsInput,
+    text: TextInput,
+    checkbox: SingleCheckbox,
+  }
+
+  @Emit("change")
+  onChange(field, value) {
+    return {
+      ...this.formData,
+      [field]: value,
+    };
+  }
+
+  validate() {
+    let isValid = false;
+
+    this.form.validate(valid => {
+      if (valid) {
+        isValid = this.onSubmit();
+      }
+    });
+
+    if (!isValid) {
+      this.$message({
+        type: "error",
+        message: "Проверьте правильность заполняемых полей",
+      });
+    }
+
+    return isValid;
+  }
+}
+
+export default GenericForm;
+</script>
+
+<style lang="scss" module>
+:global(.el-select-dropdown__item) {
+  height: auto !important;
+  white-space: normal;
+}
+</style>
