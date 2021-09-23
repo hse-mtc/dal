dbc19739a4ae468edf17c967b6a9ce201519a69c
Altius01 <aaeberil@edu.hse.ru>
2021-09-23 22:00:56 +0300

    Added first test

--- back-end/src/dms/tests/test_students_endpoints.py
+++ back-end/src/dms/tests/test_students_endpoints.py
@@ -1,30 +1,68 @@
+from collections import OrderedDict
 from operator import itemgetter
 
 import pytest
+from django.utils.datetime_safe import datetime
+from rest_framework.fields import SerializerMethodField
 
+from common.models.persons import Photo, BirthInfo
 from dms.models.common import Author
-from lms.models.students import Student
+from lms.models.applicants import ApplicationProcess
+from lms.models.common import Milgroup
+from lms.models.students import Student, Skill
+from lms.models.universities import UniversityInfo
+
 
 @pytest.fixture
-def student_data():
+def test_student():
 
-    def call_me(surname: str = "stud_last",
-                name: str = "stud_first",
-                patronymic: str = "stud_patronymic") -> dict:
-        return {"surname": surname, "name": name, "patronymic": patronymic}
+    def call_me(
+        name: str = "first",
+        surname: str = "second",
+        patronymic: str = "patronymic",
+    ) -> tuple:
+
+        s = Student()
+        s.name = name
+        s.surname = surname
+        s.patronymic = patronymic
+        return s, {
+            "fullname": s.full_name,
+            "name": s.name,
+            "surname": s.surname,
+            "patronymic": s.patronymic,
+            "photo": s.photo,
+            "milgroup": s.milgroup,
+            "birth_info": s.birth_info,
+            "university_info": s.university_info,
+            "application_process": s.application_process,
+            "skills": [],
+            "contact_info": s.contact_info,
+            "citizenship": s.citizenship,
+            "permanent_address": s.permanent_address,
+            "surname_genitive": s.surname_genitive,
+            "name_genitive": s.name_genitive,
+            "patronymic_genitive": s.patronymic_genitive,
+            "status": s.status,
+            "post": s.post,
+            "user": s.user,
+            "milspecialty": s.milspecialty,
+            "passport": s.passport,
+            "recruitment_office": s.recruitment_office,
+            "family": []
+        }
 
     return call_me
 
+
 @pytest.mark.django_db
-def test_get_students_returns_list(su_client, student_data):
+def test_get_students_returns_list(su_client, test_student):
     # pylint: disable=too-many-locals
 
     count = 3
-    data = student_data()
     for _ in range(count):
-        Author.objects.create(**data)
-        Student.objects.create(**data)
-    print(Author.objects.get(id=1))
+        s, data = test_student()
+        s.save()
 
     response = su_client.get("/api/lms/students/")
     assert response.status_code == 200
