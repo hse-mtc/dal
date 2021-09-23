0deb4a1f7fed2d12aa1e1c0be98c7cd6cc6a1381
Altius01 <aaeberil@edu.hse.ru>
2021-09-23 21:52:20 +0300

    Added first test

--- back-end/src/dms/tests/test_students_endpoints.py
+++ back-end/src/dms/tests/test_students_endpoints.py
@@ -1,26 +1,32 @@
+from operator import itemgetter
+
 import pytest
+
+from dms.models.common import Author
 from lms.models.students import Student
 
 @pytest.fixture
 def student_data():
 
-    def call_me(surname: str = "last",
-                name: str = "first",
-                patronymic: str = "patronymic") -> dict:
+    def call_me(surname: str = "stud_last",
+                name: str = "stud_first",
+                patronymic: str = "stud_patronymic") -> dict:
         return {"surname": surname, "name": name, "patronymic": patronymic}
 
     return call_me
 
 @pytest.mark.django_db
-def test_get_students_returns_list(su_client, author_data):
+def test_get_students_returns_list(su_client, student_data):
     # pylint: disable=too-many-locals
 
     count = 3
-    data = author_data()
+    data = student_data()
     for _ in range(count):
+        Author.objects.create(**data)
         Student.objects.create(**data)
+    print(Author.objects.get(id=1))
 
-    response = su_client.get("/api/lms/authors/")
+    response = su_client.get("/api/lms/students/")
     assert response.status_code == 200
     assert len(response.data) == count
 
@@ -28,6 +34,6 @@ def test_get_students_returns_list(su_client, author_data):
     response.data.sort(key=id_getter)
     min_id = min(response.data, key=id_getter)["id"]
 
-    for id_, author in enumerate(response.data, start=min_id):
+    for id_, student in enumerate(response.data, start=min_id):
         data["id"] = id_
-        assert author == data
+        assert student == data
