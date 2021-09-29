fb73c60fc0af5839214d03820dc5e99e6aab6f8a
Altius01 <aaeberil@edu.hse.ru>
2021-09-29 11:37:52 +0300

    Fixed conftest.py

--- back-end/src/lms/tests/conftest.py
+++ back-end/src/lms/tests/conftest.py
@@ -284,6 +284,36 @@ def student_photo():
     return call_me
 
 
+SUPERUSER_EMAIL = "superuserfortests@mail.com"
+SUPERUSER_PASSWORD = "superuserpasswordfortests"
+
+
+@pytest.fixture
+def superuser(db):
+    user = User.objects.filter(email=SUPERUSER_EMAIL)
+    if user.exists():
+        return user.first()
+
+    return User.objects.create_superuser(
+        email=SUPERUSER_EMAIL,
+        password=SUPERUSER_PASSWORD,
+    )
+
+
+@pytest.fixture
+def su_client(superuser):
+    from django.test.client import Client
+    response = Client().post(
+        "/api/auth/tokens/obtain/",
+        {
+            "email": SUPERUSER_EMAIL,
+            "password": SUPERUSER_PASSWORD
+        },
+        content_type="application/json",
+    )
+    access_token = response.data["access"]
+    return Client(HTTP_AUTHORIZATION=f"Bearer {access_token}")
+
 @pytest.fixture
 def student_data():
 
