c98864e60d5f5153b3803127a87e5784043cd3f9
Altius01 <aaeberil@edu.hse.ru>
2021-09-29 11:16:10 +0300

    Fixed mispath

--- back-end/src/dms/tests/conftest.py
+++ back-end/src/dms/tests/conftest.py
@@ -9,7 +9,6 @@ from django.contrib.auth import get_user_model
 from dms.models.documents import File
 
 from auth.models import User
-from lms.models.students import Student
 
 SUPERUSER_EMAIL = "superuserfortests@mail.com"
 SUPERUSER_PASSWORD = "superuserpasswordfortests"
@@ -118,49 +117,6 @@ def paper_data(file):
             "upload_date": upload_date,
             "publication_date": publication_date,
             "is_binned": is_binned
-        }
-
-    return call_me
-
-
-@pytest.fixture
-def student_data():
-
-    def call_me(
-        name: str = "first",
-        surname: str = "second",
-        patronymic: str = "patronymic",
-    ) -> tuple:
-        # pylint: disable=too-many-locals
-
-        s = Student()
-        s.name = name
-        s.surname = surname
-        s.patronymic = patronymic
-        return s, {
-            "fullname": s.full_name,
-            "name": s.name,
-            "surname": s.surname,
-            "patronymic": s.patronymic,
-            "photo": s.photo,
-            "milgroup": s.milgroup,
-            "birth_info": s.birth_info,
-            "university_info": s.university_info,
-            "application_process": s.application_process,
-            "skills": [],
-            "contact_info": s.contact_info,
-            "citizenship": s.citizenship,
-            "permanent_address": s.permanent_address,
-            "surname_genitive": s.surname_genitive,
-            "name_genitive": s.name_genitive,
-            "patronymic_genitive": s.patronymic_genitive,
-            "status": s.status,
-            "post": s.post,
-            "user": s.user,
-            "milspecialty": s.milspecialty,
-            "passport": s.passport,
-            "recruitment_office": s.recruitment_office,
-            "family": []
         }
 
     return call_me
