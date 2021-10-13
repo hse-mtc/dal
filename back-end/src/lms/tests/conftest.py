f39ff458a4cd5e5ff63ecf52d7d50a9894e95bff
Danil Mirt <dimirtalibov@miem.hse.ru>
2021-10-13 11:25:55 +0300

    new tests started

--- back-end/src/lms/tests/conftest.py
+++ back-end/src/lms/tests/conftest.py
@@ -14,34 +14,6 @@ SUPERUSER_PASSWORD = "superuserpasswordfortests"
 
 
 @pytest.fixture
-def superuser(db):
-    user = User.objects.filter(email=SUPERUSER_EMAIL)
-    if user.exists():
-        return user.first()
-
-    return User.objects.create_superuser(
-        email=SUPERUSER_EMAIL,
-        password=SUPERUSER_PASSWORD,
-    )
-
-
-@pytest.fixture
-def su_client(superuser):
-    from django.test.client import Client
-
-    response = Client().post(
-        "/api/auth/tokens/obtain/",
-        {
-            "email": SUPERUSER_EMAIL,
-            "password": SUPERUSER_PASSWORD
-        },
-        content_type="application/json",
-    )
-    access_token = response.data["access"]
-    return Client(HTTP_AUTHORIZATION=f"Bearer {access_token}")
-
-
-@pytest.fixture
 def get_new_lesson_data() -> dict:
 
     def call_me(ids_only: bool = True):
---
+++ back-end/src/lms/tests/test_marks.py
@@ -0,0 +1,55 @@
+# pylint: disable=redefined-outer-name
+
+import pytest
+
+from lms.models.lessons import Lesson
+
+
+# TODO: надо вынести url отдельно, чтобы его не дублировать
+
+
+def unpack_lesson(lesson: Lesson, with_id: bool = False) -> dict:
+    lesson_data = {
+        "type": lesson.type,
+        "date": lesson.date,
+        "ordinal": lesson.ordinal,
+        "subject": lesson.subject.id,
+        "room": lesson.room.id,
+        "milgroup": lesson.milgroup.id,
+        "teacher": lesson.teacher.id,
+    }
+    if with_id:
+        lesson_data["id"] = lesson.id
+
+    return lesson_data
+
+
+@pytest.mark.django_db
+def test_trailing_slash_redirect(su_client):
+    response_wrong = su_client.get("/api/lms/marks")
+    assert response_wrong.status_code == 301
+
+    response_right = su_client.get("/api/lms/marks/")
+    assert response_right.status_code == 200
+
+
+@pytest.mark.django_db
+def test_post_posts_right_data(su_client, get_new_lesson_data):
+    lesson_data = get_new_lesson_data()
+
+    first_lesson_response = su_client.post("/api/lms/marks/",
+                                           lesson_data,
+                                           content_type="application/json")
+
+    second_lesson_response = su_client.post("/api/lms/marks/",
+                                            lesson_data,
+                                            content_type="application/json")
+
+    assert first_lesson_response.status_code == 201
+    assert second_lesson_response.status_code == 201
+
+    assert first_lesson_response.data.pop(
+        "id") != second_lesson_response.data.pop("id")
+
+    assert first_lesson_response.data == lesson_data
+    assert second_lesson_response.data == lesson_data
