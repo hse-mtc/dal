957694117bdced3db25f2136ad03e69e56b625b7
TmLev <sololev27@gmail.com>
2021-10-13 15:08:41 +0300

    cfg: Add email settings

--- back-end/src/conf/settings.py
+++ back-end/src/conf/settings.py
@@ -330,3 +330,13 @@ LOGGING = {
         }
     },
 }
+
+# Email settings
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ["EMAIL_HOST"]
+EMAIL_PORT = os.environ["EMAIL_PORT"]
+EMAIL_USE_TLS = os.environ["EMAIL_USE_TLS"].lower() == "true"
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
