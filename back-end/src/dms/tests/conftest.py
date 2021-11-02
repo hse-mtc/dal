1bd66ce4e3b4b7bc95f8f3197cd5532bfab5c827
deborisov <deborisov_1@edu.hse.ru>
2021-11-03 01:05:13 +0300

    Add book permissions test

--- back-end/src/dms/tests/conftest.py
+++ back-end/src/dms/tests/conftest.py
@@ -7,45 +7,19 @@ import pytest
 from django.core.files.base import ContentFile
 from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test.client import Client
 
 from PIL import Image
 
-from auth.models import User
+from auth.models import User, Permission
 
 from dms.models.common import Author, Publisher
 from dms.models.documents import File
 
 from common.models.subjects import Subject
 
-SUPERUSER_EMAIL = "superuserfortests@mail.com"
-SUPERUSER_PASSWORD = "superuserpasswordfortests"
-
-
-@pytest.fixture
-def superuser(db):
-    user = User.objects.filter(email=SUPERUSER_EMAIL)
-    if user.exists():
-        return user.first()
-
-    return User.objects.create_superuser(
-        email=SUPERUSER_EMAIL,
-        password=SUPERUSER_PASSWORD,
-    )
-
-
-@pytest.fixture
-def su_client(superuser):
-    from django.test.client import Client
-    response = Client().post(
-        "/api/auth/tokens/obtain/",
-        {
-            "email": SUPERUSER_EMAIL,
-            "password": SUPERUSER_PASSWORD
-        },
-        content_type="application/json",
-    )
-    access_token = response.data["access"]
-    return Client(HTTP_AUTHORIZATION=f"Bearer {access_token}")
+TEST_USER_EMAIL = "test_user@mail.com"
+TEST_USER_PASSWORD = "qwerty"
 
 
 @pytest.fixture
@@ -204,3 +178,44 @@ def book_data(image):
         }
 
     return call_me
+
+
+@pytest.fixture
+def permission_data():
+
+    def call_me(viewset: str = "null",
+                method: str = "get",
+                scope: str = "self"):
+        return {
+            "viewset": viewset,
+            "method": method,
+            "scope": getattr(Permission.Scope, scope.upper())
+        }
+
+    return call_me
+
+
+@pytest.fixture
+def test_user(db):
+    user = User.objects.filter(email=TEST_USER_EMAIL)
+    if user.exists():
+        return user.first()
+
+    user = User.objects.create_user(email=TEST_USER_EMAIL,
+                                    password=TEST_USER_PASSWORD)
+    user.save()
+    return user
+
+
+@pytest.fixture
+def test_client(test_user):
+    response = Client().post(
+        "/api/auth/tokens/obtain/",
+        {
+            "email": TEST_USER_EMAIL,
+            "password": TEST_USER_PASSWORD
+        },
+        content_type="application/json",
+    )
+    access_token = response.data["access"]
+    return Client(HTTP_AUTHORIZATION=f"Bearer {access_token}")
