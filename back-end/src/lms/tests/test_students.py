3907f8e33d0f5e4b951c5e585a83795447e6ec4d
Altius01 <aaeberil@edu.hse.ru>
2021-11-03 08:56:50 +0300

    Start work 3_11_21

--- back-end/src/lms/tests/test_students.py
+++ back-end/src/lms/tests/test_students.py
@@ -7,5 +7,11 @@ def assert_student_equals():
 
 
 @pytest.mark.django_db
-def test_get_skills_by_student_id(create_student):
-    create_student
+def test_get_skills_by_student_id(su_client, create_student, get_student_data):
+    stud = create_student
+    data = get_student_data(stud_id=stud.id)
+
+    response = su_client.get(f"/api/lms/students/skills/{stud.id}/")
+    assert response.status_code == 200
+
+    assert response.data["skills"] == data["skills"]
