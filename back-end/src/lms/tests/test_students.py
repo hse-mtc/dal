b13c716fde85fc254fa3e0d31b73f58385d6cb54
kolbus130 <78971703+kolbus130@users.noreply.github.com>
2021-11-03 13:35:40 +0300

    Написал часть теста get_students, там много зависимостей с другими ручками

--- back-end/src/lms/tests/test_students.py
+++ back-end/src/lms/tests/test_students.py
@@ -8,10 +8,22 @@ def assert_student_equals():
 
 @pytest.mark.django_db
 def test_get_skills_by_student_id(su_client, create_student, get_student_data):
-    stud = create_student
-    data = get_student_data(stud_id=stud.id)
+    student, student_ids = create_student()
+    data = get_student_data(student_ids=student_ids)
 
-    response = su_client.get(f"/api/lms/students/skills/{stud.id}/")
+    response = su_client.get(f'/api/lms/students/skills/{student.id}/')
     assert response.status_code == 200
 
-    assert response.data["skills"] == data["skills"]
+    assert response.data['skills'] == data['skills']
+
+
+@pytest.mark.django_db
+def test_get_all_students(su_client, create_student):
+    students_ids = []
+    #pylint: disable=unused-variable
+    for i in range(3):
+        _, students_id = create_student()
+        students_ids.append(students_id)
+
+    response = su_client.get('/api/lms/students/')
+    print(response)
