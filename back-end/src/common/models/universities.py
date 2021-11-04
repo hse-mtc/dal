2f15eaba9b7fed1504d3791bfe615210698133ff
TmLev <sololev27@gmail.com>
2021-11-05 00:18:17 +0300

    refactor: Extract `universities` models to `common`

--- back-end/src/common/models/universities.py
+++ back-end/src/common/models/universities.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 
+class Campus(models.TextChoices):
+    MOSCOW = "MO", "Москва"
+    SAINT_PETERSBURG = "SP", "Санкт-Петербург"
+    NIZHNY_NOVGOROD = "NN", "Нижний Новгород"
+    PERM = "PE", "Пермь"
+
+
 class Faculty(models.Model):
+    campus = models.CharField(
+        choices=Campus.choices,
+        max_length=2,
+    )
     title = models.CharField(
         unique=True,
         max_length=127,
@@ -16,24 +27,15 @@ class Faculty(models.Model):
 
 
 class Program(models.Model):
-    code = models.CharField(
-        unique=True,
-        max_length=8,
-    )
-    title = models.CharField(
-        max_length=127,
-        blank=True,
-        null=True,
-    )
+    code = models.CharField(max_length=63)
+    title = models.CharField(max_length=127)
     faculty = models.ForeignKey(
         to=Faculty,
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True,
+        on_delete=models.RESTRICT,
     )
 
     def __str__(self):
-        return f"({self.code}) {self.title}"
+        return f"[{self.code}] {self.title}"
 
     class Meta:
         verbose_name = "Educational Program"
@@ -41,22 +43,9 @@ class Program(models.Model):
 
 
 class UniversityInfo(models.Model):
-
-    class Campus(models.TextChoices):
-        MOSCOW = "MO", "Москва"
-        SAINT_PETERSBURG = "SP", "Санкт-Петербург"
-        NIZHNY_NOVGOROD = "NN", "Нижний Новгород"
-        PERM = "PE", "Пермь"
-
-    campus = models.CharField(
-        max_length=2,
-        choices=Campus.choices,
-    )
     program = models.ForeignKey(
         to=Program,
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True,
+        on_delete=models.RESTRICT,
     )
     group = models.CharField(max_length=32)
     card_id = models.CharField(max_length=32)
@@ -64,3 +53,7 @@ class UniversityInfo(models.Model):
     class Meta:
         verbose_name = "University Info"
         verbose_name_plural = "University Infos"
+
+    @property
+    def campus(self) -> str:
+        return self.program.faculty.campus
