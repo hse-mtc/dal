1d7abe3f4b6ea78bd0e03facfd343eda9e626fb1
TmLev <sololev27@gmail.com>
2021-11-04 22:53:51 +0300

    refactor: Replace raw str type to concrete `Personnel` object

--- back-end/src/lms/utils/functions.py
+++ back-end/src/lms/utils/functions.py
@@ -1,38 +1,46 @@
-from datetime import timedelta, datetime
+import typing as tp
+
+from datetime import (
+    timedelta,
+    datetime,
+)
 
 from rest_framework.request import Request
-
-from lms.models.common import Milgroup
 
 from auth.models import Permission
 from auth.permissions import BasePermission
 
+from lms.models.common import Milgroup
+from lms.models.students import Student
+from lms.models.teachers import Teacher
 
-def get_user_from_request(user):
+from lms.types.personnel import Personnel
+
+
+def get_personnel_from_request_user(user) -> tp.Optional[Personnel]:
+    """Get Personnel object from the user of the incoming request.
+
+    Args:
+        user: REST/HTTP request user.
+
+    Returns:
+        personnel: Personnel object.
     """
-    Get Teacher/Student user from request.
-    Returns empty string and None if user is not
-    a teacher or a student.
 
-    :param user: user from REST/HTTP request
-    :return: user_type (str), user (User object)
-    """
-    # check if user is a teacher or a student
-
-    candidates = ["teacher", "student"]
+    candidates = ["student", "teacher"]
 
     for candidate in candidates:
         if hasattr(user, candidate):
-            return candidate, getattr(user, candidate)
+            return getattr(user, candidate)
 
-    return "", None
+    return None
 
 
 # pylint: disable=too-many-return-statements
 def milgroup_allowed_by_scope(
     milgroup: Milgroup,
     request: Request,
-    permission_class: BasePermission,
+    permission_class: tp.Type[BasePermission],
 ) -> bool:
     """
     Check if specified milgroup is compatible with permission scope.
@@ -48,32 +56,34 @@ def milgroup_allowed_by_scope(
     if request.user.is_superuser:
         return True
 
-    scope = request.user.get_perm_scope(permission_class.permission_class,
-                                        request.method)
+    scope = request.user.get_perm_scope(
+        permission_class.permission_class,
+        request.method,
+    )
 
     if scope == Permission.Scope.ALL:
         return True
 
-    # check if user is a teacher ot a student
-    user_type, user = get_user_from_request(request.user)
+    user = get_personnel_from_request_user(request.user)
     if user is None:
         return False
 
     if scope == Permission.Scope.MILFACULTY:
-        if user_type == "student":
-            milfaculty = user.milgroup.milfaculty
-        elif user_type == "teacher":
-            milfaculty = user.milfaculty
-        else:
-            return False
+        match user:
+            case Student() | Teacher():
+                milfaculty = user.milfaculty
+            case _:
+                assert False, "Unhandled Personnel type"
         return milgroup.milfaculty == milfaculty
 
     if scope in [Permission.Scope.MILGROUP, Permission.Scope.SELF]:
-        if user_type == "student":
-            return milgroup == user.milgroup
-        if user_type == "teacher":
-            return milgroup in user.milgroups
-        return False
+        match user:
+            case Student():
+                return milgroup == user.milgroup
+            case Teacher():
+                return milgroup in user.milgroups
+            case _:
+                assert False, "Unhandled Personnel type"
 
     return False
 
