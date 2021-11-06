6b831a580273f321fa4a29829ed1cdf28b9f0aec
TmLev <sololev27@gmail.com>
2021-11-06 16:37:43 +0300

    feat: Enable `ChoicesModule`

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -1,29 +1,34 @@
 import Vue from "vue";
+
 import Vuex from "vuex";
 import { getModule } from "vuex-module-decorators";
+
 import app from "./modules/app";
-import settings from "./modules/settings";
-import user from "./modules/user";
-import subjects from "./modules/subjects";
+import choices from "./modules/choices";
 import papers from "./modules/papers";
 import reference from "./modules/reference";
+import settings from "./modules/settings";
+import subjects from "./modules/subjects";
+import user from "./modules/user";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
     app,
-    settings,
-    user,
-    subjects,
+    choices,
     papers,
     reference,
+    settings,
+    subjects,
+    user,
   },
 });
 
 export default store;
 
 export const AppModule = getModule(app, store);
+export const ChoicesModule = getModule(choices, store);
 export const PapersModule = getModule(papers, store);
 export const ReferenceModule = getModule(reference, store);
 export const SettingsModule = getModule(settings, store);
