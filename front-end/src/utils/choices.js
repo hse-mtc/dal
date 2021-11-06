7810181c748560626723d872f59851e0573f5ee5
TmLev <sololev27@gmail.com>
2021-11-06 19:44:04 +0300

    feat: Return `defaultLabel` that defaults to `value` if not found

--- front-end/src/utils/choices.js
+++ front-end/src/utils/choices.js
@@ -1,14 +1,15 @@
 export const defaultChoiceLabelFromValue = (
   choices,
   value,
+  defaultLabel = value,
 ) => {
   const filtered = Object
     .values(choices)
     .filter(choice => choice.value === value);
 
-  // If no exact match, return original value for debugging purposes.
+  // If no exact match happened, return `defaultLabel`.
   if (filtered.length !== 1) {
-    return value;
+    return defaultLabel;
   }
 
   return filtered[0].label;
