bde0386c2d958aa2e48ba5749734b2fba824bc8e
TmLev <sololev27@gmail.com>
2021-11-06 19:57:54 +0300

    refactor: Remove unused consts

--- front-end/src/utils/enums.js
+++ front-end/src/utils/enums.js
@@ -15,11 +15,6 @@ export const APPLICATIONS_EXPORT_OPTIONS = {
   CSP: "протокол",
 };
 
-export const STUDENT_POSTS = {
-  GC: "Командир взвода",
-  SC: "Командир отделения",
-};
-
 export const WEEKDAYS = [
   "Понедельник",
   "Вторник",
@@ -30,38 +25,10 @@ export const WEEKDAYS = [
   "Воскресенье",
 ];
 
-export const EXCUSES = {
-  LE: "Уважительная",
-  IL: "Неуважительная",
-  LA: "Опоздание",
-};
-
-export const ABSENCE_STATUSES = {
-  OP: "Открыт",
-  CL: "Закрыт",
-};
-
-export const ENCOURAGEMENT_TYPES = {
-  EN: "Благодарность",
-  RE: "Снятие взыскания",
-};
-
-export const PUNISHMENT_TYPES = {
-  PU: "Взыскание",
-  RE: "Выговор",
-};
-
-export const LESSON_TYPES = {
-  SE: "Семинар",
-  LE: "Лекция",
-  GR: "Групповое занятия",
-  PR: "Практическое занятие",
-  FI: "Зачет",
-  EX: "Экзамен",
-};
 export const HEADDRESSES = {
   CA, HA,
 };
+
 export const OUTERWEARS = {
   JA: "", // Uniform uses JA as default, so no picture is needed here.
   PC,
