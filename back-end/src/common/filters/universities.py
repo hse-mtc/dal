0eef135faa8f0524e817dad00476bace75043914
ShishckovA <liosha.shishckov@yandex.ru>
2021-11-17 16:45:18 +0300

    Add handle for campus filtering

--- back-end/src/common/filters/universities.py
+++ back-end/src/common/filters/universities.py
@@ -1,10 +1,21 @@
-from django_filters.rest_framework import FilterSet
+from django_filters.rest_framework import (
+    FilterSet,
+    ChoiceFilter,
+)
 
-from common.models.universities import Program
+from common.models.universities import Program, Campus
 
 
 class ProgramFilter(FilterSet):
+    campus = ChoiceFilter(
+        choices=Campus.choices,
+        method="filter_by_campus",
+    )
+
+    def filter_by_campus(self, queryset, name, value):
+        # pylint: disable=unused-argument
+        return queryset.filter(faculty__campus=value)
 
     class Meta:
         model = Program
-        fields = "__all__"
+        fields = ["campus"]
