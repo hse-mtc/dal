e387110df3ff8ba7207560484a0c45ecfb7149fb
ShishckovA <liosha.shishckov@yandex.ru>
2021-11-24 17:01:04 +0300

    Add default picture

--- front-end/src/constants/applicantForm.js
+++ front-end/src/constants/applicantForm.js
@@ -267,6 +267,19 @@ export const HEADERS_BY_STEPS = {
   agreement: "Соглашение",
 };
 
+function dataURLtoFile(dataurl, filename) {
+  const arr = dataurl.split(",");
+  const mime = arr[0].match(/:(.*?);/)[1];
+  const bstr = atob(arr[1]);
+  const n = bstr.length;
+  const u8arr = new Uint8Array(n);
+
+  for (let i = 0; i < n; i += 1) {
+    u8arr[i] = bstr.charCodeAt(i);
+  }
+  return new File([u8arr], filename, { type: mime });
+}
+
 export const devInitData = {
   about: {
     surname: "Фамилия",
@@ -329,7 +342,15 @@ export const devInitData = {
   brothers: [],
   sisters: [],
   photo: {
-    photo: []
+    photo: [
+      {
+        name: "1x1_pxl.png",
+        percentage: 0,
+        raw: dataURLtoFile("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjULHy/A8AAtQBp1XLiUMAAAAASUVORK5CYII=", "1x1_pxl.png"),
+        size: 82,
+        status: "ready",
+      },
+    ],
   },
   milspecialty: { milspecialty: 8 },
   agreement: {
