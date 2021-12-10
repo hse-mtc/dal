a7a311138694bac7b2fbded04d49e3a92d0f5e12
TmLev <sololev27@gmail.com>
2021-12-10 22:29:41 +0300

    feat: Use `gunicorn` in prod

---
+++ back-end/src/gunicorn.conf.py
@@ -0,0 +1,16 @@
+import os
+
+# The socket to bind.
+bind = f"0.0.0.0:{os.environ['BACK_END_PORT']}"
+
+# The number of worker processes for handling requests.
+workers = 4
+
+# The type of workers to use.
+worker_class = "uvicorn.workers.UvicornWorker"
+
+# The maximum number of requests a worker will process before restarting.
+max_requests = 100
+
+# Front-end’s IPs from which allowed to handle set secure headers (comma separate).
+forwarded_allow_ips = "*"
