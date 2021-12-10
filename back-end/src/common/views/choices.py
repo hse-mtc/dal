7c256b7b76c94e12280ce9a333d7ac30fafc566f
TmLev <sololev27@gmail.com>
2021-12-10 23:08:46 +0300

    feat: Add default serializer for `ChoicesList` views
    Helps Swagger to match schema with the actual values

--- back-end/src/common/views/choices.py
+++ back-end/src/common/views/choices.py
@@ -1,14 +1,38 @@
 from rest_framework import permissions
 from rest_framework import views
+from rest_framework import serializers
 
 from rest_framework.request import Request
 from rest_framework.response import Response
+
+from drf_spectacular.utils import inline_serializer
+
+
+class ChoiceSerializer(serializers.Serializer):
+    value = serializers.CharField()
+    label = serializers.CharField()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
 
 
 class GenericChoicesList(views.APIView):
     choices_class = None
 
     permission_classes = [permissions.AllowAny]
+
+    def get_serializer(self):
+        serializer = inline_serializer(
+            name=self.__class__.__name__ + "Serializer",
+            fields={
+                choice.name: ChoiceSerializer()
+                for choice in self.choices_class
+            },
+        )
+        return serializer
 
     def make_choices(self, *args, **kwargs) -> dict:
         return {
