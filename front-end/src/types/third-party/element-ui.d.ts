38a0487bbdf2de2abfe420e62a9acfb475df801f
TmLev <sololev27@gmail.com>
2022-01-03 15:52:26 +0300

    refactor: Migrate `elementUIApply.js` to TypeScript

---
+++ front-end/src/types/third-party/element-ui.d.ts
@@ -0,0 +1,28 @@
+import { ElementUIComponent } from "element-ui";
+
+declare module "element-ui" {
+  class ElScrollbar extends ElementUIComponent {
+    /** Native type */
+    native: boolean
+
+    /** WrapStyle type */
+    wrapStyle: any
+
+    /** WrapClass type */
+    wrapClass: any
+
+    /** ViewClass type */
+    viewClass: any
+
+    /** ViewStyle type */
+    viewStyle: any
+
+    /** Noresize type */
+    noresize: boolean
+
+    /** Tag type */
+    tag: string
+  }
+
+  export class Scrollbar extends ElScrollbar {}
+}
