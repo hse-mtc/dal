f8934ca14857bfc3ba7ab34fbbf893a570d4c8ab
TmLev <sololev27@gmail.com>
2022-01-08 20:38:54 +0700

    refactor: Migrate `TagsInput.vue` to TypeScript

--- front-end/src/components/Tags/TagsInput.vue
+++ front-end/src/components/Tags/TagsInput.vue
@@ -1,7 +1,7 @@
 <template>
-  <ElFormItem :label="label">
+  <ElFormItem v-loading="fetchingTags" :label="label">
     <ElTag
-      v-for="selectedTag in selected"
+      v-for="selectedTag in selectedTags"
       :key="selectedTag"
       closable
       class="mr-2"
@@ -20,86 +20,83 @@
   </ElFormItem>
 </template>
 
-<script>
+<script lang="ts">
+import {
+  Component, Model, Prop, Vue,
+} from "vue-property-decorator";
+
 import { getExistingTags } from "@/api/existingTags";
 
-export default {
-  name: "TagsInput",
-  model: {
-    prop: "selected",
-    event: "change",
-  },
-
-  props: {
-    label: {
-      type: String,
-      default: "Ключевые слова",
-    },
-    selected: {
-      type: Array,
-      default: () => [],
-    },
-  },
-
-  data() {
-    return {
-      tag: "",
-      all: [],
-    };
-  },
-
-  created() {
-    this.getSuggestions();
-  },
-
-  methods: {
-    updateModel() {
-      this.$emit("change", this.selected);
-    },
-
-    handleSelect() {
-      const tag = this.tag.trim().toLowerCase();
-      if (this.tag && !this.selected.includes(tag)) {
-        // todo
-        // eslint-disable-next-line vue/no-mutating-props
-        this.selected.push(tag);
-      }
-      this.tag = "";
-
-      this.updateModel();
-    },
-
-    handleClose(tag) {
-      const index = this.selected.indexOf(tag);
-      if (index > -1) {
-        // todo
-        // eslint-disable-next-line vue/no-mutating-props
-        this.selected.splice(index, 1);
-      }
-
-      this.updateModel();
-    },
-
-    suggestions(text, cb) {
-      // Two-step filter:
-      //   1. Filter tags that are not selected yet.
-      //   2. Leave the ones that contain tag as substring.
-      const asValues = this.all
-        .filter(
-          tag => !this.selected.includes(tag) && tag.indexOf(text.toLowerCase()) > -1,
-        )
-        .map(tag => ({ value: tag }));
-
-      cb(asValues);
-    },
-
-    async getSuggestions() {
-      try {
-        this.all = (await getExistingTags()).data;
-      } catch (error) {
-        console.log("Failed to fetch Tags: ", error);
-      }
-    },
-  },
+type Suggestion = {
+  value: string;
 };
+
+@Component({
+  name: "TagsInput",
+})
+class TagsInput extends Vue {
+  @Prop() label = "Ключевые слова";
+
+  @Model("change", {
+    type: Array,
+    default: () => [],
+  })
+  readonly selectedTags!: string[];
+
+  tag = "";
+  allTags: string[] = [];
+  fetchingTags = false;
+
+  async created(): Promise<void> {
+    await this.getTags();
+  }
+
+  async getTags(): Promise<void> {
+    this.fetchingTags = true;
+    try {
+      this.allTags = (await getExistingTags()).data;
+    } catch (error) {
+      // FIXME(TmLev): Report error to user.
+      console.log("Failed to fetch Tags: ", error);
+    } finally {
+      this.fetchingTags = false;
+    }
+  }
+
+  suggestions(text: string, cb: (arg: Suggestion[]) => void): void {
+    // Two-step filter:
+    //   1. Filter tags that are not selected yet.
+    //   2. Leave the ones that contain tag as substring.
+    const asValues = this.allTags
+      .filter(tag => !this.selectedTags.includes(tag) && tag.indexOf(text.toLowerCase()) > -1)
+      .map(tag => ({ value: tag }));
+
+    cb(asValues);
+  }
+
+  updateModel(): void {
+    this.$emit("change", this.selectedTags);
+  }
+
+  handleSelect(): void {
+    const tag = this.tag.trim().toLowerCase();
+    if (tag && !this.selectedTags.includes(tag)) {
+      this.selectedTags.push(tag);
+    }
+    this.tag = "";
+
+    this.updateModel();
+  }
+
+  handleClose(tag: string): void {
+    const index = this.selectedTags.indexOf(tag);
+    if (index > -1) {
+      this.selectedTags.splice(index, 1);
+    }
+
+    this.updateModel();
+  }
+}
+
+export default TagsInput;
 </script>
