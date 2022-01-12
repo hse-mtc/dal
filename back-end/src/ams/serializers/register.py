4ec596c2c91ae815079e273bd131caf384fee26a
deborisov <deborisov_1@edu.hse.ru>
2022-01-12 11:17:30 +0300

    Raw email confirmation for applicants

---
+++ back-end/src/ams/serializers/register.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from auth.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    #password = serializers.CharField(max_length = 50, min_length = 6, write_only = True)
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
--- back-end/src/ams/urls.py
+++ back-end/src/ams/urls.py
@@ -6,6 +6,7 @@ from django.urls import (
 from rest_framework import routers
 
 from ams.views.applicants import ApplicantViewSet
+from ams.views.register import RegisterView
 
 router = routers.DefaultRouter()
 router.register("applicants", ApplicantViewSet)
@@ -13,4 +14,5 @@ router.register("applicants", ApplicantViewSet)
 urlpatterns = [
     # Router.
     path("", include(router.urls)),
+    path("register/", RegisterView.as_view(), name = "register")
 ]
