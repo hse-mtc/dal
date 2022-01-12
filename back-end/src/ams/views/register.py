4a34018d8b2b1996d5508ae016a20e3432aeb26c
deborisov <deborisov_1@edu.hse.ru>
2022-01-12 15:23:38 +0300

    Fix formatting

--- back-end/src/ams/views/register.py
+++ back-end/src/ams/views/register.py
@@ -7,6 +7,7 @@ from auth.tokens.registration import generate_regconf_token
 from common.email.registration import send_regconf_email
 from django.contrib.auth.base_user import BaseUserManager
 
+
 class RegisterView(generics.GenericAPIView):
 
     serializer_class = RegisterSerializer
@@ -18,20 +19,20 @@ class RegisterView(generics.GenericAPIView):
         if not email.endswith("@edu.hse.ru"):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        if not User.objects.filter(email = email).exists():
+        if not User.objects.filter(email=email).exists():
             user["password"] = User.objects.make_random_password()
-            serializer = self.serializer_class(data = user)
-            serializer.is_valid(raise_exception = True)
+            serializer = self.serializer_class(data=user)
+            serializer.is_valid(raise_exception=True)
             serializer.save()
-            
-        user = User.objects.get(email = email)
-        applicant_permission = Permission.objects.get(name = "Applicant")
+
+        user = User.objects.get(email=email)
+        applicant_permission = Permission.objects.get(name="Applicant")
         user.permissions.add(applicant_permission)
         send_regconf_email(
-            address = user.username,
-            email = user.email,
-            url = request.META["HTTP_REFERER"],
-            token = generate_regconf_token(user),
+            address=user.username,
+            email=user.email,
+            url=request.META["HTTP_REFERER"],
+            token=generate_regconf_token(user),
         )
 
         return Response(status=status.HTTP_201_CREATED)
