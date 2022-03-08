b466fdf706c9d64f5088b919e7f725103a86cd75
deborisov <deborisov_1@edu.hse.ru>
2022-03-08 12:26:59 +0300

    Add applicants to prod populate

--- back-end/src/common/management/commands/prod_populate.py
+++ back-end/src/common/management/commands/prod_populate.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 from auth.models import Group
 from auth.populate.permissions import (
+    get_applicant_permissions,
     get_student_permissions,
     get_teacher_permissions,
     get_milfaculty_head_permissions,
@@ -19,12 +20,12 @@ class Command(BaseCommand):
 
         print("Populating `auth` models...", end="")
 
-        User = get_user_model()
-
+        applicants, _ = Group.objects.get_or_create(name="Абитуриент")
         students, _ = Group.objects.get_or_create(name="Студент")
         teachers, _ = Group.objects.get_or_create(name="Преподаватель")
         milfaculty_heads, _ = Group.objects.get_or_create(name="Начальник цикла")
 
+        applicants.permissions.set(get_applicant_permissions())
         students.permissions.set(get_student_permissions())
         teachers.permissions.set(get_teacher_permissions())
         milfaculty_heads.permissions.set(get_milfaculty_head_permissions())
