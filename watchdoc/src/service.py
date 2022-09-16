e4b0c2635042120c39d060b2707355316fd92612
altius01 <altius01@mail.ru>
2022-09-16 11:26:43 +0300

    Maded api for applicant docs generating

--- watchdoc/src/service.py
+++ watchdoc/src/service.py
@@ -42,6 +42,7 @@ DOCUMENTS = [
     ("ro-reference-signed.docx", "Направление ВК (подписано).docx"),
     ("medical-records.docx", "Медкарта.docx"),
     ("family-info.docx", "Сведения о семье.docx"),
+    ("applicant-form.docx", "Анкета абитуриента.docx"),
 ]
 
 
