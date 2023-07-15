c1fd5f177f8b5e3e3b26ca5a060d88fd41cecb7e
Alexey Shishkov <liosha.shishckov@yandex.ru>
2023-07-15 15:14:33 +0300

    Add user for marks creation

--- back-end/src/lms/populate/marks.py
+++ back-end/src/lms/populate/marks.py
@@ -1,3 +1,4 @@
+from auth.models import User
 from common.utils.populate import get_or_create
 
 from lms.models.lessons import Lesson
@@ -8,37 +9,44 @@ from lms.models.marks import Mark
 def create_marks(
     lessons: list[Lesson],
     students: dict[str, Student],
+    user_to_create_marks: User
 ) -> None:
     marks = [
         {
             "values": [5],
             "lesson": lessons[0],
             "student": students["Хромов"],
+            "changed_by": user_to_create_marks
         },
         {
             "values": [4],
             "lesson": lessons[0],
             "student": students["Исаков"],
+            "changed_by": user_to_create_marks
         },
         {
             "values": [3],
             "lesson": lessons[0],
             "student": students["Кацевалов"],
+            "changed_by": user_to_create_marks
         },
         {
             "values": [5],
             "lesson": lessons[1],
             "student": students["Хромов"],
+            "changed_by": user_to_create_marks
         },
         {
             "values": [3],
             "lesson": lessons[1],
             "student": students["Исаков"],
+            "changed_by": user_to_create_marks
         },
         {
             "values": [2],
             "lesson": lessons[1],
             "student": students["Кацевалов"],
+            "changed_by": user_to_create_marks
         },
     ]
 
