aa49196afc106af48081fa69deb0cd91737687b3
altius01 <altius01@mail.ru>
2023-07-18 17:56:45 +0300

    fixed bugs

--- tgbot/src/handlers/menu.py
+++ tgbot/src/handlers/menu.py
@@ -1,7 +1,15 @@
 from aiogram.types import Message
 
+from keyboards.message_texts import get_hello_message
 from keyboards.reply import list_milgroup_keyboard
 from keyboards.button_texts import ButtonText
+
+from api.auth import fetch_phone
+from api.student import (
+    fetch_students,
+    Post,
+    Student,
+)
 
 
 async def menu_handler(message: Message) -> None:
@@ -13,6 +21,7 @@ menu_handler.handler_filters = [
     lambda message: message.text == ButtonText.MAIN_MENU.value,
 ]
 
+
 async def start_handler(message: Message) -> None:
     if message.text == ButtonText.START.value:
         phone = await fetch_phone(chat_id=message.chat.id)
@@ -20,16 +29,10 @@ async def start_handler(message: Message) -> None:
         assert isinstance(user, Student)
 
         await message.reply(
-            textwrap.dedent(f"""
-                Здравия желаю, {user.fullname.strip()}.
-                
-                Взвод: {user.milgroup.title}.
-                Должность: {getattr(Post, user.post).value}.
-                
-                Нажмите кнопку "{ButtonText.LIST_MILGROUP.value}", чтобы вывести список студентов Вашего взвода.
-            """),
+            get_hello_message(user.fullname.strip(), user.milgroup.title, getattr(Post, user.post).value),
             reply_markup=list_milgroup_keyboard(),
         )
+
 
 start_handler.handler_filters = [
     lambda message: message.text == ButtonText.MAIN_MENU.value,
