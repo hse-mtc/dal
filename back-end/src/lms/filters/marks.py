175bd8e798f4436e68100e7c1bb1a431bf71b5f0
altius01 <altius01@mail.ru>
2023-07-18 11:31:16 +0300

    deleted possibly unused year of admission filter for historicalmark queryset

--- back-end/src/lms/filters/marks.py
+++ back-end/src/lms/filters/marks.py
@@ -32,13 +32,6 @@ class MarkHistoryFilter(FilterSet):
 
     archived = BooleanFilter(field_name="student__milgroup__archived")
 
-    year_of_admission = NumberFilter(method="filter_by_admission")
-
-    def filter_by_admission(self, queryset, name, value):
-        # pylint: disable=unused-argument
-        value = value % 100  # strip first two symbols of the year
-        return queryset.filter(student__milgroup__title__startswith=value)
-
     class Meta:
         model = HistoricalMark
         fields = ["student", "lesson", "archived"]
