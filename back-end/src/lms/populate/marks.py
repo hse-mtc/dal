259e22e3c4e5afab9f925ae5cd22b0037ee3802f
Alexey Shishkov <liosha.shishckov@yandex.ru>
2023-07-24 08:33:44 +0300

    Change user to create marks in populate

--- back-end/src/lms/populate/marks.py
+++ back-end/src/lms/populate/marks.py
@@ -4,47 +4,48 @@ from common.utils.populate import get_or_create
 from lms.models.lessons import Lesson
 from lms.models.students import Student
 from lms.models.marks import Mark
+from lms.models.teachers import Teacher
 
 
 def create_marks(
-    lessons: list[Lesson], students: dict[str, Student], user_to_create_marks: User
+    lessons: list[Lesson], students: dict[str, Student], teachers: dict[str, Teacher]
 ) -> None:
     marks = [
         {
             "values": [5],
             "lesson": lessons[0],
             "student": students["Хромов"],
-            "changed_by": user_to_create_marks,
+            "changed_by": teachers["Репалов"].user,
         },
         {
             "values": [4],
             "lesson": lessons[0],
             "student": students["Исаков"],
-            "changed_by": user_to_create_marks,
+            "changed_by": teachers["Репалов"].user,
         },
         {
             "values": [3],
             "lesson": lessons[0],
             "student": students["Кацевалов"],
-            "changed_by": user_to_create_marks,
+            "changed_by": teachers["Репалов"].user,
         },
         {
             "values": [5],
             "lesson": lessons[1],
             "student": students["Хромов"],
-            "changed_by": user_to_create_marks,
+            "changed_by": teachers["Репалов"].user,
         },
         {
             "values": [3],
             "lesson": lessons[1],
             "student": students["Исаков"],
-            "changed_by": user_to_create_marks,
+            "changed_by": teachers["Репалов"].user,
         },
         {
             "values": [2],
             "lesson": lessons[1],
             "student": students["Кацевалов"],
-            "changed_by": user_to_create_marks,
+            "changed_by": teachers["Репалов"].user,
         },
     ]
 
