3a2d1ac31573228a34a90edcb62b65db1b72332e
Alexey Shishkov <liosha.shishckov@yandex.ru>
2023-10-14 15:32:11 +0300

    Refactor, remove prints

--- back-end/src/dms/tests/test_end_to_end_numbering.py
+++ back-end/src/dms/tests/test_end_to_end_numbering.py
@@ -7,12 +7,16 @@ import pytest
 from common.models.subjects import Subject
 from dms.models.class_materials import Section, Topic
 from django.db import models
+from logging import getLogger
+import random
 
 
 def check_order(collection: list[Union[Section, Topic]]):
     for i, elem in enumerate(collection):
         elem.refresh_from_db()
-        assert elem.order == i, f"Incorrect order for {elem}: expected {i}, got {elem.order}"
+        assert (
+            elem.order == i
+        ), f"Incorrect order for {elem}: expected {i}, got {elem.order}"
 
 
 def save_each(objs: list[models.Model]):
@@ -85,6 +89,7 @@ def clean_up_class_materials():
     Section.objects.all().delete()
     Subject.objects.all().delete()
 
+
 @pytest.mark.django_db
 def test_sections_creating(su_client, subject1, subject2):
     section1 = create_section(su_client, title="Section 1", subject=subject1)
@@ -101,9 +106,22 @@ def test_sections_creating(su_client, subject1, subject2):
 
 @pytest.mark.django_db
 def test_sections_reordering(
-        su_client,
-        section1, section2, section3, section4, section5, section6,
-        topic1, topic2, topic3, topic4, topic5, topic6, topic7, topic8, topic9
+    su_client,
+    section1,
+    section2,
+    section3,
+    section4,
+    section5,
+    section6,
+    topic1,
+    topic2,
+    topic3,
+    topic4,
+    topic5,
+    topic6,
+    topic7,
+    topic8,
+    topic9,
 ):
 
     check_order([section1, section2, section3, section4])
@@ -184,20 +202,7 @@ def test_sections_reordering(
     clean_up_class_materials()
 
 
-# @pytest.mark.django_db
-# def test_sections_deletion(
-#         su_client,
-#         section1, section2, section3, section4, section5, section6,
-#         topic1, topic2, topic3, topic4, topic5, topic6, topic7, topic8, topic9
-# ):
-#
-#
-
-def random_actions(
-        su_client,
-        subject1,
-        subject2
-):
+def random_actions(su_client, subject1, subject2):
     def get_section(sec_id: int):
         return Section.objects.filter(pk=sec_id).first()
 
@@ -210,13 +215,14 @@ def random_actions(
     def get_topics_order(subj_data):
         return [get_topic(topic["id"]) for s in subj_data for topic in s["topics"]]
 
-    import random
+    logger = getLogger()
     random.seed(42)
+
     s1_data = []
     s2_data = []
     sn, sn_data = subject1, s1_data
     sold, sold_data = subject2, s2_data
-    for i in range(10000):
+    for i in range(100):
         sn, sn_data, sold, sold_data = sold, sold_data, sn, sn_data
         section_order = get_sections_order(sn_data)
         topic_order = get_topics_order(sn_data)
@@ -234,43 +240,68 @@ def random_actions(
         if action == "create_section":
             new_sec = create_section(su_client, "Section title", sn)
             sn_data.append({"id": new_sec.pk, "topics": []})
-            print(f"Creating section {new_sec.pk} on subject {sn.pk}")
+            logger.debug(f"Creating section {new_sec.pk} on subject {sn.pk}")
 
         elif action == "move_section":
             c_sec_order = random.randrange(len(section_order))
             to_move = random.randrange(len(section_order))
             move_section(su_client, section_order[c_sec_order], to_move)
             sn_data.insert(to_move, sn_data.pop(c_sec_order))
-            print(f"Moving section {section_order[c_sec_order].pk} to place {to_move} on subject {sn.pk}")
+            logger.debug(
+                f"Moving section {section_order[c_sec_order].pk} to place {to_move} on subject {sn.pk}"
+            )
 
         elif action == "delete_section":
             c_sec_order = random.randrange(len(section_order))
             delete_section(su_client, section_order[c_sec_order])
             sn_data.pop(c_sec_order)
+            logger.debug(
+                f"Deleting topic {section_order[c_sec_order].pk}" f"on subject {sn.pk}"
+            )
 
         elif action == "create_topic":
             c_sec_order = random.randrange(len(section_order))
-            new_topic = create_topic(su_client, "Topic title", section_order[c_sec_order])
+            new_topic = create_topic(
+                su_client, "Topic title", section_order[c_sec_order]
+            )
             sn_data[c_sec_order]["topics"].append({"id": new_topic.pk})
-            print(f"Creating topic {new_topic.pk} on section {section_order[c_sec_order].pk} on subject {sn.pk}")
+            logger.debug(
+                f"Creating topic {new_topic.pk} on section {section_order[c_sec_order].pk} on subject {sn.pk}"
+            )
 
         elif action == "move_topic":
             while 1:
                 c_sec_order = random.randrange(len(section_order))
                 if sn_data[c_sec_order]["topics"]:
                     break
-            topic_low = sum([1 for sec_id in range(c_sec_order) for top in sn_data[sec_id]["topics"]])
-            topic_high = sum([1 for sec_id in range(c_sec_order + 1) for top in sn_data[sec_id]["topics"]])
+            topic_low = sum(
+                [
+                    1
+                    for sec_id in range(c_sec_order)
+                    for top in sn_data[sec_id]["topics"]
+                ]
+            )
+            topic_high = sum(
+                [
+                    1
+                    for sec_id in range(c_sec_order + 1)
+                    for top in sn_data[sec_id]["topics"]
+                ]
+            )
             assert topic_low < topic_high
             c_top_order = random.randrange(topic_low, topic_high)
             to_move = random.randrange(topic_low, topic_high)
             move_topic(su_client, topic_order[c_top_order], to_move)
             c_top_order_inside_section = c_top_order - topic_low
             to_move_inside_section = to_move - topic_low
-            sn_data[c_sec_order]["topics"].insert(to_move_inside_section,
-                                                  sn_data[c_sec_order]["topics"].pop(c_top_order_inside_section))
-            print(f"Moving topic {topic_order[c_top_order].pk} to place {to_move}"
-                  f" on section {section_order[c_sec_order].pk} on subject {sn.pk}")
+            sn_data[c_sec_order]["topics"].insert(
+                to_move_inside_section,
+                sn_data[c_sec_order]["topics"].pop(c_top_order_inside_section),
+            )
+            logger.debug(
+                f"Moving topic {topic_order[c_top_order].pk} to place {to_move}"
+                f" on section {section_order[c_sec_order].pk} on subject {sn.pk}"
+            )
 
         elif action == "delete_topic":
             while 1:
@@ -278,19 +309,33 @@ def random_actions(
                 if sn_data[c_sec_order]["topics"]:
                     break
 
-            topic_low = sum([1 for sec_id in range(c_sec_order) for top in sn_data[sec_id]["topics"]])
-            topic_high = sum([1 for sec_id in range(c_sec_order + 1) for top in sn_data[sec_id]["topics"]])
+            topic_low = sum(
+                [
+                    1
+                    for sec_id in range(c_sec_order)
+                    for top in sn_data[sec_id]["topics"]
+                ]
+            )
+            topic_high = sum(
+                [
+                    1
+                    for sec_id in range(c_sec_order + 1)
+                    for top in sn_data[sec_id]["topics"]
+                ]
+            )
             assert topic_low < topic_high
             c_top_order = random.randrange(topic_low, topic_high)
             delete_topic(su_client, topic_order[c_top_order])
             c_top_order_inside_section = c_top_order - topic_low
             sn_data[c_sec_order]["topics"].pop(c_top_order_inside_section)
+            logger.debug(
+                f"Deleting topic {topic_order[c_top_order].pk} "
+                f"from section {section_order[c_sec_order].pk} on subject {sn.pk}"
+            )
         else:
             assert False, "Incorrect action"
 
 
-def test_random_actions(
-        su_client,
-        subject1,
-        subject2):
+def test_random_actions(su_client, subject1, subject2):
     random_actions(su_client, subject1, subject2)
+    clean_up_class_materials()
