176d06917f7d1e3c1916c569bcf149305ce07599
Georgii <fatahoff.georgy@yandex.ru>
2024-09-13 10:22:29 +0300

    fix force push

---
+++ watchdoc/src/disk.py
@@ -0,0 +1,30 @@
+import yadisk
+
+
+class DiskService:
+    """Yandex Disk API wrapper.
+
+    Usage:
+        ds = DiskService()
+    """
+
+    def __init__(self, token) -> None:
+        self._service = yadisk.YaDisk(token=token)
+
+    def obtain_folder(self, path, **kwargs):
+        if not self._service.exists(path):
+            self._service.mkdir(path, **kwargs)
+        return path
+
+    def upload_docx(self, local_path, remote_path, **kwargs):
+        with open(local_path, "rb") as f:
+            return self._service.upload(f, remote_path, **kwargs)
+
+    def delete_file(self, path, **kwargs):
+        self._service.remove(path, **kwargs)
+
+    def read_share_folder_with_anyone(self, remote_path):
+        self._service.publish(remote_path)
+        public_key = self._service.get_meta(remote_path)["public_key"]
+        public_resource = self._service.get_public_meta(public_key)
+        return public_resource["public_url"]
