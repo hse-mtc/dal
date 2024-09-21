06bbf5d953dee4501b7d28b4d77c5a9b63b8c1b7
Alexey Shishkov <alexey.shishkov@vkteam.ru>
2024-09-21 18:03:19 +0300

    Make admission year calculation same on backend and frontend

--- back-end/src/ams/utils/common.py
+++ back-end/src/ams/utils/common.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 def get_current_admission_year():
     now = datetime.now()
-    if (now.month, now.day) > (9, 1):
+    if (now.month, now.day) >= (9, 1):
         return now.year + 1
     return now.year
