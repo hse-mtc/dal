0f507690013b1128e987871b7d5606525cf897fb
ShishckovA <liosha.shishckov@yandex.ru>
2024-10-09 01:14:21 +0000

    Fix formatting

--- back-end/src/common/management/commands/populate.py
+++ back-end/src/common/management/commands/populate.py
@@ -20,7 +20,9 @@ from common.populate.universities import (
     create_faculties,
     create_programs,
 )
-from common.populate.milspecialities_selectable_by_programs import create_milspecialities_selectable_by_programs
+from common.populate.milspecialities_selectable_by_programs import (
+    create_milspecialities_selectable_by_programs,
+)
 
 from common.utils.date import get_date_range
 
@@ -171,8 +173,7 @@ class Command(BaseCommand):
         faculties = create_faculties()
         programs = create_programs(faculties)
         create_milspecialities_selectable_by_programs(
-            milspecialties=milspecialties,
-            programs=programs
+            milspecialties=milspecialties, programs=programs
         )
 
         print(" OK")
--- back-end/src/common/populate/milspecialities_selectable_by_programs.py
+++ back-end/src/common/populate/milspecialities_selectable_by_programs.py
@@ -4,7 +4,10 @@ from common.models.universities import (
     Program,
 )
 
-def create_milspecialities_selectable_by_programs(milspecialties: dict[str, Milspecialty], programs: dict[str, Milspecialty]):
+
+def create_milspecialities_selectable_by_programs(
+    milspecialties: dict[str, Milspecialty], programs: dict[str, Milspecialty]
+):
     milspecialties["453000"].selectable_by.add(
         programs["Информационная безопасность"],
         programs["Информатика и вычислительная техника"],
