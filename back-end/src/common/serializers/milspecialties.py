173c356dedd2ca216b7e504b12e57d9adfbbf316
ShishckovA <liosha.shishckov@yandex.ru>
2024-10-11 08:55:33 +0000

    Move selectable check to method, rename serializer

--- back-end/src/common/serializers/milspecialties.py
+++ back-end/src/common/serializers/milspecialties.py
@@ -9,15 +9,12 @@ class MilspecialtySerializer(serializers.ModelSerializer):
         exclude = ["selectable_by", "selectable_by_every_program"]
 
 
-class MilspecialtySelectableByProgramSerializer(MilspecialtySerializer):
+class WithSelectableByProgramMilspecialtySerializer(MilspecialtySerializer):
     selectable_by_program = serializers.SerializerMethodField()
 
-    def get_selectable_by_program(self, obj):
+    def get_selectable_by_program(self, milspecialty: Milspecialty):
         request = self.context["request"]
         program = request.query_params.get("program")
         if program is not None:
-            return (
-                obj.selectable_by.filter(pk=program).exists()
-                or obj.selectable_by_every_program
-            )
+            return milspecialty.is_selectable_by_program(program)
         return False
