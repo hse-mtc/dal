6cf7c92a7a1b725d20c80faa1ff14925dc34573d
asadullinam <amasadullin@edu.hse.ru>
2025-10-08 13:00:28 +0300

    add_request_with_filter_name_and_type

--- front-end/src/components/Search/PapersAdvanced.vue
+++ front-end/src/components/Search/PapersAdvanced.vue
@@ -72,12 +72,49 @@
           />
         </el-col>
       </el-row>
+
+      <!-- Динамические фильтры для категории -->
+      <div v-if="currentCategoryFilters.length > 0" class="mt-4">
+        <div class="filters-title pl-1 mb-3">
+          <strong>Фильтры категории "{{ currentCategory.title }}"</strong>
+        </div>
+        <el-row
+          v-for="(filter, index) in currentCategoryFilters"
+          :key="index"
+          :class="index > 0 ? 'mt-3' : ''"
+        >
+          <el-col :offset="1" :span="10">
+            <div class="filters-title pl-1">
+              {{ filter.filtername }}
+            </div>
+
+            <!-- Поле для строкового фильтра -->
+            <el-input
+              v-if="filter.type === 'string'"
+              v-model="categoryFilters[filter.filtername]"
+              :placeholder="`Введите ${filter.filtername.toLowerCase()}`"
+              clearable
+              @input="updateQuery"
+            />
+
+            <!-- Поле для числового фильтра -->
+            <el-input-number
+              v-else-if="filter.type === 'number'"
+              v-model="categoryFilters[filter.filtername]"
+              :placeholder="`Введите ${filter.filtername.toLowerCase()}`"
+              style="width: 100%"
+              @input="updateQuery"
+            />
+          </el-col>
+        </el-row>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { Component, Vue } from "vue-property-decorator";
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Component, Vue, Watch } from "vue-property-decorator";
 import moment from "moment";
 import { surnameWithInitials } from "@/utils/person";
 import { PapersModule } from "@/store";
@@ -89,11 +126,38 @@ class AdvancedPaperSearch extends Vue {
   author = null
   publisher = null
   valueDate = ""
+  categoryFilters = {}
 
   get publishers() { return PapersModule.publishers; }
   get authors() { return PapersModule.authors; }
+  get categories() { return PapersModule.categories; }
+
+  // Получаем текущую категорию на основе route параметра
+  get currentCategory() {
+    const categoryId = this.$route.query.category;
+    if (!categoryId) { return null; }
+    return this.categories.find(cat => cat.id === parseInt(categoryId, 10)) || null;
+  }
+
+  // Получаем фильтры из JSON Schema формата
+  get currentCategoryFilters() {
+    if (!this.currentCategory || !this.currentCategory.filters || !this.currentCategory.filters.properties) {
+      return [];
+    }
+
+    return Object.keys(this.currentCategory.filters.properties).map(propertyName => ({
+      filtername: propertyName,
+      type: this.currentCategory.filters.properties[propertyName].type,
+    }));
+  }
 
   surnameWithInitials = surnameWithInitials
+
+  // Отслеживаем изменение route для обновления фильтров категории
+  @Watch("$route")
+  onRouteChange() {
+    this.initializeCategoryFilters();
+  }
 
   advancedClick() {
     const filters = document.querySelector(".filters");
@@ -104,6 +168,41 @@ class AdvancedPaperSearch extends Vue {
     } else {
       filters.style.display = "none";
       array.style.transform = "rotate(0deg)";
+    }
+  }
+
+  created() {
+    // Инициализируем фильтры категории из query параметров
+    this.initializeCategoryFilters();
+
+    // Инициализируем стандартные фильтры
+    this.author = this.$route.query.authors ? parseInt(this.$route.query.authors, 10) : null;
+    this.publisher = this.$route.query.publishers ? parseInt(this.$route.query.publishers, 10) : null;
+
+    // Инициализируем дату из query параметров
+    if (this.$route.query.start_date && this.$route.query.end_date) {
+      this.valueDate = [
+        new Date(this.$route.query.start_date),
+        new Date(this.$route.query.end_date),
+      ];
+    }
+  }
+
+  initializeCategoryFilters() {
+    this.categoryFilters = {};
+    if (this.currentCategoryFilters.length > 0) {
+      this.currentCategoryFilters.forEach(filter => {
+        const paramKey = `filter_${filter.filtername}`;
+        const queryValue = this.$route.query[paramKey];
+
+        if (queryValue) {
+          if (filter.type === "number") {
+            this.categoryFilters[filter.filtername] = parseFloat(queryValue);
+          } else {
+            this.categoryFilters[filter.filtername] = queryValue;
+          }
+        }
+      });
     }
   }
 
@@ -118,6 +217,16 @@ class AdvancedPaperSearch extends Vue {
     if (this.valueDate) {
       query.start_date = moment(this.valueDate[0]).format("YYYY-MM-DD");
       query.end_date = moment(this.valueDate[1]).format("YYYY-MM-DD");
+    }
+
+    // Добавляем фильтры категории в query
+    if (this.currentCategoryFilters.length > 0) {
+      this.currentCategoryFilters.forEach(filter => {
+        const filterValue = this.categoryFilters[filter.filtername];
+        if (filterValue !== null && filterValue !== undefined && filterValue !== "") {
+          query[`filter_${filter.filtername}`] = filterValue;
+        }
+      });
     }
 
     this.$router.push({ query });
